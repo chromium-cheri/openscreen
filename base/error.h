dc298002fea381c890506a62b2b320cc9cf19e7c
mark a. foltz <mfoltz@chromium.org>
2018-09-25 11:37:09 -0700

    [Open Screen] Adds ErrorOr<T> and a global Error object.
    Bug: openscreen:2
    Change-Id: I908916fb3170de0bb832a77e85bcf8fa0f410691

---
+++ base/error.h
@@ -0,0 +1,109 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_ERROR_H_
+#define BASE_ERROR_H_
+
+#include <memory>
+#include <ostream>
+#include <string>
+#include <utility>
+
+namespace openscreen {
+
+// Represents an error returned by an OSP library operation.  An error has a
+// code and an optional message.
+class Error {
+ public:
+  enum class Code {
+    // No error occurred.
+    kNone = 0,
+    // CBOR parsing error.
+    kCborParsing = 1,
+  };
+
+  Error();
+  explicit Error(const Error& error);
+  explicit Error(Error&& error);
+  explicit Error(Code code);
+  Error(Code code, const std::string& message);
+  Error(Code code, std::string&& message);
+
+  Error& operator=(const Error& other) = default;
+  Error& operator=(Error&& other) = default;
+  bool operator==(const Error& other) const;
+
+  Code code() const { return code_; }
+  const std::string& message() const { return message_; }
+
+  static const std::string CodeToString(Error::Code code);
+
+ private:
+  Code code_ = Code::kNone;
+  std::string message_;
+};
+
+std::ostream& operator<<(std::ostream& out, const Error& error);
+
+
+// A convenience function to return a single value from a function that can
+// return a value or an error.  For normal results, construct with a Value*
+// (ErrorOr takes ownership) and the Error will be kNone with an empty message.
+// For Error results, construct with an error code and value.
+//
+// Example:
+//
+// ErrorOr<Bar> Foo:DoSomething() {
+//   if (success) {
+//     return new Bar();
+//   } else {
+//     return Error(kBadThingHappened, "No can do");
+//   }
+// }
+//
+// TODO(mfoltz):
+// - Add support for type conversions
+// - Better support for primitive (non-movable) values
+template <typename Value>
+class ErrorOr {
+ public:
+  explicit ErrorOr(ErrorOr&& error_or) = default;
+  explicit ErrorOr(std::unique_ptr<Value> value) : value_(std::move(value)) {}
+  explicit ErrorOr(Value* value) : value_(value) {}
+  explicit ErrorOr(Error&& error) : error_(std::move(error)) {}
+  explicit ErrorOr(const Error& error) : error_(error) {}
+  explicit ErrorOr(Error::Code code) : error_(code) {}
+  ErrorOr(Error::Code code, std::string&& message) : error_(code, message) {}
+  ErrorOr(Error::Code code, const std::string& message) :
+    error_(code, message) {}
+  ~ErrorOr() = default;
+
+  ErrorOr& operator=(ErrorOr&& other) = default;
+
+  // Move-only type.
+  ErrorOr(const ErrorOr&) = delete;
+  ErrorOr& operator=(const ErrorOr&) = delete;
+
+  bool is_error() const { return error_.code() != Error::Code::kNone; }
+  bool is_value() const { return value_.get(); }
+  const Error& error() const { return error_; }
+
+  Error&& error()  {
+    return std::move(error_);
+  }
+
+  const Value& value() { return *value_; }
+
+  std::unique_ptr<Value>&& MoveValue() {
+    return std::move(value_);
+  }
+
+ private:
+  Error error_;
+  std::unique_ptr<Value> value_;
+};
+
+}  // namespace openscreen
+
+#endif  // BASE_ERROR_H_
