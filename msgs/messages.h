ebd9bb78e76fa0f1272615a6f13374be2b048be4
btolsch <btolsch@chromium.org>
2018-09-26 13:40:56 -0700

    Add CDDL code generator
    The CDDL code generator works for current Presentation API message
    definitions and outputs C++ code for encoding and decoding the messages
    as CBOR.
    
    Change-Id: If0bf205a05d25b2056dd1df7a9598e7dbb254fdc

---
+++ msgs/messages.h
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MSGS_MESSAGES_H_
+#define MSGS_MESSAGES_H_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace openscreen {
+namespace msgs {
+
+#include "msgs/osp.h"
+
+}  // namespace msgs
+}  // namespace openscreen
+
+#endif  // MSGS_MESSAGES_H_
