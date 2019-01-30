ba5de5606b2e26c63fdf1f8335472322535c79ba
Jordan Bayles <jophba@chromium.org>
2019-01-30 10:33:53 -0800

    Fix QUIC stream close + test support
    This change fixes the behavior of stream destruction on close.
    Previously, it was ambiguous whether OnReceived would be called again
    after OnClose.  After looking more carefully at the source, it was
    determined that the stream needs to live for the rest of the current
    event loop iteration (really just the particular OnDataReceived stack)
    after OnClose is called.
    
    This change also irons out the test support for QUIC connections by
    adding FakeQuicBridge.  This class manages the connection factories,
    message demuxers, and clocks associated with a controller and receiver
    side.  This simplifies writing both single-sided tests (e.g.
    availability requests) on both the controller and receiver sides and
    dual-sided tests (e.g. presentation connection tests).
    
    Note: this change was originally submitted by @btolsch, at the following
    code review URL:
    
    https://chromium-review.googlesource.com/c/openscreen/+/1417054
    
    Bug: openscreen:28
    Change-Id: I757d9f6d584715e796a0f96ea5bc2471597438dd

---
+++ api/impl/quic/testing/quic_test_support.h
@@ -0,0 +1,68 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef API_IMPL_QUIC_TESTING_FAKE_QUIC_BRIDGE_H_
+#define API_IMPL_QUIC_TESTING_FAKE_QUIC_BRIDGE_H_
+
+#include <memory>
+
+#include "api/impl/quic/testing/fake_quic_connection_factory.h"
+#include "api/impl/testing/fake_clock.h"
+#include "api/public/message_demuxer.h"
+#include "api/public/network_metrics.h"
+#include "api/public/protocol_connection_client.h"
+#include "api/public/protocol_connection_server.h"
+#include "base/ip_address.h"
+#include "third_party/googletest/src/googlemock/include/gmock/gmock.h"
+
+namespace openscreen {
+
+class MockServiceObserver final : public ProtocolConnectionServiceObserver {
+ public:
+  ~MockServiceObserver() override = default;
+
+  MOCK_METHOD0(OnRunning, void());
+  MOCK_METHOD0(OnStopped, void());
+  MOCK_METHOD1(OnMetrics, void(const NetworkMetrics& metrics));
+  MOCK_METHOD1(OnError, void(const Error& error));
+};
+
+class MockServerObserver final : public ProtocolConnectionServer::Observer {
+ public:
+  ~MockServerObserver() override = default;
+
+  MOCK_METHOD0(OnRunning, void());
+  MOCK_METHOD0(OnStopped, void());
+  MOCK_METHOD1(OnMetrics, void(const NetworkMetrics& metrics));
+  MOCK_METHOD1(OnError, void(const Error& error));
+
+  MOCK_METHOD0(OnSuspended, void());
+
+  void OnIncomingConnection(
+      std::unique_ptr<ProtocolConnection>&& connection) override {
+    OnIncomingConnectionMock(connection.release());
+  }
+  MOCK_METHOD1(OnIncomingConnectionMock, void(ProtocolConnection* connection));
+};
+
+struct FakeQuicBridge {
+  FakeQuicBridge();
+  ~FakeQuicBridge();
+
+  void RunTasksUntilIdle();
+
+  FakeClock* controller_fake_clock;
+  FakeClock* receiver_fake_clock;
+  const IPEndpoint controller_endpoint{{192, 168, 1, 3}, 4321};
+  const IPEndpoint receiver_endpoint{{192, 168, 1, 17}, 1234};
+  std::unique_ptr<MessageDemuxer> controller_demuxer;
+  std::unique_ptr<MessageDemuxer> receiver_demuxer;
+  std::unique_ptr<FakeQuicConnectionFactoryBridge> fake_bridge;
+  MockServiceObserver mock_client_observer;
+  MockServerObserver mock_server_observer;
+};
+
+}  // namespace openscreen
+
+#endif  // API_IMPL_QUIC_TESTING_FAKE_QUIC_BRIDGE_H_
