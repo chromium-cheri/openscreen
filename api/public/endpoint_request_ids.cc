e2393a841d45cec217b5d02f31efd66afb18891a
btolsch <btolsch@chromium.org>
2019-02-28 11:26:54 -0800

    Track request IDs by endpoint ID
    This change adds a client/server-scoped tracker for request IDs that is
    indexed by endpoint IDs.  This is necessary because any API using CBOR
    messages shares the |request_id| field which must be unique within a
    connection.
    
    Change-Id: I39ec730cbc951a8cbc2d7417f40f051738fb5f20

---
+++ api/public/endpoint_request_ids.cc
@@ -0,0 +1,20 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "api/public/endpoint_request_ids.h"
+
+namespace openscreen {
+
+EndpointRequestIds::EndpointRequestIds() = default;
+EndpointRequestIds::~EndpointRequestIds() = default;
+
+uint64_t EndpointRequestIds::GetNextRequestId(uint64_t endpoint_id) {
+  return ++request_ids_by_endpoint_id_[endpoint_id];
+}
+
+void EndpointRequestIds::ResetRequestId(uint64_t endpoint_id) {
+  request_ids_by_endpoint_id_.erase(endpoint_id);
+}
+
+}  // namespace openscreen
---
+++ api/public/endpoint_request_ids_unittest.cc
@@ -0,0 +1,38 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "api/public/endpoint_request_ids.h"
+
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+
+namespace openscreen {
+
+TEST(EndpointRequestIdsTest, StrictlyIncreasingRequestIdSequence) {
+  EndpointRequestIds request_ids;
+
+  EXPECT_EQ(1u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(2u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(3u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(1u, request_ids.GetNextRequestId(3));
+  EXPECT_EQ(4u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(2u, request_ids.GetNextRequestId(3));
+}
+
+TEST(EndpointRequestIdsTest, ResetRequestId) {
+  EndpointRequestIds request_ids;
+
+  EXPECT_EQ(1u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(2u, request_ids.GetNextRequestId(7));
+  request_ids.ResetRequestId(7);
+  EXPECT_EQ(1u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(2u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(1u, request_ids.GetNextRequestId(3));
+  EXPECT_EQ(2u, request_ids.GetNextRequestId(3));
+  request_ids.ResetRequestId(7);
+  EXPECT_EQ(1u, request_ids.GetNextRequestId(7));
+  EXPECT_EQ(2u, request_ids.GetNextRequestId(7));
+  request_ids.ResetRequestId(9);
+}
+
+}  // namespace openscreen
