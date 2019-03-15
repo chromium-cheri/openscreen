7e1302d10283e18a08a70f2d23998ea6c9307f58
Ryan Keane <rwkeane@google.com>
2019-03-14 17:17:05 -0700

    Run clang-format on generated CDDL code during build
    The changes here are to allow for safely calling both the CDDL generation tool
    and Clang-Format from a python stript. This approach was taken because:
    	1) Calling custom tooling (such as clang-format) cannot be done directly in
         gn.
    	2) To call Clang-format, a python script needs to be used. For this, an output
         file needs to be specified. The same output as in the existing action
         cannot be specified due to gn limitations, and using a 'fake' file causes
         rebuilds of the code to fail.
    	3) The script is already specialized to generate C++ source files from the
         CDDL spec. This just adds further functionality that should be invisible to
         the caller (in this case, the build system).
    
    Change-Id: Iba0c89b23186c02826edc422a0fc0d5ea92f8c39

--- tools/cddl/cddl.py
+++ tools/cddl/cddl.py
@@ -3,9 +3,74 @@
 # found in the LICENSE file.
 
 import os
+import subprocess
 import sys
 
 def main():
-    os.execv('./cddl', ['./cddl'] + sys.argv[1:])
+  header = ''
+  cc = ''
+  genDir = ''
+  cddlFile = ''
+
+  # Parse all setting values from the input parameters.
+  settingCount = 0
+  i = 1
+  while i < len(sys.argv):
+    settingCount += 1;
+    arg = sys.argv[i]
+    if arg == '--header':
+      i += 1
+      header = sys.argv[i]
+    elif arg == '--cc':
+      i += 1
+      cc = sys.argv[i]
+    elif arg == '--gen-dir':
+      i += 1
+      genDir = sys.argv[i]
+    else:
+      cddlFile = sys.argv[i]
+    i += 1
+
+  # Validate recieved parameters.
+  if settingCount != 4:
+    print("Error: 5 input parameters expected and instead received " +
+          str(settingCount))
+    sys.exit(1)
+
+  if not validateHeaderInput(header):
+    print("Error: '" + header + "' is not a valid .h file")
+    sys.exit(1)
+  elif not validateCodeInput(cc):
+    print("Error: '" + cc + "' is not a valid .cc file")
+    sys.exit(1)
+  elif not validatePathInput(genDir):
+    print("Error: '" + genDir + "' is not a valid output directory")
+    sys.exit(1)
+  elif not validateCddlInput(cddlFile):
+    print("Error: '" + cddlFile + "' is not a valid CDDL file")
+    sys.exit(1)
+
+  # Execute command line commands with these variables.
+  print('Creating C++ files from provided CDDL file...')
+  print('\tExecuting command: ./cddl --header ' + header + ' --cc ' +
+         cc + ' --gen-dir ' + genDir + ' cddlFile')
+  subprocess.Popen(['./cddl', "--header", header, "--cc", cc, "--gen-dir",
+                    genDir, cddlFile]).wait()
+  print('\tExecuting command: clang-format -i ' + genDir + '/' + header)
+  subprocess.Popen(['clang-format', "-i", genDir + "/" + header]).wait()
+  print('\tExecuting command: clang-format -i ' + genDir + '/' + cc)
+  subprocess.Popen(['clang-format', "-i", genDir + "/" + cc]).wait()
+
+def validateHeaderInput(headerFile):
+  return headerFile != '' and headerFile.endswith('.h')
+
+def validateCodeInput(ccFile):
+  return ccFile != '' and ccFile.endswith('.cc')
+
+def validatePathInput(dirPath):
+  return dirPath != '' and os.path.isdir(dirPath)
+
+def validateCddlInput(cddlFile):
+  return cddlFile != '' and os.path.isfile(cddlFile)
 
 main()
