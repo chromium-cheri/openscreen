ebcf95f7d9de8121ad12f3366f495ec610df44df
Ryan Keane <rwkeane@google.com>
2019-03-14 17:17:05 -0700

    Run clang-format on generated CDDL code during build
    The changes here are to allow for safely calling both the CDDL generation tool
    and Clang-Format from a python stript. This approach was taken because:
    	1) Calling custom tooling (such as clang-format) cannot be done directly in
         gn.
    	2) To call Clang-format, a python script needs to be used. For this, an output
         file needs to be specified. The same output as in the existing action
         cannot be specified due to gn limitations, and using a 'fake' file causes
         rebuilds of the code to fail.
    	3) The script is already specialized to generate C++ source files from the
         CDDL spec. This just adds further functionality that should be invisible to
         the caller (in this case, the build system).
    
    Change-Id: Iba0c89b23186c02826edc422a0fc0d5ea92f8c39

--- tools/cddl/cddl.py
+++ tools/cddl/cddl.py
@@ -3,9 +3,48 @@
 # found in the LICENSE file.
 
 import os
+import subprocess
 import sys
 
 def main():
-    os.execv('./cddl', ['./cddl'] + sys.argv[1:])
+  if len(sys.argv) != 5:
+    print("Error: 5 input variables expected and instead recieved "
+          + len(sys.argv))
+    sys.exit(1)
+
+  header = sys.argv[1]
+  cc = sys.argv[2]
+  genDir = sys.argv[3]
+  cddlFile = sys.argv[4]
+
+  if not validateHeaderInput(header):
+    print("Error: '" + header + "' is not a valid .h file")
+    sys.exit(1)
+  elif not validateCodeInput(cc):
+    print("Error: '" + cc + "' is not a valid .cc file")
+    sys.exit(1)
+  elif not validatePathInput(genDir):
+    print("Error: '" + genDir + "' is not a valid output directory")
+    sys.exit(1)
+  elif not validateCddlInput(cddlFile):
+    print("Error: '" + cddlFile + "' is not a valid CDDL file")
+    sys.exit(1)
+
+  subprocess.Popen(['./cddl', "--header", header, "--cc", cc, "--gen-dir",
+                    genDir, cddlFile]).wait()
+  subprocess.Popen(['clang-format', "-i", genDir + "/" + header]).wait()
+  subprocess.Popen(['clang-format', "-i", genDir + "/" + cc]).wait()
+
+def validateHeaderInput(headerFile):
+  return headerFile.endswith('.h')
+
+def validateCodeInput(ccFile):
+  return ccFile.endswith('.cc')
+
+def validatePathInput(dirPath):
+  return os.path.isdir(dirPath)
+
+def validateCddlInput(cddlFile):
+  return os.path.isfile(cddlFile)
 
 main()
