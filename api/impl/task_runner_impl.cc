f891305bec3b545ffcf2bf893c20196e086f7f26
Jordan Bayles <jophba@chromium.org>
2019-03-26 15:49:57 -0700

    Add TaskRunner implementation to Open Screen
    This patch contains an initial TaskRunner for use with Open Screen.
    This initial version does not contain a Chromium wrapper, nor does
    it permit cancelling. A unit test suite is included for this new
    class.
    
    Change-Id: I2404980acf3b40b78d9772925ebe11ed460cc0b6

---
+++ api/impl/task_runner_impl.cc
@@ -0,0 +1,85 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file
+
+#include "api/impl/task_runner_impl.h"
+
+#include "platform/api/logging.h"
+
+namespace openscreen {
+namespace platform {
+TaskRunnerImpl::TaskRunnerImpl(platform::ClockNowFunctionPtr now_function)
+    : now_function_(now_function), is_running_(false) {}
+
+TaskRunnerImpl::~TaskRunnerImpl() {
+  Stop();
+}
+
+void TaskRunnerImpl::PostTask(Task task) {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  tasks_.push_back(std::move(task));
+  run_loop_wakeup_.notify_all();
+}
+
+void TaskRunnerImpl::PostTaskWithDelay(Task task, Clock::duration delay) {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  delayed_tasks_.emplace(std::move(task), now_function_() + delay);
+  run_loop_wakeup_.notify_all();
+}
+
+void TaskRunnerImpl::Start() {
+  is_running_ = true;
+  RunTasksUntilStopped();
+}
+
+void TaskRunnerImpl::Stop() {
+  is_running_ = false;
+}
+
+void TaskRunnerImpl::RunUntilIdleForTesting() {
+  ScheduleDelayedTasks();
+  RunCurrentTasks();
+}
+
+bool TaskRunnerImpl::RunCurrentTasks() {
+  std::deque<Task> current_tasks;
+
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  tasks_.swap(current_tasks);
+  task_mutex_.unlock();
+
+  for (Task& task : current_tasks) {
+    task();
+  }
+
+  return !current_tasks.empty();
+}
+
+void TaskRunnerImpl::RunTasksUntilStopped() {
+  while (is_running_) {
+    ScheduleDelayedTasks();
+
+    if (!RunCurrentTasks()) {
+      std::unique_lock<std::mutex> lock(task_mutex_);
+
+      if (!delayed_tasks_.empty()) {
+        run_loop_wakeup_.wait_until(lock,
+                                    delayed_tasks_.top().time_runnable_after);
+      } else {
+        run_loop_wakeup_.wait(lock);
+      }
+    }
+  }
+}
+
+void TaskRunnerImpl::ScheduleDelayedTasks() {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+
+  while (!delayed_tasks_.empty() &&
+         (delayed_tasks_.top().time_runnable_after < now_function_())) {
+    tasks_.push_back(std::move(delayed_tasks_.top().task));
+    delayed_tasks_.pop();
+  }
+}
+}  // namespace platform
+}  // namespace openscreen
---
+++ api/impl/task_runner_unittest.cc
@@ -0,0 +1,138 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <thread>
+
+#include "api/impl/task_runner_impl.h"
+#include "api/public/task_runner_factory.h"
+#include "platform/api/time.h"
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+
+namespace {
+using openscreen::platform::Clock;
+using std::chrono::milliseconds;
+
+const auto kTaskRunnerSleepTime = milliseconds(1);
+
+void WaitUntilCondition(std::function<bool()> predicate) {
+  while (!predicate()) {
+    std::this_thread::sleep_for(kTaskRunnerSleepTime);
+  }
+}
+}  // anonymous namespace
+
+namespace openscreen {
+namespace platform {
+
+TEST(TaskRunnerTest, TaskRunnerFromFactoryExecutesTask) {
+  auto runner = TaskRunnerFactory::Create();
+
+  std::string ran_tasks = "";
+  const auto task = [&ran_tasks] { ran_tasks += "1"; };
+  EXPECT_EQ(ran_tasks, "");
+
+  runner->PostTask(task);
+
+  WaitUntilCondition([&ran_tasks] { return ran_tasks == "1"; });
+  EXPECT_EQ(ran_tasks, "1");
+}
+
+TEST(TaskRunnerTest, TaskRunnerRunsDelayedTasks) {
+  const auto kDelayTime = milliseconds(5);
+  auto runner = TaskRunnerFactory::Create();
+
+  std::string ran_tasks = "";
+  const auto task = [&ran_tasks] { ran_tasks += "1"; };
+
+  const auto kMinimumClockTaskShouldRunAt = platform::Clock::now() + kDelayTime;
+  runner->PostTaskWithDelay(task, kDelayTime);
+
+  while (platform::Clock::now() < kMinimumClockTaskShouldRunAt) {
+    EXPECT_EQ(ran_tasks, "");
+    std::this_thread::sleep_for(kTaskRunnerSleepTime);
+  }
+
+  WaitUntilCondition([&ran_tasks] { return ran_tasks == "1"; });
+  EXPECT_EQ(ran_tasks, "1");
+}
+
+TEST(TaskRunnerTest, SingleThreadedTaskRunnerRunsSequentially) {
+  TaskRunnerImpl runner(platform::Clock::now);
+
+  std::string ran_tasks;
+  const auto task_one = [&ran_tasks] { ran_tasks += "1"; };
+  const auto task_two = [&ran_tasks] { ran_tasks += "2"; };
+  const auto task_three = [&ran_tasks] { ran_tasks += "3"; };
+  const auto task_four = [&ran_tasks] { ran_tasks += "4"; };
+  const auto task_five = [&ran_tasks] { ran_tasks += "5"; };
+
+  runner.PostTask(task_one);
+  runner.PostTask(task_two);
+  runner.PostTask(task_three);
+  runner.PostTask(task_four);
+  runner.PostTask(task_five);
+  EXPECT_EQ(ran_tasks, "");
+
+  runner.RunUntilIdleForTesting();
+  EXPECT_EQ(ran_tasks, "12345");
+}
+
+TEST(TaskRunnerTest, TaskRunnerCanStopRunning) {
+  TaskRunnerImpl runner(platform::Clock::now);
+
+  std::string ran_tasks;
+  const auto task_one = [&ran_tasks] { ran_tasks += "1"; };
+  const auto task_two = [&ran_tasks] { ran_tasks += "2"; };
+
+  runner.PostTask(task_one);
+  EXPECT_EQ(ran_tasks, "");
+
+  std::thread start_thread([&runner] { runner.Start(); });
+
+  WaitUntilCondition([&ran_tasks] { return !ran_tasks.empty(); });
+  EXPECT_EQ(ran_tasks, "1");
+
+  // Since Stop is called first, and the single threaded task
+  // runner should honor the queue, we know the task runner is not running
+  // since task two doesn't get ran.
+  runner.Stop();
+  runner.PostTask(task_two);
+  EXPECT_EQ(ran_tasks, "1");
+
+  start_thread.join();
+}
+
+TEST(TaskRunnerTest, StoppingDoesNotDeleteTasks) {
+  TaskRunnerImpl runner(platform::Clock::now);
+
+  std::string ran_tasks;
+  const auto task_one = [&ran_tasks] { ran_tasks += "1"; };
+
+  runner.PostTask(task_one);
+  runner.Stop();
+
+  EXPECT_EQ(ran_tasks, "");
+  runner.RunUntilIdleForTesting();
+
+  EXPECT_EQ(ran_tasks, "1");
+}
+
+TEST(TaskRunnerTest, TaskRunnerIsStableWithLotsOfTasks) {
+  TaskRunnerImpl runner(platform::Clock::now);
+
+  const int kNumberOfTasks = 500;
+  std::string expected_ran_tasks;
+  expected_ran_tasks.append(kNumberOfTasks, '1');
+
+  std::string ran_tasks;
+  for (int i = 0; i < kNumberOfTasks; ++i) {
+    const auto task = [&ran_tasks] { ran_tasks += "1"; };
+    runner.PostTask(task);
+  }
+
+  runner.RunUntilIdleForTesting();
+  EXPECT_EQ(ran_tasks, expected_ran_tasks);
+}
+}  // namespace platform
+}  // namespace openscreen
