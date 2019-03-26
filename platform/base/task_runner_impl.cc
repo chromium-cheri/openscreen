55b2a3c97575961dad1429cc76ddc99aca138dff
Jordan Bayles <jophba@chromium.org>
2019-03-26 15:49:57 -0700

    Add TaskRunner implementation to Open Screen
    This patch contains an initial TaskRunner for use with Open Screen.
    This initial version does not contain a Chromium wrapper, nor does
    it permit cancelling. A unit test suite is included for this new
    class.
    
    Change-Id: I2404980acf3b40b78d9772925ebe11ed460cc0b6

---
+++ platform/base/task_runner_impl.cc
@@ -0,0 +1,81 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file
+
+#include "platform/base/task_runner_impl.h"
+
+#include "platform/api/logging.h"
+
+namespace openscreen {
+namespace platform {
+TaskRunnerImpl::TaskRunnerImpl(platform::ClockNowFunctionPtr now_function)
+    : now_function_(now_function) {}
+
+TaskRunnerImpl::~TaskRunnerImpl() {
+  Stop();
+}
+
+void TaskRunnerImpl::PostTask(Task task) {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  tasks_.push_back(std::move(task));
+}
+
+void TaskRunnerImpl::PostTaskWithDelay(Task task, Clock::duration delay) {
+  std::lock_guard<std::mutex> lock(delayed_task_mutex_);
+  delayed_tasks_.emplace_back(std::move(task), now_function_() + delay);
+}
+
+void TaskRunnerImpl::Start() {
+  has_stopped_ = false;
+  RunTasksUntilStopped();
+}
+
+void TaskRunnerImpl::Stop() {
+  has_stopped_ = true;
+}
+
+void TaskRunnerImpl::RunUntilIdleForTesting() {
+  ScheduleDelayedTasks();
+  RunCurrentTasks();
+}
+
+void TaskRunnerImpl::RunCurrentTasks() {
+  std::deque<Task> current_tasks;
+
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  tasks_.swap(current_tasks);
+  task_mutex_.unlock();
+
+  for (auto task : current_tasks) {
+    task();
+  }
+}
+
+void TaskRunnerImpl::RunTasksUntilStopped() {
+  while (!has_stopped_) {
+    ScheduleDelayedTasks();
+    RunCurrentTasks();
+  }
+}
+
+void TaskRunnerImpl::ScheduleDelayedTasks() {
+  const Clock::time_point now = Clock::now();
+
+  std::deque<Task> tasks_to_enqueue;
+  std::unique_lock<std::mutex> delayed_lock(delayed_task_mutex_);
+  auto it = delayed_tasks_.begin();
+  while (it != delayed_tasks_.end()) {
+    if (now >= it->second) {
+      tasks_to_enqueue.push_back(std::move(it->first));
+      it = delayed_tasks_.erase(it);
+    } else {
+      ++it;
+    }
+  }
+  delayed_lock.unlock();
+
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  tasks_.insert(tasks_.end(), tasks_to_enqueue.begin(), tasks_to_enqueue.end());
+}
+}  // namespace platform
+}  // namespace openscreen
