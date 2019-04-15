503e9efb7055872839e28e288cc9a1b8c1ca6a78
Max Yakimakha <yakimakha@chromium.org>
2019-04-15 15:40:25 -0700

    BoringSSL error printing
    Change-Id: I0a50a0065de7b5dbd1f7ec2e4fd6415ca7b91d7f

---
+++ base/boringssl_util.h
@@ -0,0 +1,20 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_BORINGSSL_UTIL_H_
+#define BASE_BORINGSSL_UTIL_H_
+
+#include <ostream>
+#include <string>
+#include <utility>
+
+//#include "base/macros.h"
+
+namespace openscreen {
+
+  void LogAndClearBoringSslErrors();
+
+}  // namespace openscreen
+
+#endif  // BASE_BORINGSSL_UTIL_H_
