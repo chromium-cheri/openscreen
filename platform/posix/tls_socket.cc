4318b2db6b9d0c6410e7440f1a6a974ebe6668e5
Jordan Bayles <jophba@chromium.org>
2019-05-30 14:10:02 -0700

    Implement TLSSocket class for POSIX
    This patch adds a TLSSocket implementation to Open Screen for POSIX,
    including some unit testing.
    
    Change-Id: I745b79aec5a6669c140b4c3d457866586e4c0c28

---
+++ platform/posix/tls_socket.cc
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/posix/tls_socket.h"
+
+namespace openscreen {
+namespace platform {
+namespace {}
+// static
+ErrorOr<TlsSocketUniquePtr> TlsSocket::Create(IPAddress::Version version) {
+  return Error::None();
+}
+
+void TlsSocket::Close(CloseReason reason) {
+  auto socket = TlsSocketPosix::From(this);
+}
+
+Error TlsSocket::Read() {
+  auto socket = TlsSocketPosix::From(this);
+  return Error::None();
+}
+
+Error TlsSocket::SendMessage(const TlsSocketMessage& message) {
+  auto socket = TlsSocketPosix::From(this);
+  return Error::None();
+}
+}  // namespace platform
+}  // namespace openscreen
