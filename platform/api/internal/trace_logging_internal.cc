c7adf5ebc29944e4b6406adb8e68d69b33307af5
Ryan Keane <rwkeane@google.com>
2019-06-27 15:09:10 -0700

    Create TraceLogging Framework
    Changes based on Design Doc: https://docs.google.com/document/d/1pmZXIULMzDIuQJAQaJg0-rKrJ-eJlTYf7TRZYCBNXmo/edit?usp=sharing
    - Create new TraceLogging Framework
    - Define Macros for improved perf
    - Define interface for platform-specific code
      - Add example interface implementation to be used in standalone mode
    - Create UTs for above
    - Integrate TraceLogging infra with existing TaskRunner and Informational Logging infras
    
    Change-Id: I728b5742116468c48ffc999144c3d339d2547795

---
+++ platform/api/internal/trace_logging_internal.cc
@@ -0,0 +1,140 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/internal/trace_logging_internal.h"
+
+#include "absl/types/optional.h"
+
+namespace openscreen {
+namespace platform {
+namespace internal {
+
+ScopedTraceOperation::ScopedTraceOperation(TraceId trace_id,
+                                           TraceId parent_id,
+                                           TraceId root_id) {
+  // Setting trace id fields.
+  root_id_ = root_id != kUnsetTraceId ? root_id : traces_.top()->root_id_;
+  parent_id_ =
+      parent_id != kUnsetTraceId ? parent_id : traces_.top()->trace_id_;
+  trace_id_ =
+      trace_id != kUnsetTraceId ? trace_id : trace_id_counter_.fetch_add(1);
+
+  // Add this item to the stack.
+  traces_.push(this);
+}
+
+ScopedTraceOperation::~ScopedTraceOperation() {
+  traces_.pop();
+}
+
+// static
+TraceId ScopedTraceOperation::GetCurrentId() {
+  return traces_.top()->trace_id_;
+}
+
+// static
+TraceId ScopedTraceOperation::GetRootId() {
+  return traces_.top()->root_id_;
+}
+
+// static
+TraceIdHierarchy ScopedTraceOperation::GetHierarchy() {
+  return {traces_.top()->trace_id_, traces_.top()->parent_id_,
+          traces_.top()->root_id_};
+}
+
+// static
+bool ScopedTraceOperation::SetResult(Error error) {
+  return traces_.top()->SetTraceResult(error);
+}
+
+// Suppress exit time destructor warning, since we require a stack initialized
+// with a TraceId for perf reasons. In practice, deconstructing these 2 objects
+// should take minimal time, so the benefits of using them outweigh the costs.
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wexit-time-destructors"
+#endif
+
+// static
+thread_local TraceIdSetter ScopedTraceOperation::root_node_{
+    TraceIdHierarchy::Empty()};
+
+// static
+thread_local ScopedTraceOperation::TraceStack ScopedTraceOperation::traces_{
+    std::vector<ScopedTraceOperation*>{&ScopedTraceOperation::root_node_}};
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+
+// static
+std::atomic_uint64_t ScopedTraceOperation::trace_id_counter_{
+    uint64_t{0x01} << (sizeof(TraceId) * 8 - 1)};
+
+TraceLoggerBase::TraceLoggerBase(const char* name,
+                                 const char* file,
+                                 uint32_t line,
+                                 TraceCategory::Value category,
+                                 TraceId current,
+                                 TraceId parent,
+                                 TraceId root)
+    : ScopedTraceOperation(current, parent, root),
+      start_time_(Clock::now()),
+      result_(Error::Code::kNone),
+      name_(name),
+      file_name_(file),
+      line_number_(line),
+      category_(category) {}
+
+TraceLoggerBase::TraceLoggerBase(const char* name,
+                                 const char* file,
+                                 uint32_t line,
+                                 TraceCategory::Value category,
+                                 TraceIdHierarchy ids)
+    : TraceLoggerBase(name,
+                      file,
+                      line,
+                      category,
+                      ids.current,
+                      ids.parent,
+                      ids.root) {}
+
+bool TraceLoggerBase::SetTraceResult(Error error) {
+  result_ = error;
+  return true;
+}
+
+SynchronousTraceLogger::~SynchronousTraceLogger() {
+  // If this object has an instance variable platform, use that. Otherwise,
+  // use the static variable for the shared class. In practice, the instance
+  // variable should only be set when testing, so branch prediction will
+  // always pick the correct path in production code and it should be of
+  // negligable cost.
+  auto* current_platform = this->platform_override_ != nullptr
+                               ? this->platform_override_
+                               : TraceBase::default_trace_platform_;
+  auto end_time = Clock::now();
+  current_platform->LogTrace(this->name_, this->line_number_, this->file_name_,
+                             this->start_time_, end_time, this->trace_id_,
+                             this->parent_id_, this->root_id_, this->result_);
+}
+
+AsynchronousTraceLogger::~AsynchronousTraceLogger() {
+  // If this object has an instance variable platform, use that. Otherwise,
+  // use the static variable for the shared class. In practice, the instance
+  // variable should only be set when testing, so branch prediction will
+  // always pick the correct path in production code and it should be of
+  // negligable cost.
+  auto* current_platform = this->platform_override_ != nullptr
+                               ? this->platform_override_
+                               : TraceBase::default_trace_platform_;
+  current_platform->LogAsyncStart(
+      this->name_, this->line_number_, this->file_name_, this->start_time_,
+      this->trace_id_, this->parent_id_, this->root_id_);
+}
+
+}  // namespace internal
+}  // namespace platform
+}  // namespace openscreen
---
+++ platform/api/internal/trace_logging_internal.h
@@ -0,0 +1,186 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PLATFORM_API_INTERNAL_TRACE_LOGGING_INTERNAL_H_
+#define PLATFORM_API_INTERNAL_TRACE_LOGGING_INTERNAL_H_
+
+#include <atomic>
+#include <cstring>
+#include <stack>
+#include <vector>
+
+#include "osp_base/error.h"
+#include "platform/api/time.h"
+#include "platform/api/trace_logging_platform.h"
+#include "platform/api/trace_logging_types.h"
+
+namespace openscreen {
+namespace platform {
+namespace internal {
+
+// Forward declaration so that it can be used in below classes.
+class TraceIdSetter;
+
+// Base class needed for macro calls. It has been intentionally left with
+// no instance variables so that construction is simple and fast.
+class TraceBase {
+ public:
+  TraceBase() = default;
+  virtual ~TraceBase() = default;
+
+  // Traces the end of an asynchronous call.
+  // NOTE: This cannot be a void due to Macro behavior above, as the
+  // ternary operator requires a return value.
+  inline bool TraceAsyncEnd(const uint32_t line,
+                            const char* file,
+                            TraceId id,
+                            Error e) {
+    auto end_time = Clock::now();
+    default_trace_platform_->LogAsyncEnd(line, file, end_time, id, e);
+    return true;
+  }
+
+ protected:
+  // For now, only support one TraceLoggingPlatform. In future, we could add
+  // support for mupltple (ie: one to log to Chromium/etc, one to go to SQL, one
+  // for Metrics, etc).
+  static TraceLoggingPlatform* default_trace_platform_;
+};
+
+// A base class for TraceLogging to allow for:
+// 1) The sharing of all static and thread_local variables across template
+// specializations.
+// 2) Putting all template specializations into the same traces vector.
+class ScopedTraceOperation : public TraceBase {
+ public:
+  // Define the destructor to remove this item from the stack when it's
+  // destroyed.
+  ~ScopedTraceOperation() override;
+
+  // Getters the current Trace Hierarchy.
+  static TraceId GetCurrentId();
+  static TraceId GetRootId();
+  static TraceIdHierarchy GetHierarchy();
+
+  // Static method to set the result of the most recent trace.
+  static bool SetResult(Error error);
+
+  // Sets the result of this trace log.
+  // NOTE: this must be define in this class rather than TraceLogger so that it
+  // can be called on traces.back() without a potentially unsafe cast or type
+  // checking at runtime.
+  virtual bool SetTraceResult(Error error) = 0;
+
+ protected:
+  // Constructor to set all trace id information.
+  ScopedTraceOperation(TraceId current_id = kUnsetTraceId,
+                       TraceId parent_id = kUnsetTraceId,
+                       TraceId root_id = kUnsetTraceId);
+
+  // Current TraceId information.
+  TraceId trace_id_;
+  TraceId root_id_;
+  TraceId parent_id_;
+
+ private:
+  using TraceStack =
+      std::stack<ScopedTraceOperation*, std::vector<ScopedTraceOperation*>>;
+
+  // Counter to pick IDs when it is not provided.
+  static std::atomic_uint64_t trace_id_counter_;
+
+  // The LIFO stack of TraceLoggers currently being watched by this
+  // thread.
+  static thread_local TraceStack traces_;
+  static thread_local TraceIdSetter root_node_;
+
+  OSP_DISALLOW_COPY_AND_ASSIGN(ScopedTraceOperation);
+};
+
+// The class which does actual trace logging.
+class TraceLoggerBase : public ScopedTraceOperation {
+ public:
+  TraceLoggerBase(const char* name,
+                  const char* file,
+                  uint32_t line,
+                  TraceCategory::Value category,
+                  TraceId current = kUnsetTraceId,
+                  TraceId parent = kUnsetTraceId,
+                  TraceId root = kUnsetTraceId);
+
+  TraceLoggerBase(const char* name,
+                  const char* file,
+                  uint32_t line,
+                  TraceCategory::Value category,
+                  TraceIdHierarchy ids);
+
+  // Set the result.
+  bool SetTraceResult(Error error);
+
+ protected:
+  // Timestamp for when the object was created.
+  Clock::time_point start_time_;
+
+  // Result of this operation.
+  Error result_;
+
+  // Name of this operation.
+  const char* name_;
+
+  // Name of the file.
+  const char* file_name_;
+
+  // Line number the log was generated from.
+  uint32_t line_number_;
+
+  // Category of this trace log.
+  TraceCategory::Value category_;
+
+  // Trace logging layer to use for this trace log. It is overridden for
+  // testing purposes, but in production code is always set to nullptr.
+  TraceLoggingPlatform* platform_override_ = nullptr;
+
+ private:
+  OSP_DISALLOW_COPY_AND_ASSIGN(TraceLoggerBase);
+};
+
+class SynchronousTraceLogger : public TraceLoggerBase {
+ public:
+  using TraceLoggerBase::TraceLoggerBase;
+
+  virtual ~SynchronousTraceLogger() override;
+
+ private:
+  OSP_DISALLOW_COPY_AND_ASSIGN(SynchronousTraceLogger);
+};
+
+class AsynchronousTraceLogger : public TraceLoggerBase {
+ public:
+  using TraceLoggerBase::TraceLoggerBase;
+
+  virtual ~AsynchronousTraceLogger() override;
+
+ private:
+  OSP_DISALLOW_COPY_AND_ASSIGN(AsynchronousTraceLogger);
+};
+
+// Inserts a fake element into the ScopedTraceOperation stack to set
+// the current TraceId Hierarchy manually.
+class TraceIdSetter : public ScopedTraceOperation {
+ public:
+  explicit TraceIdSetter(TraceIdHierarchy ids)
+      : ScopedTraceOperation(ids.current, ids.parent, ids.root) {}
+
+  // Implement abstract method for use in Macros.
+  bool SetTraceResult(Error error) { return false; }
+
+ private:
+  OSP_DISALLOW_COPY_AND_ASSIGN(TraceIdSetter);
+};
+
+}  // namespace internal
+}  // namespace platform
+}  // namespace openscreen
+
+#endif  // PLATFORM_API_INTERNAL_TRACE_LOGGING_INTERNAL_H_
