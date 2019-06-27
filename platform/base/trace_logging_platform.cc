3207f4c3f070f3758344d48290902d9fcd496817
Ryan Keane <rwkeane@google.com>
2019-06-27 10:03:25 -0700

    Create TraceLogging Framework
    Changes based on Design Doc: https://docs.google.com/document/d/1pmZXIULMzDIuQJAQaJg0-rKrJ-eJlTYf7TRZYCBNXmo/edit?usp=sharing
    - Create new TraceLogging Framework
    - Define Macros for improved perf
    - Define interface for platform-specific code
      - Add example interface implementation to be used in standalone mode
    - Create UTs for above
    - Integrate TraceLogging infra with existing TaskRunner and Informational Logging infras
    
    Change-Id: I728b5742116468c48ffc999144c3d339d2547795

---
+++ platform/base/trace_logging_platform.cc
@@ -0,0 +1,73 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/logging.h"
+#include "platform/api/trace_logging.h"
+#include "platform/api/trace_logging_types.h"
+
+namespace openscreen {
+namespace platform {
+
+// clang-format off
+bool IsLoggingEnabled(TraceCategory::Value category) {
+  #ifndef NDEBUG
+      constexpr uint64_t kAllLogCategoriesMask =
+          std::numeric_limits<uint64_t>::max();
+      return (kAllLogCategoriesMask & category) != 0;
+  #else
+      return false;
+  #endif
+}
+// clang-format on
+
+class TextTraceLoggingPlatform : public TraceLoggingPlatform {
+ public:
+  TextTraceLoggingPlatform() = default;
+  ~TextTraceLoggingPlatform() = default;
+
+  void LogTrace(const char* name,
+                const uint32_t line,
+                const char* file,
+                Clock::time_point start_time,
+                Clock::time_point end_time,
+                TraceId trace_id,
+                TraceId parent_id,
+                TraceId root_id,
+                Error error) {
+    auto total_runtime = std::chrono::duration_cast<std::chrono::microseconds>(
+                             end_time - start_time)
+                             .count();
+    OSP_LOG << "TRACE [" << std::hex << root_id << ":" << parent_id << ":"
+            << trace_id << "] (" << std::dec << total_runtime << "\u03BC"
+            << "s"
+            << ") " << name << "<" << file << ":" << line << "> "
+            << error.code();
+  }
+
+  void LogAsyncStart(const char* name,
+                     const uint32_t line,
+                     const char* file,
+                     Clock::time_point timestamp,
+                     TraceId trace_id,
+                     TraceId parent_id,
+                     TraceId root_id) {
+    OSP_LOG << "ASYNC TRACE START [" << std::hex << root_id << ":" << parent_id
+            << ":" << trace_id << std::dec << "] (" << timestamp << ") " << name
+            << "<" << file << ":" << line << ">";
+  }
+
+  void LogAsyncEnd(const uint32_t line,
+                   const char* file,
+                   Clock::time_point timestamp,
+                   TraceId trace_id,
+                   Error error) {
+    OSP_LOG << "ASYNC TRACE END [" << std::hex << trace_id << std::dec << "] ("
+            << timestamp << ") " << error.code();
+  }
+};
+
+TRACE_SET_DEFAULT_PLATFORM(new TextTraceLoggingPlatform);
+
+}  // namespace platform
+}  // namespace openscreen
