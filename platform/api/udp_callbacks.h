3f4fe3dc35043a00217d0afabfb945065206f5ce
Ryan Keane <rwkeane@google.com>
2019-06-28 17:12:39 -0700

    Networking Changes Pt 1
    This is the first set of changes making up CL:
    https://chromium-review.googlesource.com/c/openscreen/+/1654330
    
    Note that EventWaiter contains both the new and old implementations in this
    change, allowing for a smaller patch set.
    
    Primary changes are:
    1) Rework EventWaiter to only be platform-specific methods
    2) Add NetworkLoop class to handle non-platform-specific parts of network calls
    and handle callbacks
    3) Add NetworkRunner to handle interactions between NetworkLoop and TaskRunner
    
    Change-Id: Ied65cc5e5c6d1f1c013f1e2d0999f5c29ca7d9ac

--- platform/api/udp_callbacks.h
+++ platform/api/udp_callbacks.h
@@ -22,16 +22,20 @@ static constexpr int kUdpMaxPacketSize = 1 << 16;
 class UdpReadCallback {
  public:
   struct Packet : std::array<uint8_t, kUdpMaxPacketSize> {
-    Packet();
-    ~Packet();
+    Packet() = default;
+    ~Packet() = default;
 
     IPEndpoint source;
     IPEndpoint original_destination;
-    ssize_t length;
+    size_t length;
     // TODO(btolsch): When this gets to implementation, make sure the callback
     // is never called with a |socket| that could have been destroyed (e.g.
     // between queueing the read data and running the task).
     UdpSocket* socket;
+
+    // Override the default implementation since we want the number of elements,
+    // not kUdpMaxPacketSize.
+    size_t size() const { return length; }
   };
 
   virtual ~UdpReadCallback() = default;
