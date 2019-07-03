faabae6f94a9533ac50d73157de35f42e8681c5e
Ryan Keane <rwkeane@google.com>
2019-07-03 11:26:51 -0700

    Create TraceLogging Framework
    Changes based on Design Doc: https://docs.google.com/document/d/1pmZXIULMzDIuQJAQaJg0-rKrJ-eJlTYf7TRZYCBNXmo/edit?usp=sharing
    - Create new TraceLogging Framework
    - Define Macros for improved perf
    - Define interface for platform-specific code
      - Add example interface implementation to be used in standalone mode
    - Create UTs for above
    - Integrate TraceLogging infra with existing TaskRunner and Informational Logging infras
    
    Change-Id: I728b5742116468c48ffc999144c3d339d2547795

---
+++ platform/api/trace_logging.h
@@ -0,0 +1,117 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PLATFORM_API_TRACE_LOGGING_H_
+#define PLATFORM_API_TRACE_LOGGING_H_
+
+#include "platform/api/internal/trace_logging_internal.h"
+#include "platform/api/trace_logging_types.h"
+
+namespace openscreen {
+
+// Helper macros. These are used to simplify the macros below.
+// NOTE: These cannot be #undef'd or they will stop working outside this file.
+// NOTE: Two of these below macros are intentionally the same. This is to work
+// around optimizations in the C++ Precompiler.
+#define TRACE_INTERNAL_CONCAT(a, b) a##b
+#define TRACE_INTERNAL_CONCAT_DUP(a, b) TRACE_INTERNAL_CONCAT(a, b)
+#define TRACE_INTERNAL_UNIQUE_VAR_NAME(a) TRACE_INTERNAL_CONCAT_DUP(a, __LINE__)
+
+// Because we need to suppress unused variables, and this code is used
+// repeatedly in below macros, define helper macros to do this on a per-compiler
+// basis until we begin using C++ 17 which supports [[maybe_unused]] officially.
+#if defined(__clang__)
+#define TRACE_INTERNAL_IGNORE_UNUSED_VAR [[maybe_unused]]
+#elif defined(__GNUC__)
+#define TRACE_INTERNAL_IGNORE_UNUSED_VAR __attribute__((unused))
+#else
+#define TRACE_INTERNAL_IGNORE_UNUSED_VAR [[maybe_unused]]
+#endif  // defined(__clang__)
+
+#if defined(__clang__) && defined(__clang_major__) && __clang_major__ < 4
+#define TRACE_INTERNAL_IS_OUTDATED_COMPILER true
+#else
+#define TRACE_INTERNAL_IS_OUTDATED_COMPILER false
+#endif
+
+// All compile-time macros for tracing.
+// NOTE: The ternary operator is used here to ensure that the TraceLogger object
+// is only constructed if tracing is enabled, but at the same time is created in
+// the caller's scope. The C++ standards guide guarantees that the constructor
+// should only be called when IsTraceLoggingEnabled(...) evaluates to true.
+// static_cast calls are used because if the type of the result of the ternary
+// operator does not match the expected type, temporary storage is used for the
+// created object, which results in an extra call to the constructor and
+// destructor of the tracing objects.
+// The clang specific sections are because old versions of clang used on mac
+// don't correctly handle the ternary operator in some cases, so a workaround
+// is needed.
+//
+// Further details about how these macros are used can be found in
+// docs/trace_logging.md.
+// TODO(rwkeane): Add option to compile these macros out entirely.
+// TODO(rwkeane): Add support for user-provided properties.
+#define TRACE_SET_RESULT(result)                                               \
+  do {                                                                         \
+    if (openscreen::platform::IsTraceLoggingEnabled(                           \
+            TraceCategory::Value::Any)) {                                      \
+      openscreen::platform::internal::ScopedTraceOperation::SetResult(result); \
+    }                                                                          \
+  } while (false)
+#define TRACE_SET_HIERARCHY(ids)                                               \
+  TRACE_INTERNAL_IGNORE_UNUSED_VAR                                             \
+  const openscreen::platform::internal::TraceBase&                             \
+      TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref_) =                             \
+          openscreen::platform::IsTraceLoggingEnabled(                         \
+              TraceCategory::Value::Any)                                       \
+              ? static_cast<const openscreen::platform::internal::TraceBase&>( \
+                    openscreen::platform::internal::TraceIdSetter(ids))        \
+              : static_cast<const openscreen::platform::internal::TraceBase&>( \
+                    openscreen::platform::internal::TraceBase())
+#define TRACE_HIERARCHY                                                       \
+  (openscreen::platform::IsTraceLoggingEnabled(TraceCategory::Value::Any)     \
+       ? openscreen::platform::internal::ScopedTraceOperation::GetHierarchy() \
+       : TraceIdHierarchy::Empty())
+#define TRACE_CURRENT_ID                                                      \
+  (openscreen::platform::IsTraceLoggingEnabled(TraceCategory::Value::Any)     \
+       ? openscreen::platform::internal::ScopedTraceOperation::GetCurrentId() \
+       : kEmptyTraceId)
+#define TRACE_ROOT_ID                                                      \
+  (openscreen::platform::IsTraceLoggingEnabled(TraceCategory::Value::Any)  \
+       ? openscreen::platform::internal::ScopedTraceOperation::GetRootId() \
+       : kEmptyTraceId)
+
+// Synchronous Trace Macro.
+#define TRACE_SCOPED(category, name, ...)                                      \
+  TRACE_INTERNAL_IGNORE_UNUSED_VAR                                             \
+  const openscreen::platform::internal::TraceBase&                             \
+      TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref_) =                             \
+          openscreen::platform::IsTraceLoggingEnabled(category)                \
+              ? static_cast<const openscreen::platform::internal::TraceBase&>( \
+                    openscreen::platform::internal::SynchronousTraceLogger{    \
+                        category, name, __FILE__, __LINE__, ##__VA_ARGS__})    \
+              : static_cast<const openscreen::platform::internal::TraceBase&>( \
+                    openscreen::platform::internal::TraceBase())
+
+// Asynchronous Trace Macros.
+#define TRACE_ASYNC_START(category, name, ...)                                 \
+  TRACE_INTERNAL_IGNORE_UNUSED_VAR                                             \
+  const openscreen::platform::internal::TraceBase&                             \
+      TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref_) =                             \
+          openscreen::platform::IsTraceLoggingEnabled(category)                \
+              ? static_cast<const openscreen::platform::internal::TraceBase&>( \
+                    openscreen::platform::internal::AsynchronousTraceLogger{   \
+                        category, name, __FILE__, __LINE__, ##__VA_ARGS__})    \
+              : static_cast<const openscreen::platform::internal::TraceBase&>( \
+                    openscreen::platform::internal::TraceBase())
+
+#define TRACE_ASYNC_END(category, id, result)                     \
+  openscreen::platform::IsTraceLoggingEnabled(category)           \
+      ? openscreen::platform::internal::TraceBase::TraceAsyncEnd( \
+            __LINE__, __FILE__, id, result)                       \
+      : false
+
+}  // namespace openscreen
+
+#endif  // PLATFORM_API_TRACE_LOGGING_H_
