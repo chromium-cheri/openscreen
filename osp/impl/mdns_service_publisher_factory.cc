2cdec9e99de8d4f765a5c7eb69ebc019a0cf8a30
Ryan Keane <rwkeane@google.com>
2019-07-18 11:44:35 -0700

    Use new Networking Model
    Swap out the old networking model for the new one which uses the Network
    Runner
    
    Currently, the demo does not fully work
    I plan to fix this before committing it, but there are issues on the
    master branch that I must fix first
    
    Change-Id: Ic01d8f61bd61eeba227d88a01645a53fbf157e90

--- osp/impl/mdns_service_publisher_factory.cc
+++ osp/impl/mdns_service_publisher_factory.cc
@@ -5,14 +5,16 @@
 #include "osp/public/mdns_service_publisher_factory.h"
 
 #include "osp/impl/internal_services.h"
+#include "platform/api/network_runner.h"
 
 namespace openscreen {
 
 // static
 std::unique_ptr<ServicePublisher> MdnsServicePublisherFactory::Create(
     const ServicePublisher::Config& config,
-    ServicePublisher::Observer* observer) {
-  return InternalServices::CreatePublisher(config, observer);
+    ServicePublisher::Observer* observer,
+    platform::NetworkRunner* network_runner) {
+  return InternalServices::CreatePublisher(config, observer, network_runner);
 }
 
 }  // namespace openscreen
--- osp/impl/quic/quic_server.cc
+++ osp/impl/quic/quic_server.cc
@@ -4,9 +4,12 @@
 
 #include "osp/impl/quic/quic_server.h"
 
+#include <functional>
 #include <memory>
 
+#include "absl/types/optional.h"
 #include "platform/api/logging.h"
+#include "platform/api/time.h"
 
 namespace openscreen {
 
@@ -17,7 +20,10 @@ QuicServer::QuicServer(
     ProtocolConnectionServer::Observer* observer)
     : ProtocolConnectionServer(demuxer, observer),
       connection_endpoints_(config.connection_endpoints),
-      connection_factory_(std::move(connection_factory)) {}
+      connection_factory_(std::move(connection_factory)) {
+  connection_factory_->ScheduleCleanUp(
+      std::bind(&QuicServer::CleanConnections, this));
+}
 
 QuicServer::~QuicServer() {
   CloseAllConnections();
@@ -59,9 +65,7 @@ bool QuicServer::Resume() {
   return true;
 }
 
-void QuicServer::RunTasks() {
-  if (state_ == State::kRunning)
-    connection_factory_->RunTasks();
+absl::optional<platform::Clock::duration> QuicServer::CleanConnections() {
   for (auto& entry : connections_)
     entry.second.delegate->DestroyClosedStreams();
 
@@ -69,15 +73,19 @@ void QuicServer::RunTasks() {
     connections_.erase(entry);
 
   delete_connections_.clear();
+
+  return std::chrono::milliseconds(state_ != State::kStopped ? 500 : 0);
 }
 
 std::unique_ptr<ProtocolConnection> QuicServer::CreateProtocolConnection(
     uint64_t endpoint_id) {
-  if (state_ != State::kRunning)
+  if (state_ != State::kRunning) {
     return nullptr;
+  }
   auto connection_entry = connections_.find(endpoint_id);
-  if (connection_entry == connections_.end())
+  if (connection_entry == connections_.end()) {
     return nullptr;
+  }
   return QuicProtocolConnection::FromExisting(
       this, connection_entry->second.connection.get(),
       connection_entry->second.delegate.get(), endpoint_id);
