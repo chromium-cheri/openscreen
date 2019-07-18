fcfa62e88dab090631dace157b936b949ada0fc0
Jordan Bayles <jophba@chromium.org>
2019-07-18 13:56:02 -0700

    Fixup saturate_cast signed compares
    Currently, Open Screen doesn't build in Chromium due to signed/unsigned
    comparisons being caught by -Wsign-compare. This patch fixes up the
    new code that has broken the build.
    
    Unfortunately, in order to compile abseil we have to have the
    -Wno-sign-compare flag added, meaning that any part of the library that
    uses it misses this flag.
    
    Change-Id: Id6f259211833a81ddaf759315be4778afe07aca7

--- util/saturate_cast.h
+++ util/saturate_cast.h
@@ -24,29 +24,37 @@ constexpr To saturate_cast(From from) {
   // any int32_t compared with a uint32_t will NOT promote to a int64_t↔int64_t
   // comparison. Instead, it will become a uint32_t↔uint32_t comparison (!),
   // which will sometimes produce invalid results.
+
+  constexpr auto to_max = std::numeric_limits<To>::max();
+  constexpr auto from_max = std::numeric_limits<From>::max();
+  constexpr auto to_min = std::numeric_limits<To>::min();
+  constexpr auto from_min = std::numeric_limits<From>::min();
+
   if (std::numeric_limits<From>::is_signed ==
       std::numeric_limits<To>::is_signed) {
     // Case 1: "From" and "To" are either both signed, or are both unsigned. In
     // this case, the smaller of the two types will be promoted to match the
     // larger's size, and a valid comparison will be made.
-    if (from <= std::numeric_limits<To>::min()) {
-      return std::numeric_limits<To>::min();
+    if ((from_min <= to_min) && (from <= static_cast<From>(to_min))) {
+      return to_min;
     }
-    if (from >= std::numeric_limits<To>::max()) {
-      return std::numeric_limits<To>::max();
+
+    if ((from_max >= to_max) && (from >= static_cast<From>(to_max))) {
+      return to_max;
     }
   } else if (std::numeric_limits<From>::is_signed) {
     // Case 2: "From" is signed, but "To" is unsigned.
     if (from <= From{0}) {
       return To{0};
     }
-    if (from >= std::numeric_limits<To>::max()) {
-      return std::numeric_limits<To>::max();
+
+    if ((from_max >= to_max) && (from >= static_cast<From>(to_max))) {
+      return to_max;
     }
   } else {
     // Case 3: "From" is unsigned, but "To" is signed.
-    if (from >= std::numeric_limits<To>::max()) {
-      return std::numeric_limits<To>::max();
+    if ((from_max >= to_max) && (from >= static_cast<From>(to_max))) {
+      return to_max;
     }
     // Note: "From" can never be less than "To's" minimum value.
   }
