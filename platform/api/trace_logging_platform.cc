be1d31e86ac04ba32f3ac8f7797ca864c063fb3b
Ryan Keane <rwkeane@google.com>
2019-07-18 17:39:43 -0700

    Create TraceLogging Framework
    Changes based on Design Doc: https://docs.google.com/document/d/1pmZXIULMzDIuQJAQaJg0-rKrJ-eJlTYf7TRZYCBNXmo/edit?usp=sharing
    - Create new TraceLogging Framework
    - Define Macros for improved perf
    - Define interface for platform-specific code
      - Add example interface implementation to be used in standalone mode
    - Create UTs for above
    - Integrate TraceLogging infra with existing TaskRunner and Informational Logging infras
    
    Change-Id: I728b5742116468c48ffc999144c3d339d2547795

---
+++ platform/api/trace_logging_platform.cc
@@ -0,0 +1,31 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/trace_logging_platform.h"
+
+#include "platform/api/logging.h"
+
+namespace openscreen {
+namespace platform {
+
+TraceLoggingPlatform::~TraceLoggingPlatform() = default;
+
+// static
+TraceLoggingPlatform* TraceLoggingPlatform::GetDefaultTracingPlatform() {
+  return default_platform_;
+}
+
+// static
+void TraceLoggingPlatform::SetDefaultTraceLoggingPlatform(
+    TraceLoggingPlatform* platform) {
+  OSP_DCHECK(default_platform_ == nullptr)
+      << "Default tracing platform already assigned";
+  default_platform_ = platform;
+}
+
+// static
+TraceLoggingPlatform* TraceLoggingPlatform::default_platform_ = nullptr;
+
+}  // namespace platform
+}  // namespace openscreen
