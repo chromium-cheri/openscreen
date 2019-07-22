dbfb7c1d3983b5ee3904dd82b5e314d9fa4550f3
Ryan Keane <rwkeane@google.com>
2019-07-22 12:05:33 -0700

    IGNORE: Test Buildbots Pt2
    Hitting issues with buiildbots that aren't showing up on local tests.
    Trying to confirm the issues are on my side and not in master.
    
    Change-Id: I11e0b61781cfac83423ddb029e8529b4091ea20a

---
+++ platform/api/trace_logging_platform.cc
@@ -0,0 +1,29 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/trace_logging_platform.h"
+
+#include "platform/api/logging.h"
+
+namespace openscreen {
+namespace platform {
+
+TraceLoggingPlatform::~TraceLoggingPlatform() = default;
+
+// static
+TraceLoggingPlatform* TraceLoggingPlatform::GetDefaultTracingPlatform() {
+  return default_platform;
+}
+
+// static
+void TraceLoggingPlatform::SetDefaultTraceLoggingPlatform(
+    TraceLoggingPlatform* platform) {
+  default_platform = platform;
+}
+
+// static
+TraceLoggingPlatform* TraceLoggingPlatform::default_platform = nullptr;
+
+}  // namespace platform
+}  // namespace openscreen
