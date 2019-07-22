255677193f60978c8f19ddd2a1cbecb09f9bf4d6
Ryan Keane <rwkeane@google.com>
2019-07-22 10:17:10 -0700

    IGNORE: Test Buildbots Pt2
    Hitting issues with buiildbots that aren't showing up on local tests.
    Trying to confirm the issues are on my side and not in master.
    
    Change-Id: I11e0b61781cfac83423ddb029e8529b4091ea20a

---
+++ platform/api/internal/trace_logging_internal.cc
@@ -0,0 +1,130 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/internal/trace_logging_internal.h"
+
+#include "absl/types/optional.h"
+#include "platform/api/logging.h"
+
+namespace openscreen {
+namespace platform {
+namespace internal {
+
+ScopedTraceOperation::ScopedTraceOperation(TraceId trace_id,
+                                           TraceId parent_id,
+                                           TraceId root_id,
+                                           bool push_to_stack) {
+  // Setting trace id fields.
+  root_id_ =
+      root_id != kUnsetTraceId ? root_id : GetTraceStack()->top()->root_id_;
+  parent_id_ = parent_id != kUnsetTraceId ? parent_id
+                                          : GetTraceStack()->top()->trace_id_;
+  trace_id_ =
+      trace_id != kUnsetTraceId ? trace_id : trace_id_counter_.fetch_add(1);
+
+  // Add this item to the stack.
+  if (push_to_stack) {
+    GetTraceStack()->push(this);
+    OSP_DCHECK(GetTraceStack()->size() < 1024);
+  }
+}
+
+ScopedTraceOperation::~ScopedTraceOperation() {
+  GetTraceStack()->pop();
+}
+
+// Suppress exit time destructor warning, since we require a stack initialized
+// with a TraceId for perf reasons. In practice, deconstructing these 2 objects
+// should take minimal time, so the benefits of using them outweigh the costs.
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wexit-time-destructors"
+#endif
+
+// static
+ScopedTraceOperation::TraceStack* ScopedTraceOperation::GetTraceStack() {
+  static thread_local std::unique_ptr<TraceIdSetter> root_node(
+      TraceIdSetter::CreateStackRootNode());
+  static thread_local ScopedTraceOperation::TraceStack traces{
+      {root_node.get()}};
+  return &traces;
+}
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+
+// static
+std::atomic<std::uint64_t> ScopedTraceOperation::trace_id_counter_{
+    uint64_t{0x01} << (sizeof(TraceId) * 8 - 1)};
+
+TraceLoggerBase::TraceLoggerBase(TraceCategory::Value category,
+                                 const char* name,
+                                 const char* file,
+                                 uint32_t line,
+                                 TraceId current,
+                                 TraceId parent,
+                                 TraceId root)
+    : ScopedTraceOperation(current, parent, root),
+      start_time_(Clock::now()),
+      result_(Error::Code::kNone),
+      name_(name),
+      file_name_(file),
+      line_number_(line),
+      category_(category) {}
+
+TraceLoggerBase::TraceLoggerBase(TraceCategory::Value category,
+                                 const char* name,
+                                 const char* file,
+                                 uint32_t line,
+                                 TraceIdHierarchy ids)
+    : TraceLoggerBase(category,
+                      name,
+                      file,
+                      line,
+                      ids.current,
+                      ids.parent,
+                      ids.root) {}
+
+SynchronousTraceLogger::~SynchronousTraceLogger() {
+  // If this object has an instance variable platform, use that. Otherwise,
+  // use the static variable for the shared class. In practice, the instance
+  // variable should only be set when testing, so branch prediction will
+  // always pick the correct path in production code and it should be of
+  // negligable cost.
+  auto* current_platform =
+      this->platform_override_ != nullptr
+          ? this->platform_override_
+          : TraceLoggingPlatform::GetDefaultTracingPlatform();
+  auto end_time = Clock::now();
+  current_platform->LogTrace(this->name_, this->line_number_, this->file_name_,
+                             this->start_time_, end_time, this->trace_id_,
+                             this->parent_id_, this->root_id_, this->result_);
+}
+
+AsynchronousTraceLogger::~AsynchronousTraceLogger() {
+  // If this object has an instance variable platform, use that. Otherwise,
+  // use the static variable for the shared class. In practice, the instance
+  // variable should only be set when testing, so branch prediction will
+  // always pick the correct path in production code and it should be of
+  // negligable cost.
+  auto* current_platform =
+      this->platform_override_ != nullptr
+          ? this->platform_override_
+          : TraceLoggingPlatform::GetDefaultTracingPlatform();
+  current_platform->LogAsyncStart(
+      this->name_, this->line_number_, this->file_name_, this->start_time_,
+      this->trace_id_, this->parent_id_, this->root_id_);
+}
+
+TraceIdSetter::~TraceIdSetter() = default;
+
+// static
+TraceIdSetter* TraceIdSetter::CreateStackRootNode() {
+  return new TraceIdSetter(TraceIdHierarchy::Empty(), false);
+}
+
+}  // namespace internal
+}  // namespace platform
+}  // namespace openscreen
---
+++ platform/api/internal/trace_logging_unittest.cc
@@ -0,0 +1,500 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/trace_logging.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "absl/types/optional.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace openscreen {
+namespace platform {
+namespace internal {
+
+using ::testing::_;
+using ::testing::Invoke;
+
+// Mocks needed for testing purposes.
+class MockSyncTraceLogger : public SynchronousTraceLogger {
+ public:
+  MockSyncTraceLogger(TraceLoggingPlatform* test_platform,
+                      const char* name,
+                      const char* file,
+                      uint32_t line,
+                      TraceCategory::Value category)
+      : SynchronousTraceLogger(category, name, file, line) {
+    this->platform_override_ = test_platform;
+  }
+  MockSyncTraceLogger(TraceLoggingPlatform* test_platform,
+                      const char* name,
+                      const char* file,
+                      uint32_t line,
+                      TraceCategory::Value category,
+                      TraceId id)
+      : SynchronousTraceLogger(category, name, file, line, id) {
+    this->platform_override_ = test_platform;
+  }
+  MockSyncTraceLogger(TraceLoggingPlatform* test_platform,
+                      const char* name,
+                      const char* file,
+                      uint32_t line,
+                      TraceCategory::Value category,
+                      TraceId trace_id,
+                      TraceId parent_id,
+                      TraceId root_id)
+      : SynchronousTraceLogger(category,
+                               name,
+                               file,
+                               line,
+                               trace_id,
+                               parent_id,
+                               root_id) {
+    this->platform_override_ = test_platform;
+  }
+  MockSyncTraceLogger(TraceLoggingPlatform* test_platform,
+                      const char* name,
+                      const char* file,
+                      uint32_t line,
+                      TraceCategory::Value category,
+                      TraceIdHierarchy ids)
+      : SynchronousTraceLogger(category, name, file, line, ids) {
+    this->platform_override_ = test_platform;
+  }
+};
+
+class MockAsyncTraceLogger : public AsynchronousTraceLogger {
+ public:
+  MockAsyncTraceLogger(TraceLoggingPlatform* test_platform,
+                       const char* name,
+                       const char* file,
+                       uint32_t line,
+                       TraceCategory::Value category)
+      : AsynchronousTraceLogger(category, name, file, line) {
+    this->platform_override_ = test_platform;
+  }
+  MockAsyncTraceLogger(TraceLoggingPlatform* test_platform,
+                       const char* name,
+                       const char* file,
+                       uint32_t line,
+                       TraceCategory::Value category,
+                       TraceId id)
+      : AsynchronousTraceLogger(category, name, file, line, id) {
+    this->platform_override_ = test_platform;
+  }
+  MockAsyncTraceLogger(TraceLoggingPlatform* test_platform,
+                       const char* name,
+                       const char* file,
+                       uint32_t line,
+                       TraceCategory::Value category,
+                       TraceId trace_id,
+                       TraceId parent_id,
+                       TraceId root_id)
+      : AsynchronousTraceLogger(category,
+                                name,
+                                file,
+                                line,
+                                trace_id,
+                                parent_id,
+                                root_id) {
+    this->platform_override_ = test_platform;
+  }
+  MockAsyncTraceLogger(TraceLoggingPlatform* test_platform,
+                       const char* name,
+                       const char* file,
+                       uint32_t line,
+                       TraceCategory::Value category,
+                       TraceIdHierarchy ids)
+      : AsynchronousTraceLogger(category, name, file, line, ids) {
+    this->platform_override_ = test_platform;
+  }
+};
+
+class MockLoggingPlatform : public TraceLoggingPlatform {
+ public:
+  MOCK_METHOD9(LogTrace,
+               void(const char*,
+                    const uint32_t,
+                    const char* file,
+                    Clock::time_point,
+                    Clock::time_point,
+                    TraceId,
+                    TraceId,
+                    TraceId,
+                    Error::Code));
+  MOCK_METHOD7(LogAsyncStart,
+               void(const char*,
+                    const uint32_t,
+                    const char* file,
+                    Clock::time_point,
+                    TraceId,
+                    TraceId,
+                    TraceId));
+  MOCK_METHOD5(LogAsyncEnd,
+               void(const uint32_t,
+                    const char* file,
+                    Clock::time_point,
+                    TraceId,
+                    Error::Code));
+};
+
+// Methods to validate the results of platform-layer calls.
+template <uint64_t milliseconds>
+void ValidateTraceTimestampDiff(const char* name,
+                                const uint32_t line,
+                                const char* file,
+                                Clock::time_point start_time,
+                                Clock::time_point end_time,
+                                TraceId trace_id,
+                                TraceId parent_id,
+                                TraceId root_id,
+                                Error error) {
+  const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+      end_time - start_time);
+  EXPECT_GE(elapsed.count(), milliseconds);
+}
+
+template <Error::Code result>
+void ValidateTraceErrorCode(const char* name,
+                            const uint32_t line,
+                            const char* file,
+                            Clock::time_point start_time,
+                            Clock::time_point end_time,
+                            TraceId trace_id,
+                            TraceId parent_id,
+                            TraceId root_id,
+                            Error error) {
+  EXPECT_EQ(error.code(), result);
+}
+
+// Test methods. Because the macros always calling TraceBase::trace_platform,
+// testing will skip over the macros and directly call the relevant classes.
+// Since TraceBase's IsLoggingEnabled(...) and TraceAsyncEnd(...) do no work on
+// their own and only directly call the platform layer, this should not leave
+// a large hole in testing.
+constexpr TraceCategory::Value category =
+    TraceCategory::Value::CastPlatformLayer;
+constexpr uint32_t line = 10;
+
+TEST(TraceLoggingTest, MacroCallScopedDoesntFail) {
+  TRACE_SCOPED(TraceCategory::Value::Any, "test");
+}
+
+TEST(TraceLoggingTest, MacroCallUnscopedDoesntFail) {
+  TRACE_ASYNC_START(TraceCategory::Value::Any, "test");
+}
+
+TEST(TraceLoggingTest, MacroVariablesUniquelyNames) {
+  TRACE_SCOPED(TraceCategory::Value::Any, "test1");
+  TRACE_SCOPED(TraceCategory::Value::Any, "test2");
+  TRACE_ASYNC_START(TraceCategory::Value::Any, "test3");
+  TRACE_ASYNC_START(TraceCategory::Value::Any, "test4");
+}
+
+TEST(TraceLoggingTest, TestMacroStyleInitializationTrue) {
+  constexpr uint32_t delay_in_ms = 50;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform, LogTrace(_, _, _, _, _, _, _, _, _))
+      .Times(1)
+      .WillOnce(DoAll(Invoke(ValidateTraceTimestampDiff<delay_in_ms>),
+                      Invoke(ValidateTraceErrorCode<Error::Code::kNone>)));
+
+  {
+    uint8_t temp[sizeof(MockSyncTraceLogger)];
+    auto ptr = true ? TraceInstanceHelper<MockSyncTraceLogger>::Create(
+                          temp, platform_ptr, "Name", __FILE__, line, category)
+                    : TraceInstanceHelper<MockSyncTraceLogger>::Empty();
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay_in_ms));
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_NE(ids.current, kEmptyTraceId);
+  }
+  auto ids2 = ScopedTraceOperation::hierarchy();
+  EXPECT_EQ(ids2.current, kEmptyTraceId);
+  EXPECT_EQ(ids2.parent, kEmptyTraceId);
+  EXPECT_EQ(ids2.root, kEmptyTraceId);
+}
+
+TEST(TraceLoggingTest, TestMacroStyleInitializationFalse) {
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform, LogTrace(_, _, _, _, _, _, _, _, _)).Times(0);
+
+  {
+    uint8_t temp[sizeof(MockSyncTraceLogger)];
+    auto ptr = false ? TraceInstanceHelper<MockSyncTraceLogger>::Create(
+                           temp, platform_ptr, "Name", __FILE__, line, category)
+                     : TraceInstanceHelper<MockSyncTraceLogger>::Empty();
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, kEmptyTraceId);
+    EXPECT_EQ(ids.parent, kEmptyTraceId);
+    EXPECT_EQ(ids.root, kEmptyTraceId);
+  }
+  auto ids2 = ScopedTraceOperation::hierarchy();
+  EXPECT_EQ(ids2.current, kEmptyTraceId);
+  EXPECT_EQ(ids2.parent, kEmptyTraceId);
+  EXPECT_EQ(ids2.root, kEmptyTraceId);
+}
+
+TEST(TraceLoggingTest, ExpectTimestampsReflectDelay) {
+  constexpr uint32_t delay_in_ms = 50;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform, LogTrace(_, _, _, _, _, _, _, _, _))
+      .WillOnce(DoAll(Invoke(ValidateTraceTimestampDiff<delay_in_ms>),
+                      Invoke(ValidateTraceErrorCode<Error::Code::kNone>)));
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            MockSyncTraceLogger(platform_ptr, "Name", __FILE__, line, category);
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay_in_ms));
+  }
+}
+
+TEST(TraceLoggingTest, ExpectErrorsPassedToResult) {
+  constexpr Error::Code result_code = Error::Code::kParseError;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, _, _, _, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<result_code>));
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            MockSyncTraceLogger{platform_ptr, "Name", __FILE__, line, category};
+    ScopedTraceOperation::set_result(result_code);
+  }
+}
+
+TEST(TraceLoggingTest, ExpectUnsetTraceIdNotSet) {
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, _, _, _, _))
+      .Times(1);
+
+  TraceIdHierarchy h = {kUnsetTraceId, kUnsetTraceId, kUnsetTraceId};
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockSyncTraceLogger(
+            platform_ptr, "Name", __FILE__, line, category, h);
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_NE(ids.current, kUnsetTraceId);
+    EXPECT_NE(ids.parent, kUnsetTraceId);
+    EXPECT_NE(ids.root, kUnsetTraceId);
+  }
+}
+
+TEST(TraceLoggingTest, ExpectCreationWithIdsToWork) {
+  constexpr TraceId current = 0x32;
+  constexpr TraceId parent = 0x47;
+  constexpr TraceId root = 0x84;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, current, parent, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+
+  {
+    TraceIdHierarchy h = {current, parent, root};
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockSyncTraceLogger(
+            platform_ptr, "Name", __FILE__, line, category, h);
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, current);
+    EXPECT_EQ(ids.parent, parent);
+    EXPECT_EQ(ids.root, root);
+    EXPECT_EQ(ScopedTraceOperation::current_id(), current);
+    EXPECT_EQ(ScopedTraceOperation::root_id(), root);
+  }
+}
+
+TEST(TraceLoggingTest, ExpectHirearchyToBeApplied) {
+  constexpr TraceId current = 0x32;
+  constexpr TraceId parent = 0x47;
+  constexpr TraceId root = 0x84;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, _, current, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, current, parent, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockSyncTraceLogger{
+            platform_ptr, "Name",   __FILE__,
+            line,         category, {current, parent, root}};
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, current);
+    EXPECT_EQ(ids.parent, parent);
+    EXPECT_EQ(ids.root, root);
+
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            MockSyncTraceLogger{platform_ptr, "Name", __FILE__, line, category};
+    ids = ScopedTraceOperation::hierarchy();
+    EXPECT_NE(ids.current, current);
+    EXPECT_EQ(ids.parent, current);
+    EXPECT_EQ(ids.root, root);
+  }
+}
+
+TEST(TraceLoggingTest, ExpectHirearchyToEndAfterScopeWhenSetWithSetter) {
+  constexpr TraceId current = 0x32;
+  constexpr TraceId parent = 0x47;
+  constexpr TraceId root = 0x84;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, _, current, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            TraceIdSetter{{current, parent, root}};
+    {
+      TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+          TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockSyncTraceLogger{
+              platform_ptr, "Name", __FILE__, line, category};
+      auto ids = ScopedTraceOperation::hierarchy();
+      EXPECT_NE(ids.current, current);
+      EXPECT_EQ(ids.parent, current);
+      EXPECT_EQ(ids.root, root);
+    }
+
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, current);
+    EXPECT_EQ(ids.parent, parent);
+    EXPECT_EQ(ids.root, root);
+  }
+}
+
+TEST(TraceLoggingTest, ExpectHirearchyToEndAfterScope) {
+  constexpr TraceId current = 0x32;
+  constexpr TraceId parent = 0x47;
+  constexpr TraceId root = 0x84;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, _, current, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, current, parent, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockSyncTraceLogger{
+            platform_ptr, "Name",   __FILE__,
+            line,         category, {current, parent, root}};
+    {
+      TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+          TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockSyncTraceLogger{
+              platform_ptr, "Name", __FILE__, line, category};
+      auto ids = ScopedTraceOperation::hierarchy();
+      EXPECT_NE(ids.current, current);
+      EXPECT_EQ(ids.parent, current);
+      EXPECT_EQ(ids.root, root);
+    }
+
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, current);
+    EXPECT_EQ(ids.parent, parent);
+    EXPECT_EQ(ids.root, root);
+  }
+}
+
+TEST(TraceLoggingTest, ExpectSetHierarchyToApply) {
+  constexpr TraceId current = 0x32;
+  constexpr TraceId parent = 0x47;
+  constexpr TraceId root = 0x84;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogTrace(testing::StrEq("Name"), line, testing::StrEq(__FILE__),
+                       _, _, _, current, root, _))
+      .WillOnce(Invoke(ValidateTraceErrorCode<Error::Code::kNone>));
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            TraceIdSetter{{current, parent, root}};
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, current);
+    EXPECT_EQ(ids.parent, parent);
+    EXPECT_EQ(ids.root, root);
+
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            MockSyncTraceLogger{platform_ptr, "Name", __FILE__, line, category};
+    ids = ScopedTraceOperation::hierarchy();
+    EXPECT_NE(ids.current, current);
+    EXPECT_EQ(ids.parent, current);
+    EXPECT_EQ(ids.root, root);
+  }
+}
+
+TEST(TraceLoggingTest, CheckTraceAsyncStartLogsCorrectly) {
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogAsyncStart(testing::StrEq("Name"), line,
+                            testing::StrEq(__FILE__), _, _, _, _))
+      .Times(1);
+
+  { MockAsyncTraceLogger{platform_ptr, "Name", __FILE__, line, category}; }
+}
+
+TEST(TraceLoggingTest, CheckTraceAsyncStartSetsHierarchy) {
+  constexpr TraceId current = 32;
+  constexpr TraceId parent = 47;
+  constexpr TraceId root = 84;
+  MockLoggingPlatform mock_platform;
+  TraceLoggingPlatform* platform_ptr = &mock_platform;
+  EXPECT_CALL(mock_platform,
+              LogAsyncStart(testing::StrEq("Name"), line,
+                            testing::StrEq(__FILE__), _, _, current, root))
+      .Times(1);
+
+  {
+    TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+        TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) =
+            TraceIdSetter{{current, parent, root}};
+    {
+      TRACE_INTERNAL_IGNORE_UNUSED_VAR const TraceBase&
+          TRACE_INTERNAL_UNIQUE_VAR_NAME(trace_ref) = MockAsyncTraceLogger{
+              platform_ptr, "Name", __FILE__, line, category};
+      auto ids = ScopedTraceOperation::hierarchy();
+      EXPECT_NE(ids.current, current);
+      EXPECT_EQ(ids.parent, current);
+      EXPECT_EQ(ids.root, root);
+    }
+
+    auto ids = ScopedTraceOperation::hierarchy();
+    EXPECT_EQ(ids.current, current);
+    EXPECT_EQ(ids.parent, parent);
+    EXPECT_EQ(ids.root, root);
+  }
+}
+
+}  // namespace internal
+}  // namespace platform
+}  // namespace openscreen
