cfb1ed06fe383d74c98fd49b78fc3bf96788bc07
Ryan Keane <rwkeane@google.com>
2019-07-30 11:29:39 -0700

    Trace Logging: User Provided Args Pt 1
    This is the first part of supporting User Args:
    - Add new classes required to support User Args
    - Move shared code to a new class
    - Add more creation methods to hook the new classes into the creation flow
    
    There are limited UTs here. Full UT coverage is included as part of the next CL
    which pushes these changes through the platform layer
    
    Large CL showing this all works together / high level idea:
    https://chromium-review.googlesource.com/c/openscreen/+/1721620
    
    Change-Id: I5af62aa01d22208a900d6c022075eb1a2dcdf7d5

---
+++ platform/api/internal/trace_logging_creation_helper_unittest.cc
@@ -0,0 +1,92 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/internal/trace_logging_creation_helper.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "platform/base/error.h"
+
+namespace openscreen {
+namespace platform {
+namespace internal {
+
+using ::testing::_;
+using ::testing::Invoke;
+
+TEST(TraceLoggingCreationHelper, CreateConstCharArg) {
+  const TraceLoggingArgument& arg =
+      TraceLoggingArgumentFactory::Create("name", "value");
+  ASSERT_STREQ(arg.name(), "name");
+  ASSERT_STREQ(arg.value(), "value");
+}
+
+TEST(TraceLoggingCreationHelper, CreatePointerArg) {
+  int val = 1;
+  const TraceLoggingArgument& arg =
+      TraceLoggingArgumentFactory::Create("name", &val);
+  ASSERT_STREQ(arg.name(), "name");
+  ASSERT_STREQ(arg.value(), "1");
+}
+
+TEST(TraceLoggingCreationHelper, CreateStringStreamArg) {
+  const TraceLoggingArgument& arg =
+      TraceLoggingArgumentFactory::Create("name", 1);
+  ASSERT_STREQ(arg.name(), "name");
+  ASSERT_STREQ(arg.value(), "1");
+
+  const TraceLoggingArgument& arg2 =
+      TraceLoggingArgumentFactory::Create("name", Error::Code::kNone);
+  ASSERT_STREQ(arg2.name(), "name");
+  ASSERT_STREQ(arg2.value(), "Success");
+}
+
+TEST(TraceLoggingCreationHelper, CreateFunctionArg) {
+  int value = 0;
+  std::function<int()> func = [value_ptr = &value]() { return (*value_ptr)++; };
+  const TraceLoggingArgument& arg =
+      TraceLoggingArgumentFactory::Create("name", func);
+  ASSERT_STREQ(arg.name(), "name");
+  EXPECT_EQ(value, 0);
+  ASSERT_STREQ(arg.value(), "0");
+  EXPECT_EQ(value, 1);
+  ASSERT_STREQ(arg.value(), "0");
+  EXPECT_EQ(value, 1);
+
+  const TraceLoggingArgument& arg2 =
+      TraceLoggingArgumentFactory::Create("name2", func);
+  ASSERT_STREQ(arg2.name(), "name2");
+  EXPECT_EQ(value, 1);
+  ASSERT_STREQ(arg2.value(), "1");
+  EXPECT_EQ(value, 2);
+  ASSERT_STREQ(arg2.value(), "1");
+  EXPECT_EQ(value, 2);
+}
+
+class Parent {
+ public:
+  Parent(int* i) : i_(i) {}
+  virtual ~Parent() { *i_ *= 2; }
+
+ protected:
+  int* i_;
+};
+
+class Child : public Parent {
+ public:
+  Child(int* i) : Parent(i) {}
+  ~Child() override { *i_ *= 2; }
+};
+
+TEST(TraceLoggingCreationHelper, UniquePtrStackWrapperRunsDestructors) {
+  int value = 1;
+
+  { UniquePtrStackWrapper<Parent>::Create<Child>(&value); }
+
+  EXPECT_EQ(value, 4);
+}
+
+}  // namespace internal
+}  // namespace platform
+}  // namespace openscreen
