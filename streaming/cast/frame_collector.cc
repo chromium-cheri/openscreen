2a52f0aa0f22beee9d737540086c0483ef56192a
Yuri Wiitala <miu@chromium.org>
2019-08-28 18:14:11 -0700

    Cast Streaming: FrameCollector for collecting/assembling from packets.
    Introdues a FrameCollector, which a Receiver uses to collect a single
    frame's metadata and divied-up payloads (across packets) into a fully-
    assembled frame.
    
    Bug: openscreen:55
    Change-Id: I3449f76a36f248c54ddc80f9fecbc538e6c947f9

---
+++ streaming/cast/frame_collector.cc
@@ -0,0 +1,156 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "streaming/cast/frame_collector.h"
+
+#include <algorithm>
+#include <limits>
+
+#include "platform/api/logging.h"
+#include "streaming/cast/frame_id.h"
+#include "streaming/cast/rtp_defines.h"
+
+namespace openscreen {
+namespace cast_streaming {
+
+namespace {
+
+// Integer constant representing that the number of packets is not yet known.
+constexpr int kUnknownNumberOfPackets = std::numeric_limits<int>::max();
+
+}  // namespace
+
+FrameCollector::FrameCollector()
+    : num_missing_packets_(kUnknownNumberOfPackets) {}
+
+FrameCollector::~FrameCollector() = default;
+
+bool FrameCollector::CollectPart(const RtpPacketParser::ParseResult& part) {
+  OSP_DCHECK(!frame_.frame_id.is_null());
+
+  if (part.frame_id != frame_.frame_id) {
+    OSP_LOG_WARN
+        << "Ignoring potentially corrupt packet (frame ID mismatch). Expected: "
+        << frame_.frame_id << " Got: " << part.frame_id;
+    return false;
+  }
+
+  const int frame_packet_count = static_cast<int>(part.max_packet_id) + 1;
+  if (num_missing_packets_ == kUnknownNumberOfPackets) {
+    // This is the first packet being processed for the frame.
+    num_missing_packets_ = frame_packet_count;
+    chunks_.assign(num_missing_packets_,
+                   DataChunkSpan{DataChunkSpan::kMissingOffset, 0});
+
+    // Optimization: Assume that all packets are the same size as the first, and
+    // reserve the frame's entire payload buffer up-front to prevent needless
+    // re-allocations/copies when the vector capacity must be auto-increased.
+    // While the Cast Streaming spec doesn't guarantee anything about how a
+    // frame's payload is split-up over multiple packets, Senders will usually
+    // spread it evenly across all packets (with any smaller remainder in the
+    // last packet). If this assumption is wrong, no harm is being done here.
+    frame_.owned_data_.reserve(part.payload.size() * frame_packet_count);
+  } else {
+    // Since this is not the first packet being processed, sanity-check that the
+    // "frame ID" and "max packet ID" are the expected values.
+    if (frame_packet_count != static_cast<int>(chunks_.size())) {
+      OSP_LOG_WARN << "Ignoring potentially corrupt packet (packet count "
+                      "mismatch). packet_count="
+                   << chunks_.size() << " is not equal to 1 + max_packet_id="
+                   << part.max_packet_id;
+      return false;
+    }
+  }
+
+  // The packet ID must not be greater than the max packet ID.
+  if (part.packet_id >= chunks_.size()) {
+    return false;
+  }
+
+  // Don't process duplicate packets.
+  if (chunks_[part.packet_id].offset != DataChunkSpan::kMissingOffset) {
+    return true;
+  }
+
+  // Populate metadata from packet 0 only, which is the only packet that must
+  // contain a complete set of values.
+  if (part.packet_id == FramePacketId{0}) {
+    if (part.is_key_frame) {
+      frame_.dependency = EncodedFrame::Dependency::KEY;
+    } else if (part.frame_id == part.referenced_frame_id) {
+      frame_.dependency = EncodedFrame::Dependency::INDEPENDENT;
+    } else {
+      frame_.dependency = EncodedFrame::Dependency::DEPENDENT;
+    }
+    frame_.referenced_frame_id = part.referenced_frame_id;
+    frame_.rtp_timestamp = part.rtp_timestamp;
+    frame_.new_playout_delay = part.new_playout_delay;
+  }
+
+  // Copy the packet's payload into frame storage. For efficiency, just append
+  // the data to the end. Later, PeekAtAssembledFrame() will shuffle it all back
+  // into the correct order if necessary.
+  chunks_[part.packet_id] =
+      DataChunkSpan{static_cast<int>(frame_.owned_data_.size()),
+                    static_cast<int>(part.payload.size())};
+  frame_.owned_data_.insert(frame_.owned_data_.end(), part.payload.begin(),
+                            part.payload.end());
+  --num_missing_packets_;
+  OSP_DCHECK_GE(num_missing_packets_, 0);
+  return true;
+}
+
+void FrameCollector::AppendMissingPackets(
+    std::vector<PacketNack>* nacks) const {
+  OSP_DCHECK(!frame_.frame_id.is_null());
+
+  if (num_missing_packets_ == 0) {
+    return;
+  }
+
+  const int frame_packet_count = chunks_.size();
+  if (num_missing_packets_ >= frame_packet_count) {
+    nacks->push_back(PacketNack{frame_.frame_id, kAllPacketsLost});
+    return;
+  }
+
+  for (int packet_id = 0; packet_id < frame_packet_count; ++packet_id) {
+    if (chunks_[packet_id].offset == DataChunkSpan::kMissingOffset) {
+      nacks->push_back(
+          PacketNack{frame_.frame_id, static_cast<FramePacketId>(packet_id)});
+    }
+  }
+}
+
+const EncryptedFrame& FrameCollector::PeekAtAssembledFrame() {
+  OSP_DCHECK_EQ(num_missing_packets_, 0);
+
+  if (!frame_.data.data()) {
+    // If the parts of the frame were collected out-of-order, re-order them now.
+    if (!std::is_sorted(chunks_.begin(), chunks_.end())) {
+      std::vector<uint8_t> data_in_order;
+      data_in_order.reserve(frame_.owned_data_.size());
+      for (int i = 0; i < static_cast<int>(chunks_.size()); ++i) {
+        const auto begin = frame_.owned_data_.begin() + chunks_[i].offset;
+        const auto end = begin + chunks_[i].size;
+        data_in_order.insert(data_in_order.end(), begin, end);
+      }
+      frame_.owned_data_.swap(data_in_order);
+    }
+    frame_.data = absl::Span<uint8_t>(frame_.owned_data_);
+  }
+
+  return frame_;
+}
+
+void FrameCollector::Reset() {
+  num_missing_packets_ = kUnknownNumberOfPackets;
+  frame_.frame_id = FrameId();
+  frame_.owned_data_.clear();
+  frame_.owned_data_.shrink_to_fit();
+  frame_.data = absl::Span<uint8_t>();
+}
+
+}  // namespace cast_streaming
+}  // namespace openscreen
