0e4448164d8607222d2c9c2fdbfd8848cada5e98
Jordan Bayles <jophba@chromium.org>
2019-09-06 14:09:10 -0700

    Implement TlsCredentials construction
    This patch adds certificate creation to the TlsCredentials class.
    
    Change-Id: Ic991bd23672fbe8c173549a7b51adab55472c9c0

--- platform/base/tls_credentials.cc
+++ platform/base/tls_credentials.cc
@@ -4,14 +4,201 @@
 
 #include "platform/base/tls_credentials.h"
 
+#include <openssl/asn1.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/x509.h>
+#include <time.h>
+
+#include <chrono>
+#include <utility>
+
+#include "absl/strings/str_cat.h"
+#include "platform/api/logging.h"
+#include "util/crypto/openssl_util.h"
+
 namespace openscreen {
 namespace platform {
 
-// TODO(jophba): write constructor and certificate generation code.
-TlsCredentials::TlsCredentials(
+namespace {
+
+// OpenSSL methods return either 0 or nullptr when any errors occur.
+#define OPENSSL_CHECK(return_code)                        \
+  {                                                       \
+    if (return_code == 0) {                               \
+      OSP_LOG_WARN << "OpenSSL function error occurred."; \
+      return nullptr;                                     \
+    }                                                     \
+  }
+
+// Returns whether or not the certificate field successfully was added.
+bool AddCertificateField(X509_NAME* certificate_name,
+                         absl::string_view field,
+                         absl::string_view value) {
+  return X509_NAME_add_entry_by_txt(
+             certificate_name, std::string(field).c_str(), MBSTRING_ASC,
+             reinterpret_cast<const unsigned char*>(value.data()),
+             value.length(), -1, 0) == 1;
+}
+
+bssl::UniquePtr<ASN1_TIME> ToAsn1Time(const Clock::time_point time) {
+  // We don't have access to system_clock::to_time_t.
+  const std::time_t time_as_t =
+      std::chrono::duration_cast<std::chrono::seconds>(time.time_since_epoch())
+          .count();
+
+  return bssl::UniquePtr<ASN1_TIME>(ASN1_TIME_set(nullptr, time_as_t));
+}
+
+bssl::UniquePtr<X509> CreateCertificate(
     absl::string_view name,
     std::chrono::seconds certificate_duration,
-    bssl::UniquePtr<EVP_PKEY> private_public_keypair) {}
+    EVP_PKEY key_pair,
+    const Clock::time_point now_time_point) {
+  bssl::UniquePtr<X509> certificate(X509_new());
+
+  // Serial numbers must be unique for this session. As a pretend CA, we should
+  // not issue certificates with the same serial number in the same session.
+  static std::atomic_int serial_number(1);
+  OPENSSL_CHECK(ASN1_INTEGER_set(X509_get_serialNumber(certificate.get()),
+                                 serial_number++));
+
+  const bssl::UniquePtr<ASN1_TIME> now(ToAsn1Time(now_time_point));
+  const bssl::UniquePtr<ASN1_TIME> expiration_time(
+      ToAsn1Time(now_time_point + certificate_duration));
+
+  OPENSSL_CHECK(X509_set_notBefore(certificate.get(), now.get()));
+  OPENSSL_CHECK(X509_set_notAfter(certificate.get(), expiration_time.get()));
+
+  X509_NAME* certificate_name = X509_get_subject_name(certificate.get());
+
+  if (!AddCertificateField(certificate_name, "CN", name)) {
+    return nullptr;
+  }
+
+  OPENSSL_CHECK(X509_set_issuer_name(certificate.get(), certificate_name));
+  OPENSSL_CHECK(X509_set_pubkey(certificate.get(), &key_pair));
+  OPENSSL_CHECK(X509_sign(certificate.get(), &key_pair, EVP_sha256()));
+  OPENSSL_CHECK(X509_verify(certificate.get(), &key_pair));
+  return certificate;
+}
+
+ErrorOr<std::vector<uint8_t>> Sha256Digest(std::vector<uint8_t> data) {
+  bssl::UniquePtr<EVP_MD_CTX> context(EVP_MD_CTX_new());
+  uint8_t digest[EVP_MAX_MD_SIZE];
+  unsigned int digest_len;
+  if (!EVP_Digest(data.data(), data.size(), digest, &digest_len, EVP_sha256(),
+                  nullptr)) {
+    return Error::Code::kParseError;
+  }
+
+  return std::vector<uint8_t>(digest, digest + digest_len);
+}
+
+ErrorOr<std::vector<uint8_t>> WriteOutKey(EVP_PKEY key, bool is_public) {
+  bssl::UniquePtr<BIO> bio(BIO_new(BIO_s_mem()));
+
+  if (is_public) {
+    if (!PEM_write_bio_PUBKEY(bio.get(), &key)) {
+      return Error::Code::kParseError;
+    }
+  } else {
+    if (!PEM_write_bio_PrivateKey(
+            bio.get(), &key, nullptr /* EVP_CIPHER */, nullptr /* kstr */,
+            -1 /* klen */, nullptr /* pem_password_cb */, nullptr /* u */)) {
+      return Error::Code::kParseError;
+    }
+  }
+
+  char* data = nullptr;
+  const int len = BIO_get_mem_data(bio.get(), &data);
+  if (!data || len < 0) {
+    return Error::Code::kParseError;
+  }
+
+  return std::vector<uint8_t>(data, data + len);
+}
+
+}  // namespace
+
+ErrorOr<TlsCredentials> TlsCredentials::Create(
+    absl::string_view name,
+    std::chrono::seconds certificate_duration,
+    ClockNowFunctionPtr now_function,
+    bssl::UniquePtr<EVP_PKEY> key_pair) {
+  EnsureOpenSSLInit();
+
+  bssl::UniquePtr<X509> certificate =
+      CreateCertificate(name, certificate_duration, *key_pair, now_function());
+
+  if (!certificate) {
+    return Error::Code::kItemNotFound;
+  }
+
+  return TlsCredentials(std::move(certificate), std::move(key_pair));
+}
+
+const ErrorOr<std::vector<uint8_t>>& TlsCredentials::private_key_base64() {
+  if (!private_key_base64_) {
+    private_key_base64_ = std::make_unique<ErrorOr<std::vector<uint8_t>>>(
+        WriteOutKey(*key_pair_, false /* is_public */));
+  }
+
+  return *private_key_base64_;
+}
+
+const ErrorOr<std::vector<uint8_t>>& TlsCredentials::public_key_base64() {
+  if (!public_key_base64_) {
+    public_key_base64_ = std::make_unique<ErrorOr<std::vector<uint8_t>>>(
+        WriteOutKey(*key_pair_, true /* is_public */));
+  }
+
+  return *public_key_base64_;
+}
+
+const ErrorOr<std::vector<uint8_t>>& TlsCredentials::public_key_hash() {
+  if (!public_key_hash_) {
+    const ErrorOr<std::vector<uint8_t>>& public_key = public_key_base64();
+
+    if (public_key.is_error() || public_key.value().empty()) {
+      public_key_hash_ = std::make_unique<ErrorOr<std::vector<uint8_t>>>(
+          Error::Code::kParameterInvalid);
+
+      return *public_key_hash_;
+    }
+
+    public_key_hash_ = std::make_unique<ErrorOr<std::vector<uint8_t>>>(
+        Sha256Digest(public_key.value()));
+  }
+
+  return *public_key_hash_;
+}
+
+const ErrorOr<std::vector<uint8_t>>& TlsCredentials::raw_der_certificate() {
+  if (!raw_der_certificate_) {
+    unsigned char* buffer = nullptr;
+    const int len = i2d_X509(certificate_.get(), &buffer);
+    if (len <= 0) {
+      raw_der_certificate_ = std::make_unique<ErrorOr<std::vector<uint8_t>>>(
+          Error::Code::kItemNotFound);
+      return *raw_der_certificate_;
+    }
+
+    raw_der_certificate_ = std::make_unique<ErrorOr<std::vector<uint8_t>>>(
+        std::vector<uint8_t>(buffer, buffer + len));
+
+    // TODO(jophba): remove when BoringSSL complies with OpenSSL's definition
+    // of a temporary buffer. In OpenSSL, i2d_X509 uses a temporary buffer
+    // that should not be freed.
+    OPENSSL_free(buffer);
+  }
+
+  return *raw_der_certificate_;
+}
+
+TlsCredentials::TlsCredentials(bssl::UniquePtr<X509> certificate,
+                               bssl::UniquePtr<EVP_PKEY> key_pair)
+    : certificate_(std::move(certificate)), key_pair_(std::move(key_pair)) {}
 
 }  // namespace platform
 }  // namespace openscreen
--- platform/base/tls_credentials.h
+++ platform/base/tls_credentials.h
@@ -6,14 +6,18 @@
 #define PLATFORM_BASE_TLS_CREDENTIALS_H_
 
 #include <openssl/crypto.h>
+#include <openssl/rsa.h>
 #include <openssl/ssl.h>
 
+#include <atomic>
 #include <chrono>
 #include <memory>
 #include <string>
 #include <vector>
 
 #include "absl/strings/string_view.h"
+#include "platform/api/time.h"
+#include "platform/base/error.h"
 #include "platform/base/macros.h"
 
 namespace openscreen {
@@ -21,50 +25,55 @@ namespace platform {
 
 class TlsCredentials {
  public:
+  // Empty constructor is used for ErrorOr.
+  TlsCredentials() = default;
+
+  // We are move only due to unique pointers.
+  TlsCredentials(TlsCredentials&&) = default;
+  TlsCredentials& operator=(TlsCredentials&&) = default;
+
   // TlsCredentials generates a self signed certificate given (1) the name
   // to use for the certificate, (2) the length of time the certificate will
-  // be valid, and (3) a private key used to generate the certificate.
-  TlsCredentials(absl::string_view name,
-                 std::chrono::seconds certificate_duration,
-                 bssl::UniquePtr<EVP_PKEY> private_public_keypair);
-
-  // The raw, generated self signed certficate.
-  const std::vector<uint8_t>& raw_der_certificate() {
-    return raw_der_certificate_;
-  }
+  // be valid, and (3) a private/public key_pair.
+  static ErrorOr<TlsCredentials> Create(
+      absl::string_view name,
+      std::chrono::seconds certificate_duration,
+      ClockNowFunctionPtr now_function,
+      bssl::UniquePtr<EVP_PKEY> key_pair);
 
   // The OpenSSL encoded self signed certificate.
-  const CRYPTO_BUFFER& certificate() const { return *certificate_; }
+  const X509& certificate() const { return *certificate_; }
 
-  // The original private key provided on construction.
-  const EVP_PKEY& private_key() const { return *private_key_; }
+  // The original key pair provided on construction.
+  const EVP_PKEY& key_pair() const { return *key_pair_; }
 
-  // A base64 encoded version of the private/public keypair's private key
+  // A base64 encoded version of the private/public key_pair's private key
   // provided on construction.
-  const std::vector<uint8_t>& private_key_base64() const {
-    return private_key_base64_;
-  }
+  const ErrorOr<std::vector<uint8_t>>& private_key_base64();
 
-  // A base64 encoded version of the private/public keypair's associated public
+  // A base64 encoded version of the private/public key_pair's associated public
   // key.
-  const std::vector<uint8_t>& public_key_base64() const {
-    return public_key_base64_;
-  }
+  const ErrorOr<std::vector<uint8_t>>& public_key_base64();
 
-  // A SHA-256 digest of the private/public keypair's associated public key.
-  const std::vector<uint8_t>& public_key_hash() const {
-    return public_key_hash_;
-  }
+  // A SHA-256 digest of the private/public key_pair's associated public key.
+  const ErrorOr<std::vector<uint8_t>>& public_key_hash();
+
+  // The raw, generated self signed certficate.
+  const ErrorOr<std::vector<uint8_t>>& raw_der_certificate();
 
  private:
-  bssl::UniquePtr<CRYPTO_BUFFER> certificate_;
+  TlsCredentials(bssl::UniquePtr<X509> certificate,
+                 bssl::UniquePtr<EVP_PKEY> key_pair);
 
-  bssl::UniquePtr<EVP_PKEY> private_key_;
-  std::vector<uint8_t> private_key_base64_;
-  std::vector<uint8_t> public_key_base64_;
-  std::vector<uint8_t> public_key_hash_;
+  bssl::UniquePtr<X509> certificate_;
+  bssl::UniquePtr<EVP_PKEY> key_pair_;
 
-  std::vector<uint8_t> raw_der_certificate_;
+  // These member variables are initialized upon the first call of their
+  // respective public getter method.
+  std::unique_ptr<ErrorOr<std::vector<uint8_t>>> private_key_base64_;
+  std::unique_ptr<ErrorOr<std::vector<uint8_t>>> public_key_base64_;
+  std::unique_ptr<ErrorOr<std::vector<uint8_t>>> public_key_hash_;
+  std::unique_ptr<ErrorOr<std::vector<uint8_t>>> raw_der_certificate_;
 };
 
 }  // namespace platform
