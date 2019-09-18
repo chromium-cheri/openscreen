133aa139b39a7d31000ea4d8128487c8ca73b8c7
Jordan Bayles <jophba@chromium.org>
2019-09-18 14:04:50 -0700

    Prototype y'all
    Change-Id: Ie7cf6201809fa99a461c94ce75cf6ec75e8aca88

---
+++ streaming/cast/api/audio_frame.h
@@ -0,0 +1,17 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef STREAMING_CAST_PLATFORM_API_AUDIO_SENDER_H_
+#define STREAMING_CAST_PLATFORM_API_AUDIO_SENDER_H_
+
+namespace openscreen {
+namespace cast_streaming {
+
+// TODO(jophba): fill out
+class AudioFrame {};
+
+}  // namespace cast_streaming
+}  // namespace openscreen
+
+#endif  // STREAMING_CAST_PLATFORM_API_AUDIO_SENDER_H_
---
+++ streaming/cast/platform/api/audio_encoder.h
@@ -0,0 +1,52 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef STREAMING_CAST_PLATFORM_API_AUDIO_ENCODER_H_
+#define STREAMING_CAST_PLATFORM_API_AUDIO_ENCODER_H_
+
+#include "absl/strings/string_view.h"
+#include "platform/api/time.h"
+#include "platform/base/error.h"
+#include "streaming/cast/api/audio_frame.h"
+#include "streaming/cast/encoded_frame.h"
+#include "streaming/cast/encoder_capabilities.h"
+#include "streaming/cast/environment.h"
+
+namespace openscreen {
+namespace cast_streaming {
+
+class AudioEncoder {
+ public:
+  class Observer {
+   public:
+    virtual void OnEncodedFrame(std::unique_ptr<EncodedFrame> frame) = 0;
+  };
+
+  // TODO(jophba): what arguments does this need?
+  // TODO(jophba): rename?
+  static openscreen::ErrorOr<std::vector<EncoderCapabilities>>
+  GetCapabilities();
+
+  // Create an encoder with the given name (provided by GetCapabilties) and
+  // settings.
+  static openscreen::ErrorOr<AudioEncoder> Create(absl::string_view name,
+                                                  Environment* environment,
+                                                  int num_channels,
+                                                  int sampling_rate,
+                                                  int bitrate,
+                                                  Observer* observer);
+
+  virtual void Encode(std::vector<AudioFrame> frames,
+                      platform::Clock::time_point recorded_time);
+  virtual int GetSamplesPerFrame() const = 0;
+  virtual platform::Clock::duration GetFrameDuration() const = 0;
+};
+
+// TODO(jophba): define advanced encoder interface
+// TODO(jophba): add capabilities
+
+}  // namespace cast_streaming
+}  // namespace openscreen
+
+#endif  // STREAMING_CAST_PLATFORM_API_AUDIO_ENCODER_H_
