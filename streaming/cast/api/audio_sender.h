ba3a2400ca05bfee975b617d0b4cb46bad694df1
Jordan Bayles <jophba@chromium.org>
2019-09-18 14:04:50 -0700

    Cast Audio/VideoSender API Prototype
    Change-Id: Ie7cf6201809fa99a461c94ce75cf6ec75e8aca88

---
+++ streaming/cast/api/audio_sender.h
@@ -0,0 +1,35 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef STREAMING_CAST_PLATFORM_API_AUDIO_SENDER_H_
+#define STREAMING_CAST_PLATFORM_API_AUDIO_SENDER_H_
+
+#include "platform/base/error.h"
+#include "streaming/cast/api/audio_frame.h"
+#include "streaming/cast/api/sender_configuration.h"
+#include "streaming/cast/api/sender_status.h"
+#include "streaming/cast/environment.h"
+
+namespace openscreen {
+namespace cast_streaming {
+
+class AudioSender {
+ public:
+  class Observer {
+   public:
+    virtual void OnStatusChange(SenderStatus status);
+  };
+
+  static ErrorOr<AudioSender> Create(Environment& environment,
+                                     const SenderConfiguration& config,
+                                     Observer& observer);
+
+  void Insert(std::vector<AudioFrame>& frames,
+              platform::Clock::duration recorded_time);
+};
+
+}  // namespace cast_streaming
+}  // namespace openscreen
+
+#endif  // STREAMING_CAST_PLATFORM_API_AUDIO_SENDER_H_
