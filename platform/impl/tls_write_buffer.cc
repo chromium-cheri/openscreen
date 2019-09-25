ec851fb2f7895bf1971e668159e656dc49e6cfbc
Ryan Keane <rwkeane@google.com>
2019-09-25 11:32:49 -0700

    TcpSocket: Implement TlsWriteBuffer
    Implement the TlsWriteBuffer for which the interface was finalized in
    previous CL
    https://chromium-review.googlesource.com/c/openscreen/+/1797222/6
    
    Change-Id: I5440fa1d73f438fc97a5eeddd2de336dc592cf45

--- platform/impl/tls_write_buffer.cc
+++ platform/impl/tls_write_buffer.cc
@@ -4,11 +4,22 @@
 
 #include "platform/impl/tls_write_buffer.h"
 
+#include <algorithm>
+#include <cstring>
+
 #include "platform/api/logging.h"
 #include "platform/api/tls_connection.h"
 
 namespace openscreen {
 namespace platform {
+namespace {
+
+inline size_t GetCurrentFillCount(size_t write_index, size_t read_index) {
+  return (write_index + TlsWriteBuffer::kBufferSizeBytes - read_index) %
+         TlsWriteBuffer::kBufferSizeBytes;
+}
+
+}  // namespace
 
 TlsWriteBuffer::~TlsWriteBuffer() = default;
 
@@ -18,25 +29,79 @@ TlsWriteBuffer::TlsWriteBuffer(TlsWriteBuffer::Observer* observer)
 }
 
 size_t TlsWriteBuffer::Write(const void* data, size_t len) {
-  // TODO(rwkeane): Implement this method.
-  OSP_UNIMPLEMENTED();
-  return 0;
+  const size_t current_write_index =
+      write_index_.load(std::memory_order_relaxed);
+  const size_t current_read_index = read_index_.load(std::memory_order_acquire);
+
+  // Calculates the current size of the buffer.
+  const size_t bytes_currently_used =
+      GetCurrentFillCount(current_write_index, current_read_index);
+
+  // Calculates how many bytes out of the requested |len| can be written without
+  // causing a buffer overflow.
+  const size_t write_len =
+      std::min(kBufferSizeBytes - bytes_currently_used - 1, len);
+
+  // Calculates the number of bytes out of |write_len| to write in the first
+  // memcpy operation, which is either all |write_len| or the number that can be
+  // written before wrapping around to the beginning of the underlying array.
+  const size_t first_write_len =
+      std::min(write_len, kBufferSizeBytes - current_write_index);
+  memcpy(&buffer_[current_write_index], data, first_write_len);
+
+  // If we didn't write all |write_len| bytes in the previous memcpy, copy any
+  // remaining bytes to the array, starting at 0 (since the last write must have
+  // finished at the end of the array).
+  if (first_write_len != write_len) {
+    const uint8_t* new_start =
+        static_cast<const uint8_t*>(data) + first_write_len;
+    memcpy(buffer_, new_start, write_len - first_write_len);
+  }
+
+  // Store and return updated values.
+  const size_t new_write_index =
+      (current_write_index + write_len) % kBufferSizeBytes;
+  write_index_.store(new_write_index, std::memory_order_release);
+  NotifyWriteBufferFill(new_write_index, current_read_index);
+  return write_len;
 }
 
 absl::Span<const uint8_t> TlsWriteBuffer::GetReadableRegion() {
-  // TODO(rwkeane): Implement this method.
-  OSP_UNIMPLEMENTED();
-  return absl::Span<const uint8_t>();
+  const size_t current_write_index =
+      write_index_.load(std::memory_order_acquire);
+  const size_t current_read_index = read_index_.load(std::memory_order_relaxed);
+
+  // Stop reading at either the end of the array or the current write index,
+  // whichever is sooner. While there may be more data wrapped around after the
+  // end of the array, the API for GetReadableRegion() only guarantees to return
+  // a subset of all available read data, so there is no reason to introduce
+  // this additional level of complexity.
+  const size_t end_index = current_write_index >= current_read_index
+                               ? current_write_index
+                               : kBufferSizeBytes - 1;
+  return absl::Span<const uint8_t>(&buffer_[current_read_index],
+                                   end_index - current_read_index);
 }
 
 void TlsWriteBuffer::Consume(size_t byte_count) {
-  // TODO(rwkeane): Implement this method.
-  OSP_UNIMPLEMENTED();
+  const size_t current_write_index =
+      write_index_.load(std::memory_order_acquire);
+  const size_t current_read_index = read_index_.load(std::memory_order_relaxed);
+
+  OSP_CHECK(GetCurrentFillCount(current_write_index, current_read_index) >=
+            byte_count);
+  const size_t new_read_index =
+      (current_read_index + byte_count) % kBufferSizeBytes;
+  read_index_.store(new_read_index, std::memory_order_release);
+
+  NotifyWriteBufferFill(current_write_index, new_read_index);
 }
 
-// NOTE: In follow-up CL, it will be useful to have this as its own method, so
-// please tolerate it for now.
-void TlsWriteBuffer::NotifyWriteBufferFill(double fraction) {
+void TlsWriteBuffer::NotifyWriteBufferFill(size_t write_index,
+                                           size_t read_index) {
+  double fraction =
+      static_cast<double>(GetCurrentFillCount(write_index, read_index)) /
+      kBufferSizeBytes;
   observer_->NotifyWriteBufferFill(fraction);
 }
 
