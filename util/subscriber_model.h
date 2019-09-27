4c021de971b1edd88a4d734b206415c513d1baf3
Ryan Keane <rwkeane@google.com>
2019-09-27 12:25:20 -0700

    General SubscriberModel util class
    Currently, we have 2 places where we need a subscriber model:
     - SocketHandleWaiter
     - UdpSocketReader
    We are about to add 3 more:
     - NetworkReaderWriter (see separate CL)
     - TlsDataRouter x 2
    
    Of these, SocketHandleWriter, NetworkReaderWriter, and TlsDataRouter
    need the ability to wait on the deletion of its managed object type
    until its 'Wait loop' is no longer running. This class provides a
    single class to use in each of the above cases
    
    More details:
    The base idea is that we need to allow for Subscribe and Unsubscribe
    calls to come while the subscribers() vector is being used. If we lock
    around the vector, it's possible that the Subscribe and Unsubscribe
    threads will starve. To get around this, instead of modifying the
    vector iself, I queue up operations. This way, the mutex is only briefly
    locked and the vector never needs to be copied.
    
    This approach, though, introduces the possibility that a removed
    provider may be called during the Wait loop. To prevent this edge case
    (and possible resulting segfault), the stop_providing_block_ condition
    variable is used. As pointed out by Yuri, "This design is 'inside-out'
    from what it should be, in that Wait() operations should always be
    interruptible when control operations need to alter the configuration of
    the system."
    
    In order to allow for cancellation, I've added
    SubscriberModelCancellationToken. This cancellation token provides a
    way for the caller to respond to an ongoing block and end ongoing
    operations earlyas follows:
     - In SocketHandleWaiter, a fake socket can be used to exit the Select
       call early.
     - In TlsDataRouter, all further Read and Write calls can be postponed.
     - In NetworkReaderWriter, the cancellation can be propegated to all
       registed providers via a new operation in the Provider interface.
    While this doesn't provide a system level guarantee that the call will
    return quickly, it should allow for the blocked operation to continue
    nearly instantly.
    
    Change-Id: Idb39eceaa73216d69da0971cc7b3bf52de5c9f7f

---
+++ util/subscriber_model.h
@@ -0,0 +1,188 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file
+
+#ifndef UTIL_SUBSCRIBER_MODEL_H_
+#define UTIL_SUBSCRIBER_MODEL_H_
+
+#include <algorithm>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <utility>
+#include <vector>
+
+#include "absl/base/thread_annotations.h"
+
+namespace openscreen {
+
+// Helper to allow for faster response of the UnsubscribeAndBlock call inside
+// SubscriberModel.
+class SubscriberModelCancellationToken {
+ public:
+  virtual ~SubscriberModelCancellationToken() = default;
+
+  // Cancels any currently running operations. The callee is expected to
+  // 'quickly' complete any ongoing operation and call
+  // SubscriberModel::ApplyPendingChanges() after receiving this call. This
+  // call is expected to be thread-safe.
+  virtual void CancelRunningOperations() = 0;
+};
+
+// This class is intended to act as a thread-safe subscriber model, where many
+// threads can add or remove subscribers and a single thread (Denoted throughout
+// this class as the 'primary thread') will read from the subscribers_ vector.
+template <class SubscriberType, class Equals = std::equal_to<SubscriberType>>
+class SubscriberModel {
+ public:
+  SubscriberModel(
+      SubscriberModelCancellationToken* cancellation_token = nullptr)
+      : cancellation_token_(cancellation_token) {}
+
+  // Subscribes the provided subscriber, overwriting any current subscriber
+  // which is equal according to the template Equals type.
+  void Subscribe(const SubscriberType& subscriber) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    subscriber_changes_.emplace_back(subscriber, SubscriberOperation::kAdd);
+    empty_subscribers_block_.notify_all();
+  }
+
+  // Unsubscribes the provided subscriber.
+  // NOTE: If there are any ongoing operations using the subscriber, their
+  // behavior is undefined.
+  void Unsubscribe(const SubscriberType& subscriber) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    subscriber_changes_.emplace_back(subscriber, SubscriberOperation::kRemove);
+  }
+
+  // Unsubscribes the provided subscriber and blocks until the pending
+  // unsubscribe call is applied.
+  void UnsubscribeAndBlock(const SubscriberType& subscriber) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    subscriber_changes_.emplace_back(subscriber, SubscriberOperation::kRemove);
+    if (cancellation_token_) {
+      cancellation_token_->CancelRunningOperations();
+    }
+    unsubscribe_block_.wait(lock, [this, &subscriber]() {
+      return !this->IsSubscriberChangePending(subscriber);
+    });
+  }
+
+  // Blocks the current thread until a subscriber is present. This method should
+  // only be called from the primary thread.
+  void BlockUntilSubscribersPresent() {
+    if (!subscribers_.empty()) {
+      return;
+    }
+
+    std::unique_lock<std::mutex> lock(mutex_);
+    empty_subscribers_block_.wait(
+        lock, [this]() { return !subscriber_changes_.empty(); });
+  }
+
+  // Applies any pending subscribe and unsubscribe calls. This method should
+  // only be called from the primary thread.
+  void ApplyPendingChanges() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    for (const SubscriberOperationInfo& operation : subscriber_changes_) {
+      const SubscriberType& subscriber = operation.first;
+      const auto it = std::find_if(subscribers_.begin(), subscribers_.end(),
+                                   [&subscriber](SubscriberType other) {
+                                     return Equals()(subscriber, other);
+                                   });
+      if (it != subscribers_.end()) {
+        subscribers_.erase(it);
+      }
+      if (operation.second == SubscriberOperation::kAdd) {
+        subscribers_.push_back(subscriber);
+      }
+    }
+    subscriber_changes_.clear();
+    unsubscribe_block_.notify_all();
+  }
+
+  // Returns the set of subscribers after applying any pending subscriber or
+  // unsubscribe calls. This method should only be called from the primary
+  // thread.
+  // NOTE: There must be a nontrivial delay between successive calls to this
+  // method, or pending Subscribe and Unsubscribe calls may not complete.
+  const std::vector<SubscriberType>& subscribers() {
+    ApplyPendingChanges();
+    return subscribers_;
+  }
+
+ private:
+  // This method expects that the caller already has ownership of the mutex_.
+  bool IsSubscriberChangePending(const SubscriberType& subscriber) {
+    return std::find_if(subscriber_changes_.begin(), subscriber_changes_.end(),
+                        [&subscriber](SubscriberOperationInfo operation) {
+                          return Equals()(subscriber, operation.first);
+                        }) != subscriber_changes_.end();
+  }
+
+  // Set of provider operations that may be performed by outside callers.
+  enum class SubscriberOperation { kAdd, kRemove };
+
+  using SubscriberOperationInfo =
+      std::pair<SubscriberType, SubscriberOperation>;
+
+  SubscriberModelCancellationToken* cancellation_token_;
+
+  // Guards against concurrent access to subscriber_changes_.
+  std::mutex mutex_;
+
+  // Set of all operations to perform on the subscribers_ vector when a valid
+  // time to do so arises.
+  std::vector<SubscriberOperationInfo> subscriber_changes_ GUARDED_BY(mutex_);
+
+  // Subscribers currently being used by this object.
+  std::vector<SubscriberType> subscribers_;
+
+  // Allows StopProviding operations to block until completed.
+  std::condition_variable unsubscribe_block_;
+
+  // Allows for blocking of the thread when there are no watched Providers. This
+  // prevents the CPU from wasting cycles when no useful work can be done.
+  std::condition_variable empty_subscribers_block_;
+};
+
+template <class Key, class T>
+class MapKeyEquals {
+ public:
+  bool operator()(const std::pair<Key, T>& lhs, const std::pair<Key, T>& rhs) {
+    return lhs.first == rhs.first;
+  }
+};
+
+// Extension of the above class to support subscribing and unsubscribing with
+// (key, value) pairs.
+template <class Key, class T>
+class MapSubscriberModel
+    : public SubscriberModel<std::pair<Key, T>, MapKeyEquals<Key, T>> {
+ public:
+  MapSubscriberModel(
+      T default_value,
+      SubscriberModelCancellationToken* cancellation_token = nullptr)
+      : Parent(cancellation_token), default_value_(std::move(default_value)) {}
+
+  void Subscribe(const Key& key, const T& value) {
+    Parent::Subscribe(std::make_pair(key, value));
+  }
+
+  void Unsubscribe(const Key& key) {
+    Parent::Unsubscribe(std::make_pair(key, default_value_));
+  }
+
+  void UnsubscribeAndBlock(const Key& key) {
+    Parent::UnsubscribeAndBlock(std::make_pair(key, default_value_));
+  }
+
+ private:
+  using Parent = SubscriberModel<std::pair<Key, T>, MapKeyEquals<Key, T>>;
+
+  const T default_value_;
+};
+
+}  // namespace openscreen
+
+#endif  // UTIL_SUBSCRIBER_MODEL_H_
