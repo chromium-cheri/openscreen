55938c9e58f254b4da576ef62a3367ac4f3d7cd0
Ryan Keane <rwkeane@google.com>
2019-10-02 13:52:57 -0700

    Unify Networking Thread: Class Definiton
    We have multiple classes that must execute wait loops on the networking
    thread:
     - SocketHandleWaiter (to watch for new TlsConnections and for
       UdpSocket data)
     - TlsDataRouter (to watch for TlsConnection data to read + write)
    
    This class provides a mechanism to integrate these two classes into a
    single wait loop
    
    Change-Id: Ic242f298bb0b07594c7ec7a745777f0f4e9a92bc

---
+++ util/operation_loop_unittest.cc
@@ -0,0 +1,28 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "util/operation_loop.h"
+
+#include <chrono>
+
+#include "gtest/gtest.h"
+
+namespace openscreen {
+namespace {
+
+TEST(OperationsLoopTest, PerformAllOperationsWaits) {
+  Clock::duration kTimeout{0};
+  constexpr Clock::duration kMinRuntime{500};
+  std::function<void(Clock::duration)> test_function =
+      [](Clock::duration timeout) {};
+  OperationLoop loop({test_function}, kTimeout, kMinRuntime);
+
+  const auto start_time = Clock::now();
+  loop.PerformAllOperationsForTesting();
+  EXPECT_GE(std::chrono::nanoseconds(Clock::now() - start_time).count(),
+            std::chrono::nanoseconds(kMinRuntime).count());
+}
+
+}  // namespace
+}  // namespace openscreen
