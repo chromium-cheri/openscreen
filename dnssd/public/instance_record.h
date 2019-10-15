8bd6862f1d46bd06d4a6a183f9a128644e40e41b
Ryan Keane <rwkeane@google.com>
2019-10-15 13:34:13 -0700

    DNS-SD: Class Skeletons
    I've uploaded class skeletons based on our discussions here:
    https://docs.google.com/document/d/1bldug-4pJB7Halbpo6hHt37PyKScqrqxsRWziNoEDfQ/edit?usp=sharing
    
    The class skeletons currently live in dnssd/public because they may be
    exposed for embedders to inject in their own implementation. These APIs
    may continue to evolve over time, but adding the public interfaces now
    will allow future CLs to go in parallel
    
    NOTES:
    - The factory methods for the public classes will come at a later point.
    - The dns_sd_temp.cc file is only here because ninja won't compile
      unreferenced header files. It will be removed in future.
    - Based on Max's suggestion, the public API may later change to expose
      a single class instead of querier + publisher. This can be addressed
      in future once the layout of each implementation is clearer.
    Change-Id: I81e521fbcb731943e39066ea69842564ae7a5699

---
+++ dnssd/public/instance_record.h
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef DNSSD_PUBLIC_INSTANCE_RECORD_H_
+#define DNSSD_PUBLIC_INSTANCE_RECORD_H_
+
+#include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
+#include "dnssd/public/txt_record.h"
+#include "platform/base/ip_address.h"
+
+namespace openscreen {
+namespace dnssd {
+
+// Represents the data stored in DNS records of types SRV, TXT, A, and AAAA
+class InstanceRecord {
+ public:
+  virtual ~InstanceRecord() = default;
+
+  // Returns the instance name for this DNS-SD record.
+  const std::string& instance_id() const;
+
+  // Returns the service id for this DNS-SD record.
+  const std::string& service_id() const;
+
+  // Returns the domain id for this DNS-SD record.
+  const std::string& domain_id() const;
+
+  // Returns the addess associated with this DNS-SD record. In any valid record,
+  // at least one of these properties should not be absl::nullopt.
+  const absl::optional<IPEndpoint>& address_v4() const;
+  const absl::optional<IPEndpoint>& address_v6() const;
+
+  // Returns the TXT record associated with this DNS-SD record
+  const TxtRecord& txt() const;
+};
+
+}  // namespace dnssd
+}  // namespace openscreen
+
+#endif  // DNSSD_PUBLIC_INSTANCE_RECORD_H_
