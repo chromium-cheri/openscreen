02d7f6f2fdedc9667e7b52a26eaff5b58ecdf301
Ryan Keane <rwkeane@google.com>
2019-10-22 14:48:19 -0700

    DNS-SD: TxtRecord implementation
    This is the implementation of the TxtRecord class as defined in CL:
    https://chromium-review.googlesource.com/c/openscreen/+/1860922
    
    txt_record is implemented under the hood using an std::unordered_map
    with custom hash and equality operators to account for no casing. Key
    validation is done as part of setting the key/flag.
    
    the txt_record interface was changed because all methods must
    immediately create an std::string (see iteration 1) in order to access
    the unordered_map via key type std::string
    
    Change-Id: I42152e2605d5af507c95226b33cab4ed184f6f93

--- discovery/dnssd/public/txt_record.h
+++ discovery/dnssd/public/txt_record.h
@@ -5,7 +5,13 @@
 #ifndef DISCOVERY_DNSSD_PUBLIC_TXT_RECORD_H_
 #define DISCOVERY_DNSSD_PUBLIC_TXT_RECORD_H_
 
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
 #include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
 #include "absl/types/span.h"
 #include "platform/base/error.h"
 
@@ -16,19 +22,57 @@ class DnsSdTxtRecord {
  public:
   // Sets the value currently stored in this DNS-SD TXT record. Returns error
   // if the provided key is already set or if either the key or value is
-  // invalid, and Error::None() otherwise. Case of the key is ignored.
-  Error SetValue(const absl::string_view& key,
-                 const absl::Span<uint8_t>& value);
-  Error SetFlag(const absl::string_view& key, bool value);
+  // invalid, and Error::None() otherwise. Keys are case-insensitive. Setting
+  // a value or flag which is already present will overwrite the previous one.
+  Error SetValue(const std::string& key,
+                 const absl::Span<const uint8_t>& value);
+  Error SetFlag(const std::string& key, bool value);
 
   // Reads the value associated with the provided key, or an error if the key
-  // is invalid or not present. Case of the key is ignored.
-  ErrorOr<absl::Span<uint8_t>> GetValue(const absl::string_view& key) const;
-  ErrorOr<bool> GetFlag(const absl::string_view& key) const;
+  // is mapped to the opposite type or the query is othewise invalid. Keys are
+  // case-insensitive.
+  ErrorOr<absl::Span<const uint8_t>> GetValue(const std::string& key) const;
+  ErrorOr<bool> GetFlag(const std::string& key) const;
 
   // Clears an existing TxtRecord value associated with the given key.
-  void ClearValue(const absl::string_view& key);
-  void ClearFlag(const absl::string_view& key);
+  Error ClearValue(const std::string& key);
+  Error ClearFlag(const std::string& key);
+
+  bool IsEmpty() const {
+    return key_value_txt_.empty() && boolean_txt_.empty();
+  }
+
+ private:
+  struct CaseInsensitiveStringHash {
+    size_t operator()(const std::string& key) const;
+  };
+
+  struct CaseInsensitiveStringEquals {
+    bool operator()(const std::string& Left, const std::string& Right) const;
+  };
+
+  // Validations for keys and (key, value) pairs.
+  bool IsKeyValid(const std::string& key) const;
+  bool IsKeyValuePairValid(const std::string& key,
+                           const absl::Span<const uint8_t>& value) const;
+
+  // Set of (key, value) pairs associated with this TXT record.
+  // NOTE: The same string name can only occur in one of key_value_txt_,
+  // boolean_txt_.
+  std::unordered_map<std::string,
+                     std::vector<uint8_t>,
+                     CaseInsensitiveStringHash,
+                     CaseInsensitiveStringEquals>
+      key_value_txt_;
+
+  // Set of named booleans associated with this TXT record. All stored boolean
+  // names are 'true', as 'false' values are not stored.
+  // NOTE: The same string name can only occur in one of key_value_txt_,
+  // boolean_txt_.
+  std::unordered_set<std::string,
+                     CaseInsensitiveStringHash,
+                     CaseInsensitiveStringEquals>
+      boolean_txt_;
 };
 
 }  // namespace discovery
