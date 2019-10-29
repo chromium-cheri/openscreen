40616eb5c89dd68e97d8f8b5eb9d9420573303c0
Ryan Keane <rwkeane@google.com>
2019-10-29 16:33:34 -0700

    DnsSd: Querier Skeleton
    This is the basic skeleton for a DNS-SD Querier implementation. Future
    CLs will implement the methods in the .cc file, but committing this CL
    first will allow for 2 CLs to do this in parallel instead of one large
    CL.
    
    Change-Id: I87f7ceaf223d8eb4673aff3d2b30e5275ab2b92c

---
+++ discovery/dnssd/impl/querier_impl.h
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DISCOVERY_DNSSD_IMPL_QUERIER_IMPL_H_
+#define DISCOVERY_DNSSD_IMPL_QUERIER_IMPL_H_
+
+#include <unordered_map>
+
+#include "absl/hash/hash.h"
+#include "absl/strings/string_view.h"
+#include "cast/common/mdns/mdns_querier.h"
+#include "cast/common/mdns/mdns_record_changed_callback.h"
+#include "cast/common/mdns/mdns_records.h"
+#include "discovery/dnssd/impl/constants.h"
+#include "discovery/dnssd/public/instance_record.h"
+#include "discovery/dnssd/public/querier.h"
+
+namespace openscreen {
+namespace discovery {
+
+class QuerierImpl : public DnsSdQuerier,
+                    public cast::mdns::MdnsRecordChangedCallback {
+ public:
+  explicit QuerierImpl(MdnsQuerier* querier);
+  virtual ~QuerierImpl() override = default;
+
+  inline bool IsQueryOngoing(const absl::string_view& service,
+                             const absl::string_view& domain);
+
+  // DnsSdQuerier overrides.
+  void StartQuery(const absl::string_view& service,
+                  const absl::string_view& domain,
+                  Callback* callback) override;
+  void StopQuery(const absl::string_view& service,
+                 const absl::string_view& domain,
+                 Callback* callback) override;
+
+  // cast::mdns::MdnsRecordChangedCallback overrides.
+  void OnRecordChanged(const cast::mdns::MdnsRecord& record,
+                       cast::mdns::RecordChangedEvent event) override;
+
+ private:
+  // Map from {instance, service, domain} to the data received so far for this
+  // service instance.
+  std::unordered_map<InstanceKey, DnsData, absl::Hash<InstanceKey>>
+      recieved_records_;
+
+  // Stores the association between {service, domain} and Callback to call for
+  // records with this ServiceKey.
+  std::map<ServiceKey, std::vector<Callback*>> callback_map_;
+
+  cast::mdns::MdnsQuerier* const querier_;
+};
+
+}  // namespace discovery
+}  // namespace openscreen
+
+#endif  // DISCOVERY_DNSSD_IMPL_QUERIER_IMPL_H_
