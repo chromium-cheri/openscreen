d45375257ec9c4e0d79059488912ad1978d7242b
Ryan Keane <rwkeane@google.com>
2019-11-14 16:59:10 -0800

    DNS-SD: Factory Method APIs
    This CL defines the DnsSdService class, which is the embedder-specific
    way of accessing a Publisher and Querier class. The embedder may make
    one of these return an unimplemented error if they only require one,
    but it makes creating shared resources (ie, the MdnsService class
    shared by our Querier and Publisher) simpler. Additionally, a factory
    method is defined on this class
    
    Additionally, a factory method is defined for MdnsService, as this is
    the primary thing that DnsSdService will need to create
    
    These methods can both be implemented in later patches.
    
    Change-Id: Ica2afcdcbcaba31f0e846e1e179fd78b7fef3679

---
+++ discovery/dnssd/public/service.h
@@ -0,0 +1,46 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DISCOVERY_DNSSD_PUBLIC_SERVICE_H_
+#define DISCOVERY_DNSSD_PUBLIC_SERVICE_H_
+
+#include <memory>
+
+#include "discovery/dnssd/publisher.h"
+#include "discovery/dnssd/querier.h"
+
+namespace openscreen {
+
+struct IPEndpoint;
+class TaskRunner;
+
+namespace discovery {
+
+// This class provides a wrapper around DnsSdQuerier and DnsSdPublisher to
+// allow for an embedder-overridable factory method below.
+class DnsSdService {
+ public:
+  virtual ~DnsSdService() = default;
+
+  // Creates a new DnsSdService instance, to be owned by the caller. On failure,
+  // the resulting object will point to nullptr.
+  static std::unique_ptr<DnsSdService> Create(TaskRunner* task_runner);
+
+  // Returns the querier instance associated with this service instance. The
+  // returned instance is owned by the DnsSdService instance on which this was
+  // called. If discovery is not supported, the result of this call will be
+  // nullptr.
+  virtual DnsSdQuerier* Querier() = 0;
+
+  // Returns the publisher instance associated with this service instance. The
+  // returned instance is owned by the DnsSdService instance on which this was
+  // called. If publishing is not supported, the result of this call will be
+  // nullptr.
+  virtual DnsSdPublisher* Publisher() = 0;
+};
+
+}  // namespace discovery
+}  // namespace openscreen
+
+#endif  // DISCOVERY_DNSSD_PUBLIC_SERVICE_H_
