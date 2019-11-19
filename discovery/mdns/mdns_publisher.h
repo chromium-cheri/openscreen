c9ae61f59d9007a747b907759fd6d18b7fccefa9
Ryan Keane <rwkeane@google.com>
2019-11-19 11:43:46 -0800

    mDNS: Responder API
    The public API for mDNS Responder API, to publish new records to our
    mDNS implementation.
    
    This is based on an API originally defined by Max in CL:
    https://chromium-review.googlesource.com/c/openscreen/+/1847209
    
    Change-Id: Ie39b1405434f5c210875a0e05468d41eca4342b2

---
+++ discovery/mdns/mdns_publisher.h
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DISCOVERY_MDNS_MDNS_PUBLISHER_H_
+#define DISCOVERY_MDNS_MDNS_PUBLISHER_H_
+
+#include "discovery/mdns/mdns_records.h"
+
+namespace openscreen {
+namespace platform {
+
+class TaskRunner;
+
+}  // namespace platform
+namespace discovery {
+
+class MdnsRecord;
+
+class MdnsPublisher {
+ public:
+  using RecordKey = std::tuple<DomainName, DnsType>;
+
+  MdnsPublisher();
+  ~MdnsPublisher();
+
+  void RegisterRecord(const MdnsRecord& record);
+
+  void UpdateRegisteredRecord(const MdnsRecord& record);
+
+  void DeregisterRecord(const MdnsRecord& record);
+
+  MdnsRecord* GetRecord(const RecordKey& key);
+
+  OSP_DISALLOW_COPY_AND_ASSIGN(MdnsPublisher);
+};
+
+}  // namespace discovery
+}  // namespace openscreen
+
+#endif  // DISCOVERY_MDNS_MDNS_PUBLISHER_H_
