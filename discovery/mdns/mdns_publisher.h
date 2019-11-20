bc46116b9a6f084697b6af18b08af1ae3b7c2b3b
Ryan Keane <rwkeane@google.com>
2019-11-19 17:39:27 -0800

    mDNS: Responder + Publisher API
    This CL adds 2 new APIs:
    - mDNS Responder
    - mDNS Publisher
    
    the mDNS Responder API is used to publish new records to our
    mDNS implementation. This is based on an API originally
    defined by Max in CL:
    https://chromium-review.googlesource.com/c/openscreen/+/1847209
    
    mDNS Publisher allows for us to register/deregister/update
    records we are currently advertising.
    
    These 2 classes could be combined (as in iteration 2), but both
    are relatively complex and have relatively disjoint purposes,
    so there is no reason to combine them
    
    Change-Id: Ie39b1405434f5c210875a0e05468d41eca4342b2

---
+++ discovery/mdns/mdns_publisher.h
@@ -0,0 +1,45 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DISCOVERY_MDNS_MDNS_PUBLISHER_H_
+#define DISCOVERY_MDNS_MDNS_PUBLISHER_H_
+
+#include <functional>
+
+#include "discovery/mdns/mdns_records.h"
+
+namespace openscreen {
+namespace platform {
+
+class TaskRunner;
+
+}  // namespace platform
+namespace discovery {
+
+class MdnsPublisher {
+ public:
+  using MdnsRecordRef = std::reference_wrapper<const MdnsRecord>;
+
+  MdnsPublisher();
+  ~MdnsPublisher();
+
+  void RegisterRecord(const MdnsRecord& record);
+
+  void UpdateRegisteredRecord(const MdnsRecord& record);
+
+  void DeregisterRecord(const MdnsRecord& record);
+
+  bool HasRecords(const DomainName& name, DnsType, DnsClass);
+
+  std::vector<MdnsRecordRef> GetRecords(const DomainName& name,
+                                        DnsType,
+                                        DnsClass);
+
+  OSP_DISALLOW_COPY_AND_ASSIGN(MdnsPublisher);
+};
+
+}  // namespace discovery
+}  // namespace openscreen
+
+#endif  // DISCOVERY_MDNS_MDNS_PUBLISHER_H_
