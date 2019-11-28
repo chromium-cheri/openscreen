df5dd4923e772636bfce16def17c49ef32e18712
Ryan Keane <rwkeane@google.com>
2019-11-27 17:19:08 -0800

    mDNS: Probe Skeleton
    This is the basic skeleton for a probing API, to handle the mDNS probing
    phase. For the near future, the probing implementation will assume that
    we are able to successfully acquire any domain name requested without
    checking over-the-wire. In future, this can be changed to implement the
    full probing protocol.
    
    NOTE: Unit Tests will come once this is checked in. Without
    MdnsPublisher further along, there isn't enough code to write UTs.
    Change-Id: I0872f9763c14ce64958237f1007d7b396657b109

---
+++ discovery/mdns/mdns_probe.cc
@@ -0,0 +1,47 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "discovery/mdns/mdns_probe.h"
+
+#include "platform/api/task_runner.h"
+
+namespace openscreen {
+namespace discovery {
+
+MdnsProbe::Observer::~Observer() = default;
+
+MdnsProbe::MdnsProbe(MdnsQuerier* querier,
+                     MdnsSender* sender,
+                     platform::TaskRunner* task_runner,
+                     MdnsRandom* random_delay,
+                     Observer* observer)
+    : querier_(querier),
+      sender_(sender),
+      task_runner_(task_runner),
+      random_delay_(random_delay),
+      observer_(observer) {
+  OSP_DCHECK(querier_);
+  OSP_DCHECK(sender_);
+  OSP_DCHECK(task_runner_);
+  OSP_DCHECK(random_delay_);
+  OSP_DCHECK(observer_);
+}
+
+MdnsProbe::~MdnsProbe() = default;
+
+void MdnsProbe::Start(DomainName requested_name,
+                      std::vector<std::function<MdnsRecord(const DomainName&)>>
+                          record_factories) {
+  OSP_DCHECK(!is_running_);
+  is_running_ = true;
+
+  // TODO(rwkeane): Implement the probing phase.
+
+  task_runner_->PostTask([this, requested_name]() {
+    observer_->OnDomainFound(this, requested_name);
+  });
+}
+
+}  // namespace discovery
+}  // namespace openscreen
---
+++ discovery/mdns/mdns_probe.h
@@ -0,0 +1,66 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DISCOVERY_MDNS_MDNS_PROBE_H_
+#define DISCOVERY_MDNS_MDNS_PROBE_H_
+
+#include "discovery/mdns/mdns_records.h"
+
+namespace openscreen {
+
+namespace platform {
+class TaskRunner;
+}  // namespace platform
+
+namespace discovery {
+
+class MdnsQuerier;
+class MdnsRandom;
+class MdnsSender;
+
+// This class is responsible for claiming a domain name as described in RFC 6762
+// Section 8.1's probing phase.
+class MdnsProbe {
+ public:
+  class Observer {
+   public:
+    virtual ~Observer();
+
+    // Called once the probing phase has been completed, and a DomainName has
+    // been confirmed.
+    virtual void OnDomainFound(MdnsProbe* probe, DomainName name) = 0;
+  };
+
+  // |querier|, |sender|, |task_runner|, |random_delay|, and |observer| must all
+  // persist for the duration of this object's lifetime.
+  MdnsProbe(MdnsQuerier* querier,
+            MdnsSender* sender,
+            platform::TaskRunner* task_runner,
+            MdnsRandom* random_delay,
+            Observer* observer);
+  ~MdnsProbe();
+
+  // Starts probing for a valid domain name based on the given one. This may
+  // only be called once per MdnsProbe instance.
+  void Start(DomainName requested_name,
+             std::vector<std::function<MdnsRecord(const DomainName&)>>
+                 record_factories);
+
+  // Returns whether this MdnsProbe has been started.
+  bool IsRunning() const { return is_running_; }
+
+ private:
+  MdnsQuerier* const querier_;
+  MdnsSender* const sender_;
+  platform::TaskRunner* const task_runner_;
+  MdnsRandom* const random_delay_;
+  Observer* const observer_;
+
+  bool is_running_ = false;
+};
+
+}  // namespace discovery
+}  // namespace openscreen
+
+#endif  // DISCOVERY_MDNS_MDNS_PROBE_H_
