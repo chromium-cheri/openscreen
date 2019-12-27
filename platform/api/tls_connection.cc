168a9a18137dba0c2991d743a4fdbb6a0e74bc81
Yuri Wiitala <miu@chromium.org>
2019-12-26 17:24:05 -0800

    TlsConnection::Write() → Send(), and return success boolean.
    Replaces TlsConnection::Write() with Send(), and changes its API
    contract to be more consistent with an "all-or-nothing send message"
    API. Removed all buffer block/unblock callbacks, since the new method
    delivers this information synchronously by returning a boolean, making
    them unnecessary.
    
    Updated TlsConnectionPosix and TlsWriteBuffer to match. The latter can
    now use its entire memory buffer.
    
    TODO: Chromium needs to implement Send() and then Write() can be
    deleted from the class.
    
    Bug: openscreen:80
    Change-Id: Icba52376813c056d32f205844b42f9fb7c88ec5c

--- platform/api/tls_connection.cc
+++ platform/api/tls_connection.cc
@@ -4,9 +4,16 @@
 
 #include "platform/api/tls_connection.h"
 
+#include <cassert>
+
 namespace openscreen {
 
 TlsConnection::TlsConnection() = default;
 TlsConnection::~TlsConnection() = default;
+
+// TODO(crbug/openscreen/80): Remove after Chromium is migrated to Send().
+void TlsConnection::Write(const void* data, size_t len) {
+  assert(false);  // Not reached.
+}
 
 }  // namespace openscreen
