89c08b7f3b478231ad1800f30980a63f41f0dac7
Ryan Keane <rwkeane@google.com>
2020-01-02 16:08:31 -0800

    Discovery: CastV2 <-> DnsSd Conversions
    Change-Id: Ic655cee9b9cfd6682df3b025914d90bf9bc93eb2

--- discovery/dnssd/public/dns_sd_txt_record.h
+++ discovery/dnssd/public/dns_sd_txt_record.h
@@ -27,6 +27,8 @@ class DnsSdTxtRecord {
   // setting a value with a key which was previously associated with a flag
   // erases the flag's value and vice versa.
   Error SetValue(const std::string& key, std::vector<uint8_t> value);
+  Error SetValue(const std::string& key, uint8_t value);
+  Error SetValue(const std::string& key, const std::string& value);
   Error SetFlag(const std::string& key, bool value);
 
   // Reads the value associated with the provided key, or an error if the key
