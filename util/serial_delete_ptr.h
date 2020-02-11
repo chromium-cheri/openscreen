f9e4ab5268ccc4a2c309af7f57a5f30bc66a1697
Ryan Keane <rwkeane@google.com>
2020-02-11 13:36:12 -0800

    Discovery: More E2E tests
    This CL adds one new E2E test to the existing Discovery E2E test suite.
    Additionally, it pulls out shared code to make the existing E2E test
    more readable and allow re-use with the new and future E2E tests.
    Third, method stubs for 2 upcoming E2E tests are added.
    
    Change-Id: If565b2e520253632428b4d73864482fc43b32e51

--- util/serial_delete_ptr.h
+++ util/serial_delete_ptr.h
@@ -19,6 +19,8 @@ namespace openscreen {
 template <typename Type, typename DeleterType>
 class SerialDelete {
  public:
+  SerialDelete() : deleter_() {}
+
   explicit SerialDelete(TaskRunner* task_runner)
       : task_runner_(task_runner), deleter_() {
     assert(task_runner);
@@ -31,6 +33,10 @@ class SerialDelete {
   }
 
   void operator()(Type* pointer) const {
+    if (!task_runner_) {
+      return;
+    }
+
     // Deletion of the object depends on the task being run by the task runner.
     task_runner_->PostTask([pointer, deleter = deleter_] { deleter(pointer); });
   }
@@ -46,6 +52,11 @@ template <typename Type, typename DeleterType = std::default_delete<Type>>
 class SerialDeletePtr
     : public std::unique_ptr<Type, SerialDelete<Type, DeleterType>> {
  public:
+  SerialDeletePtr() noexcept
+      : std::unique_ptr<Type, SerialDelete<Type, DeleterType>>(
+            nullptr,
+            SerialDelete<Type, DeleterType>()) {}
+
   explicit SerialDeletePtr(TaskRunner* task_runner) noexcept
       : std::unique_ptr<Type, SerialDelete<Type, DeleterType>>(
             nullptr,
