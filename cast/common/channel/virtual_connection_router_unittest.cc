0f5be6c97391c90e9e21ac5c437f4b65fa5d38b2
btolsch <btolsch@chromium.org>
2020-02-11 15:35:23 -0800

    Fix SenderSocketFactory integration issues
    This change fixes several problems with integrating SenderSocketFactory
    into Chrome:
     - Errors in OnMessage that lead to the destruction of the CastSocket
       need to be deferred because CastSocket is the caller.
     - Fixes a use-after-move.
     - Adds audio-only flag to CastSocket after authentication completes for
       parity with Chrome.
     - Move util/logging.h use into cc file because of name collision in
       Chrome.
    
    Bug: openscreen:59
    Change-Id: Ic1b577315400109b4e57e2eb5149b0c9d7836b73

--- cast/common/channel/virtual_connection_router_unittest.cc
+++ cast/common/channel/virtual_connection_router_unittest.cc
@@ -123,9 +123,10 @@ TEST_F(VirtualConnectionRouterTest, CloseSocketRemovesVirtualConnections) {
       VirtualConnection{"receiver-1234", "sender-4321", socket_->socket_id()},
       {});
 
-  router_.CloseSocket(socket_->socket_id());
+  int32_t id = socket_->socket_id();
+  router_.CloseSocket(id);
   EXPECT_FALSE(manager_.GetConnectionData(
-      VirtualConnection{"receiver-1234", "sender-4321", socket_->socket_id()}));
+      VirtualConnection{"receiver-1234", "sender-4321", id}));
 }
 
 }  // namespace cast
