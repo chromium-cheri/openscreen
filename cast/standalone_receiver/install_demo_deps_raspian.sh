f0f6897df43a5f2d5bf3b7bf4fa26d8463bd54b7
Jordan Bayles <jophba@chromium.org>
2020-02-11 15:00:25 -0800

    Cleanup ffmpeg, libsdl2 deps scripts
    Currently, the debian script for installing missing packages doesn't
    work on the pi's dependencies. This patch adds a new script that is
    raspian specific, cleans up the debian script, and updates
    documentation.
    
    Change-Id: I89ab5ec6fdf3757b9c66ee5edd3dfb324ce30f91

---
+++ cast/standalone_receiver/install_demo_deps_raspian.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# Installs dependencies necessary for libSDL and libAVcodec on Debian systems.
+
+sudo apt-get install libavcodec58=7:4.1.4* libavcodec-dev=7:4.1.4*       \
+                     libsdl2-2.0-0=2.0.9* libsdl2-dev=2.0.9*
