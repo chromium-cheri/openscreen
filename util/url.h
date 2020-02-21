a4d4e0bf349ddb0ff9272cdb6393372736390ef8
btolsch <btolsch@chromium.org>
2020-02-20 18:02:25 -0800

    Add URL class
    This change adds a URL class which parses URLs into their individual
    components.
    
    Bug: openscreen:60
    Change-Id: I17129dd9f3a5ad6a25279ce21288126a447e77e9

---
+++ util/url.h
@@ -0,0 +1,59 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UTIL_URL_H_
+#define UTIL_URL_H_
+
+#include <cstdint>
+#include <string>
+
+namespace openscreen {
+
+// Parses a URL and stores its components separately.  If parsing is successful,
+// is_valid() will return true, otherwise no other members should be accessed.
+// This is a thin wrapper around //third_party/mozilla.  It does not handle
+// file: or mailto: URLs.
+class URL {
+ public:
+  URL(const std::string& source);
+  URL(const URL&);
+  URL(URL&&);
+  ~URL();
+
+  URL& operator=(const URL&);
+  URL& operator=(URL&&);
+
+  // No other members should be accessed if this is false.
+  bool is_valid() const { return is_valid_; }
+
+  // A successfully parsed URL will always have a scheme.  All the other
+  // components are optional and therefore have has_*() accessors for checking
+  // their presence.
+  const std::string& scheme() const { return scheme_; }
+  bool has_host() const { return has_host_; }
+  const std::string& host() const { return host_; }
+  bool has_port() const { return has_port_; }
+  int32_t port() const { return port_; }
+  bool has_path() const { return has_path_; }
+  const std::string& path() const { return path_; }
+  bool has_query() const { return has_query_; }
+  const std::string& query() const { return query_; }
+
+ private:
+  bool is_valid_ = false;
+  bool has_host_ = false;
+  bool has_port_ = false;
+  bool has_path_ = false;
+  bool has_query_ = false;
+
+  std::string scheme_;
+  std::string host_;
+  int32_t port_ = 0;
+  std::string path_;
+  std::string query_;
+};
+
+}  // namespace openscreen
+
+#endif  // UTIL_URL_H_
