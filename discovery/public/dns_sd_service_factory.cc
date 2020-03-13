99f1ba16d26c8ee76b18694ef52116f00db55995
Ryan Keane <rwkeane@google.com>
2020-03-13 11:47:33 -0700

    Discovery: More public classes
    This CL introduces DnsSdServicePublisher and DnsSdServiceFactory into
    discovery/public
    
    Previously, @mfoltz/@btolsch/I have discussed this, and decided it was
    better to let embedders implement this themselves. The reasoning at the
    time was that this class is simple enough that there is little to be
    gained from storing this code in a centralized location. Since then,
    the following has changed:
    - Callback DnsSdPublisher::Client has been added. Most embedders do not
      care about this, so expecting them to implement it will only lead to
      confusion.
    - The recent CastAgent CL shows that use of the Discovery pipeline can
      get confusing for those not already familiar with it:
      https://chromium-review.googlesource.com/c/openscreen/+/2102902
    - Use of DEPS rules mean that external uses of the pipeline can be
      limited to just discovery/public to encourage embedders to avoid the
      complex internals of the library.
    
    Change-Id: I3fecdebb2e352c4dbf9aabe0489c17f0c8481572

---
+++ discovery/public/dns_sd_service_factory.cc
@@ -0,0 +1,16 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace openscreen {
+namespace discovery {
+
+SerialDeletePtr<DnsSdService> CreateDnsSdService(
+    TaskRunner* task_runner,
+    ReportingClient* reporting_client,
+    const Config& config) {
+  return DnsSdService::Create(task_runner, reporting_client, config);
+}
+
+}  // namespace discovery
+}  // namespace openscreen
