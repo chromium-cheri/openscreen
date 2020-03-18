ce20cce52db5a5e6f21bd9fda3878a60f9ac30d3
Jordan Bayles <jophba@chromium.org>
2020-03-18 13:29:27 -0700

    Add CastAgent class
    Change-Id: I31481906f8bfc8e6eb4268f03f2c5c32ec77f1cc

---
+++ platform/impl/scoped_wake_lock_linux.cc
@@ -0,0 +1,58 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/impl/scoped_wake_lock_linux.h"
+
+#include <CoreFoundation/CoreFoundation.h>
+
+#include "platform/api/task_runner.h"
+#include "platform/impl/platform_client_posix.h"
+#include "util/logging.h"
+
+namespace openscreen {
+
+ScopedWakeLockLinux::LockState ScopedWakeLockLinux::lock_state_{};
+
+std::unique_ptr<ScopedWakeLock> ScopedWakeLock::Create() {
+  return std::make_unique<ScopedWakeLockLinux>();
+}
+
+namespace {
+
+TaskRunner* GetTaskRunner() {
+  auto* const platform_client = PlatformClientPosix::GetInstance();
+  OSP_DCHECK(platform_client);
+  auto* const task_runner = platform_client->GetTaskRunner();
+  OSP_DCHECK(task_runner);
+  return task_runner;
+}
+
+}  // namespace
+
+ScopedWakeLockLinux::ScopedWakeLockLinux() : ScopedWakeLock() {
+  OSP_DCHECK(GetTaskRunner()->IsRunningOnTaskRunner());
+  if (reference_count_++ == 0) {
+    AcquireWakeLock();
+  }
+}
+
+ScopedWakeLockLinux::~ScopedWakeLockLinux() {
+  GetTaskRunner()->PostTask([] {
+    if (--reference_count_ == 0) {
+      ReleaseWakeLock();
+    }
+  });
+}
+
+// static
+void ScopedWakeLockLinux::AcquireWakeLock() {
+  OSP_UNIMPLEMENTED();
+}
+
+// static
+void ScopedWakeLockLinux::ReleaseWakeLock() {
+  OSP_UNIMPLEMENTED();
+}
+
+}  // namespace openscreen
