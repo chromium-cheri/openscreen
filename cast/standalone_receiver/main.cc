9726bebb03178ac1de7543847dd74c889604ea87
Jordan Bayles <jophba@chromium.org>
2020-03-25 16:04:52 -0700

    Add discovery to standalone receiver
    This patch adds DNS-SD publishing to the standalone receiver, with
    default values for fields in the service info. Future patches may make
    this more configurable, but for now everything is set from constants.
    
    Change-Id: I48412a2bb5e587cc08b3aa7f8aff76717eb8ea43

--- cast/standalone_receiver/main.cc
+++ cast/standalone_receiver/main.cc
@@ -3,12 +3,18 @@
 // found in the LICENSE file.
 
 #include <getopt.h>
+#include <uuid/uuid.h>
 
 #include <array>
 #include <chrono>  // NOLINT
 
+#include "cast/common/public/service_info.h"
 #include "cast/standalone_receiver/cast_agent.h"
 #include "cast/streaming/ssrc.h"
+#include "discovery/common/config.h"
+#include "discovery/common/reporting_client.h"
+#include "discovery/public/dns_sd_service_factory.h"
+#include "discovery/public/dns_sd_service_publisher.h"
 #include "platform/api/time.h"
 #include "platform/api/udp_socket.h"
 #include "platform/base/error.h"
@@ -23,6 +29,72 @@
 namespace openscreen {
 namespace cast {
 namespace {
+
+static constexpr char kCastV2ServiceId[] = "_googlecast._tcp";
+
+// TODO(jophba): set this based on actual values when the cast agent is
+// complete.
+static constexpr int kCastTlsPort = 80;
+
+class DiscoveryReportingClient : public discovery::ReportingClient {
+  void OnFatalError(Error error) override {
+    OSP_LOG_FATAL << "Encountered fatal discovery error: " << error;
+  }
+
+  void OnRecoverableError(Error error) override {
+    OSP_LOG_ERROR << "Encountered recoverable discovery error: " << error;
+  }
+};
+
+struct DiscoveryState {
+  SerialDeletePtr<discovery::DnsSdService> service;
+  std::unique_ptr<DiscoveryReportingClient> reporting_client;
+  std::unique_ptr<discovery::DnsSdServicePublisher<ServiceInfo>> publisher;
+};
+
+std::string GenerateUuid() {
+  uuid_t unique_id;
+  uuid_generate(unique_id);
+
+  // UUIDs are encoded as a 36-byte string plus a tailing '\0'.
+  constexpr int kUuidSize = 37;
+  char uuid_string[kUuidSize];
+  uuid_unparse(unique_id, uuid_string);
+
+  return std::string(uuid_string);
+}
+
+std::unique_ptr<DiscoveryState> StartDiscovery(TaskRunner* task_runner,
+                                               const InterfaceInfo& interface) {
+  discovery::Config config;
+
+  config.interface = interface;
+
+  auto state = std::make_unique<DiscoveryState>();
+  state->reporting_client = std::make_unique<DiscoveryReportingClient>();
+  state->service = discovery::CreateDnsSdService(
+      task_runner, state->reporting_client.get(), config);
+
+  ServiceInfo info;
+  if (interface.GetIpAddressV4()) {
+    info.v4_endpoint = IPEndpoint{interface.GetIpAddressV4(), kCastTlsPort};
+  }
+  if (interface.GetIpAddressV6()) {
+    info.v6_endpoint = IPEndpoint{interface.GetIpAddressV6(), kCastTlsPort};
+  }
+
+  info.unique_id = GenerateUuid();
+
+  // TODO(jophba): add command line arguments to set these fields.
+  info.model_name = "Cast Standalone Receiver";
+  info.friendly_name = "Cast Standalone Receiver";
+
+  state->publisher =
+      std::make_unique<discovery::DnsSdServicePublisher<ServiceInfo>>(
+          state->service.get(), kCastV2ServiceId, ServiceInfoToDnsSdRecord);
+  state->publisher->Register(info);
+  return state;
+}
 
 void RunStandaloneReceiver(TaskRunnerImpl* task_runner,
                            InterfaceInfo interface) {
@@ -115,6 +187,9 @@ int main(int argc, char* argv[]) {
   auto* const task_runner = new TaskRunnerImpl(&Clock::now);
   PlatformClientPosix::Create(Clock::duration{50}, Clock::duration{50},
                               std::unique_ptr<TaskRunnerImpl>(task_runner));
+
+  auto discovery_state =
+      openscreen::cast::StartDiscovery(task_runner, interface_info);
 
   // Runs until the process is interrupted.  Safe to pass |task_runner| as it
   // will not be destroyed by ShutDown() until this exits.
