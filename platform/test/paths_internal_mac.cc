ce28803851ae9fc2b7416f1ead4ba2f3128b61b0
btolsch <btolsch@chromium.org>
2020-03-27 17:05:12 -0700

    Get test data path by exe path
    This change makes tests that use runtime data derive the final path
    relative to the source root, which is determined from the executable
    path.  This is hard-coded to two directories up from the executable but
    is consistent with Chromium's behavior.
    
    Bug: openscreen:104
    Change-Id: I72475cc5dcdefb782ece6d4f8b75907dbe49e22e

---
+++ platform/test/paths_internal_mac.cc
@@ -0,0 +1,27 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <limits.h>
+#include <mach-o/dyld.h>
+#include <stdlib.h>
+
+#include "platform/test/paths_internal.h"
+#include "util/std_util.h"
+
+namespace openscreen {
+
+std::string GetExePath() {
+  uint32_t path_size = 0;
+  _NSGetExecutablePath(nullptr, &path_size);
+  OSP_DCHECK(path_size > 0);
+  std::string exe_path(path_size);
+  int ret = _NSGetExecutablePath(data(exe_path), &path_size);
+  OSP_DCHECK_EQ(ret, 0);
+  char* resolved = realpath(exe_path.c_str(), nullptr);
+  std::string final_path(resolved);
+  free(resolved);
+  return final_path;
+}
+
+}  // namespace openscreen
