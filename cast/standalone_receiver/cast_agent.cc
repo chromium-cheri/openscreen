df61065f2cda9c2e505257c8be4cce15d7c2ab72
Jordan Bayles <jophba@chromium.org>
2020-04-27 12:02:29 -0700

    Add basic functional tests for the CastAgent
    This patch adds some rudimentary test coverage to ensure that the
    CastAgent in the standalone receiver implementation is not entirely
    broken.
    
    Bug: b/153079471
    Change-Id: Iff26badce61fbded172ff27c490440651909b0c3

--- cast/standalone_receiver/cast_agent.cc
+++ cast/standalone_receiver/cast_agent.cc
@@ -4,6 +4,8 @@
 
 #include "cast/standalone_receiver/cast_agent.h"
 
+#include <openssl/mem.h>
+
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -11,6 +13,7 @@
 #include <vector>
 
 #include "absl/strings/str_cat.h"
+#include "cast/common/channel/message_util.h"
 #include "cast/standalone_receiver/cast_socket_message_port.h"
 #include "cast/standalone_receiver/private_key_der.h"
 #include "cast/streaming/constants.h"
@@ -35,44 +38,111 @@ constexpr auto kCertificateDuration = std::chrono::seconds(kThreeDaysInSeconds);
 // including a generated X509 certificate generated from the static private key
 // stored in private_key_der.h. The certificate is valid for
 // kCertificateDuration from when this function is called.
-ErrorOr<TlsCredentials> CreateCredentials(const IPEndpoint& endpoint) {
-  ErrorOr<bssl::UniquePtr<EVP_PKEY>> private_key =
-      ImportRSAPrivateKey(kPrivateKeyDer.data(), kPrivateKeyDer.size());
-  OSP_CHECK(private_key);
+ErrorOr<TlsCredentials> CreateCredentials(
+    const IPEndpoint& endpoint,
+    StaticCredentialsProvider* credentials_provider,
+    std::vector<uint8_t>* root_cert_der) {
+  bssl::UniquePtr<EVP_PKEY> root_key = GenerateRsaKeyPair();
+  bssl::UniquePtr<EVP_PKEY> intermediate_key = GenerateRsaKeyPair();
+  bssl::UniquePtr<EVP_PKEY> device_key = GenerateRsaKeyPair();
+  OSP_DCHECK(root_key);
+  OSP_DCHECK(intermediate_key);
+  OSP_DCHECK(device_key);
 
-  ErrorOr<bssl::UniquePtr<X509>> cert = CreateSelfSignedX509Certificate(
-      endpoint.ToString(), kCertificateDuration, *private_key.value());
-  if (!cert) {
-    return cert.error();
-  }
+  ErrorOr<bssl::UniquePtr<X509>> root_cert_or_error =
+      CreateSelfSignedX509Certificate("Cast Root CA", kCertificateDuration,
+                                      *root_key, GetWallTimeSinceUnixEpoch(),
+                                      true);
+  OSP_DCHECK(root_cert_or_error);
+  bssl::UniquePtr<X509> root_cert = std::move(root_cert_or_error.value());
 
-  auto cert_bytes = ExportX509CertificateToDer(*cert.value());
-  if (!cert_bytes) {
-    return cert_bytes.error();
-  }
+  ErrorOr<bssl::UniquePtr<X509>> intermediate_cert_or_error =
+      CreateSelfSignedX509Certificate(
+          "Cast Intermediate", kCertificateDuration, *intermediate_key,
+          GetWallTimeSinceUnixEpoch(), true, root_cert.get(), root_key.get());
+  OSP_DCHECK(intermediate_cert_or_error);
+  bssl::UniquePtr<X509> intermediate_cert =
+      std::move(intermediate_cert_or_error.value());
 
-  // TODO(jophba): either refactor the TLS server socket to use the public key
-  // and add a valid key here, or remove from the TlsCredentials struct.
-  return TlsCredentials(
-      std::vector<uint8_t>(kPrivateKeyDer.begin(), kPrivateKeyDer.end()),
-      std::vector<uint8_t>{}, std::move(cert_bytes.value()));
+  ErrorOr<bssl::UniquePtr<X509>> device_cert_or_error =
+      CreateSelfSignedX509Certificate(endpoint.ToString(), kCertificateDuration,
+                                      *device_key, GetWallTimeSinceUnixEpoch(),
+                                      false, intermediate_cert.get(),
+                                      intermediate_key.get());
+  OSP_DCHECK(device_cert_or_error);
+  bssl::UniquePtr<X509> device_cert = std::move(device_cert_or_error.value());
+
+  // NOTE: Device cert chain plumbing + serialization.
+  credentials_provider->device_creds.private_key = std::move(device_key);
+
+  int cert_length = i2d_X509(device_cert.get(), nullptr);
+  std::string cert_serial(cert_length, 0);
+  uint8_t* out = reinterpret_cast<uint8_t*>(&cert_serial[0]);
+  i2d_X509(device_cert.get(), &out);
+  credentials_provider->device_creds.certs.emplace_back(std::move(cert_serial));
+
+  cert_length = i2d_X509(intermediate_cert.get(), nullptr);
+  cert_serial.resize(cert_length);
+  out = reinterpret_cast<uint8_t*>(&cert_serial[0]);
+  i2d_X509(intermediate_cert.get(), &out);
+  credentials_provider->device_creds.certs.emplace_back(std::move(cert_serial));
+
+  cert_length = i2d_X509(root_cert.get(), nullptr);
+  std::vector<uint8_t> trust_anchor_der(cert_length);
+  out = &trust_anchor_der[0];
+  i2d_X509(root_cert.get(), &out);
+  *root_cert_der = std::move(trust_anchor_der);
+
+  // NOTE: TLS key pair + certificate generation.
+  bssl::UniquePtr<EVP_PKEY> tls_key = GenerateRsaKeyPair();
+  OSP_DCHECK_EQ(EVP_PKEY_id(tls_key.get()), EVP_PKEY_RSA);
+  ErrorOr<bssl::UniquePtr<X509>> tls_cert_or_error =
+      CreateSelfSignedX509Certificate("Test Device TLS", kCertificateDuration,
+                                      *tls_key, GetWallTimeSinceUnixEpoch());
+  OSP_DCHECK(tls_cert_or_error);
+  bssl::UniquePtr<X509> tls_cert = std::move(tls_cert_or_error.value());
+
+  // NOTE: TLS private key serialization.
+  RSA* rsa_key = EVP_PKEY_get0_RSA(tls_key.get());
+  size_t pkey_len = 0;
+  uint8_t* pkey_bytes = nullptr;
+  OSP_DCHECK(RSA_private_key_to_bytes(&pkey_bytes, &pkey_len, rsa_key));
+  OSP_DCHECK_GT(pkey_len, 0u);
+  std::vector<uint8_t> tls_key_serial(pkey_bytes, pkey_bytes + pkey_len);
+  OPENSSL_free(pkey_bytes);
+
+  // NOTE: TLS public key serialization.
+  pkey_len = 0;
+  pkey_bytes = nullptr;
+  OSP_DCHECK(RSA_public_key_to_bytes(&pkey_bytes, &pkey_len, rsa_key));
+  OSP_DCHECK_GT(pkey_len, 0u);
+  std::vector<uint8_t> tls_pub_serial(pkey_bytes, pkey_bytes + pkey_len);
+  OPENSSL_free(pkey_bytes);
+
+  // NOTE: TLS cert serialization.
+  cert_length = 0;
+  cert_length = i2d_X509(tls_cert.get(), nullptr);
+  OSP_DCHECK_GT(cert_length, 0);
+  std::vector<uint8_t> tls_cert_serial(cert_length);
+  out = &tls_cert_serial[0];
+  i2d_X509(tls_cert.get(), &out);
+  credentials_provider->tls_cert_der = tls_cert_serial;
+
+  return TlsCredentials(std::move(tls_key_serial), std::move(tls_pub_serial),
+                        std::move(tls_cert_serial));
 }
 
 }  // namespace
 
 CastAgent::CastAgent(TaskRunner* task_runner, InterfaceInfo interface)
     : task_runner_(task_runner) {
-  // Create the Environment that holds the required injected dependencies
-  // (clock, task runner) used throughout the system, and owns the UDP socket
-  // over which all communication occurs with the Sender.
-  IPEndpoint receive_endpoint{IPAddress::kV4LoopbackAddress, kDefaultCastPort};
-  receive_endpoint.address = interface.GetIpAddressV4()
-                                 ? interface.GetIpAddressV4()
-                                 : interface.GetIpAddressV6();
-  OSP_DCHECK(receive_endpoint.address);
-  environment_ = std::make_unique<Environment>(&Clock::now, task_runner_,
-                                               receive_endpoint);
-  receive_endpoint_ = std::move(receive_endpoint);
+  IPAddress address = interface.GetIpAddressV4() ? interface.GetIpAddressV4()
+                                                 : interface.GetIpAddressV6();
+  OSP_DCHECK(address);
+  environment_ = std::make_unique<Environment>(
+      &Clock::now, task_runner_,
+      IPEndpoint{address, kDefaultCastStreamingPort});
+  receive_endpoint_ = IPEndpoint{address, kDefaultCastPort};
 }
 
 CastAgent::~CastAgent() = default;
@@ -80,22 +150,29 @@ CastAgent::~CastAgent() = default;
 Error CastAgent::Start() {
   OSP_DCHECK(!current_session_);
 
-  task_runner_->PostTask(
-      [this] { this->wake_lock_ = ScopedWakeLock::Create(); });
+  this->wake_lock_ = ScopedWakeLock::Create(task_runner_);
 
   // TODO(jophba): add command line argument for setting the private key.
-  ErrorOr<TlsCredentials> credentials = CreateCredentials(receive_endpoint_);
+  bssl::UniquePtr<EVP_PKEY> pkey;
+  ErrorOr<TlsCredentials> credentials = CreateCredentials(
+      receive_endpoint_, &credentials_provider_, &root_cert_bytes_);
   if (!credentials) {
     return credentials.error();
   }
 
-  // TODO(jophba, rwkeane): begin discovery process before creating TLS
-  // connection factory instance.
-  socket_factory_ =
-      std::make_unique<ReceiverSocketFactory>(this, &message_port_);
+  auth_handler_ = MakeSerialDelete<DeviceAuthNamespaceHandler>(
+      task_runner_, &credentials_provider_);
+  router_ = MakeSerialDelete<VirtualConnectionRouter>(task_runner_,
+                                                      &connection_manager_);
+  router_->AddHandlerForLocalId(kPlatformReceiverId, auth_handler_.get());
+  socket_factory_ = MakeSerialDelete<ReceiverSocketFactory>(task_runner_, this,
+                                                            router_.get());
+  connection_factory_ = SerialDeletePtr<TlsConnectionFactory>(
+      task_runner_,
+      TlsConnectionFactory::CreateFactory(socket_factory_.get(), task_runner_)
+          .release());
+
   task_runner_->PostTask([this, creds = std::move(credentials.value())] {
-    connection_factory_ = TlsConnectionFactory::CreateFactory(
-        socket_factory_.get(), task_runner_);
     connection_factory_->SetListenCredentials(creds);
     connection_factory_->Listen(receive_endpoint_, kDefaultListenOptions);
   });
@@ -105,8 +182,14 @@ Error CastAgent::Start() {
 }
 
 Error CastAgent::Stop() {
-  controller_.reset();
-  current_session_.reset();
+  task_runner_->PostTask([this] {
+    router_.reset();
+    connection_factory_.reset();
+    controller_.reset();
+    current_session_.reset();
+    socket_factory_.reset();
+    wake_lock_.reset();
+  });
   return Error::None();
 }
 
@@ -121,7 +204,8 @@ void CastAgent::OnConnected(ReceiverSocketFactory* factory,
   }
 
   OSP_LOG_INFO << "Received connection from peer at: " << endpoint;
-  message_port_.SetSocket(std::move(socket));
+  message_port_.SetSocket(socket->GetWeakPtr());
+  router_->TakeSocket(this, std::move(socket));
   controller_ =
       std::make_unique<StreamingPlaybackController>(task_runner_, this);
   current_session_ = std::make_unique<ReceiverSession>(
@@ -131,6 +215,17 @@ void CastAgent::OnConnected(ReceiverSocketFactory* factory,
 
 void CastAgent::OnError(ReceiverSocketFactory* factory, Error error) {
   OSP_LOG_ERROR << "Cast agent received socket factory error: " << error;
+  StopCurrentSession();
+}
+
+void CastAgent::OnClose(CastSocket* cast_socket) {
+  OSP_VLOG << "Cast agent socket closed.";
+  StopCurrentSession();
+}
+
+void CastAgent::OnError(CastSocket* socket, Error error) {
+  OSP_LOG_ERROR << "Cast agent received socket error: " << error;
+  StopCurrentSession();
 }
 
 // Currently we don't do anything with the receiver output--the session
@@ -139,23 +234,30 @@ void CastAgent::OnError(ReceiverSocketFactory* factory, Error error) {
 // about the receiver configurations we will have to handle OnNegotiated here.
 void CastAgent::OnNegotiated(const ReceiverSession* session,
                              ReceiverSession::ConfiguredReceivers receivers) {
-  OSP_LOG_INFO << "Successfully negotiated with sender.";
+  OSP_VLOG << "Successfully negotiated with sender.";
 }
 
 void CastAgent::OnConfiguredReceiversDestroyed(const ReceiverSession* session) {
-  OSP_LOG_INFO << "Receiver instances destroyed.";
+  OSP_VLOG << "Receiver instances destroyed.";
 }
 
 // Currently, we just kill the session if an error is encountered.
 void CastAgent::OnError(const ReceiverSession* session, Error error) {
   OSP_LOG_ERROR << "Cast agent received receiver session error: " << error;
-  current_session_.reset();
+  StopCurrentSession();
 }
 
 void CastAgent::OnPlaybackError(StreamingPlaybackController* controller,
                                 Error error) {
   OSP_LOG_ERROR << "Cast agent received playback error: " << error;
+  StopCurrentSession();
+}
+
+void CastAgent::StopCurrentSession() {
+  controller_.reset();
   current_session_.reset();
+  router_->CloseSocket(message_port_.GetSocketId());
+  message_port_.SetSocket(nullptr);
 }
 
 }  // namespace cast
