9f74eeb199374df58f59085c63ae7aabdfaa7bb4
Jordan Bayles <jophba@chromium.org>
2020-04-27 12:02:29 -0700

    Add basic functional tests for the CastAgent
    This patch adds some rudimentary test coverage to ensure that the
    CastAgent in the standalone receiver implementation is not entirely
    broken.
    
    Bug: b/153079471
    Change-Id: Iff26badce61fbded172ff27c490440651909b0c3

--- cast/receiver/channel/testing/device_auth_test_helpers.h
+++ cast/receiver/channel/testing/device_auth_test_helpers.h
@@ -16,23 +16,6 @@
 namespace openscreen {
 namespace cast {
 
-class StaticCredentialsProvider final
-    : public DeviceAuthNamespaceHandler::CredentialsProvider {
- public:
-  StaticCredentialsProvider() = default;
-  ~StaticCredentialsProvider() = default;
-
-  absl::Span<const uint8_t> GetCurrentTlsCertAsDer() override {
-    return absl::Span<uint8_t>(tls_cert_der);
-  }
-  const DeviceCredentials& GetCurrentDeviceCredentials() override {
-    return device_creds;
-  }
-
-  DeviceCredentials device_creds;
-  std::vector<uint8_t> tls_cert_der;
-};
-
 void InitStaticCredentialsFromFiles(StaticCredentialsProvider* creds,
                                     bssl::UniquePtr<X509>* parsed_cert,
                                     TrustStore* fake_trust_store,
--- platform/impl/tls_connection_factory_posix.cc
+++ platform/impl/tls_connection_factory_posix.cc
@@ -106,6 +106,11 @@ void TlsConnectionFactoryPosix::SetListenCredentials(
       ImportRSAPrivateKey(credentials.der_rsa_private_key.data(),
                           credentials.der_rsa_private_key.size());
 
+  OSP_DCHECK(cert);
+  OSP_DCHECK(pkey);
+  OSP_DCHECK(SSL_CTX_use_certificate(ssl_context_.get(), cert.value().get()) == 1);
+  OSP_DCHECK(SSL_CTX_use_PrivateKey(ssl_context_.get(), pkey.value().get()) == 1);
+
   if (!cert || !pkey ||
       SSL_CTX_use_certificate(ssl_context_.get(), cert.value().get()) != 1 ||
       SSL_CTX_use_PrivateKey(ssl_context_.get(), pkey.value().get()) != 1) {
