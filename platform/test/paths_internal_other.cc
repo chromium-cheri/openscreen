7a2c24cfe568ea5e511e0ae4c8eb250c634b75d8
btolsch <btolsch@chromium.org>
2020-04-30 03:32:15 -0700

    Fix android test linking
    This change adds a noop function that isn't used, but is needed for
    linking other test code on android in Chromium.
    
    Bug: 1050913
    Change-Id: Iac9e57ef0b485c351e590da459894341dc6b1871

---
+++ platform/test/paths_internal_other.cc
@@ -0,0 +1,15 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/test/paths_internal.h"
+#include "util/logging.h"
+
+namespace openscreen {
+
+std::string GetExePath() {
+  OSP_NOTREACHED();
+  return {};
+}
+
+}  // namespace openscreen
