d602071c4fe89a6484e72c1132a08ea6f111b6f4
Jordan Bayles <jophba@chromium.org>
2020-06-05 10:11:09 -0700

    Implement Answer parsing
    This patch adds Answer parsing and testing, similar to how Offer
    messages are currently parsed. As part of this work, the following
    improvements are also included:
    
        1. To avoid Abseil usage in public APIs, a new Optional type with
        unit tests is included.
    
        2. message_util.h helpers have been greatly expanded, moved to
        util/json/parsing_helpers.h, and unit tests added.
    
        3. SimpleFraction has been moved from util/ to platform/base/, so it can
        be properly used in public APIs.
    
        4. SessionConfig has been cleaned up to follow coding style guidelines.
    
        5. ANSWER message creation (that encapsulates the Answer struct) has
        been moved to the ReceiverSession.
    
    Bug: b/152633271
    Change-Id: I59c20a140a5174d45378fb9b647ccbe5e6d23d1b

---
+++ platform/base/optional.h
@@ -0,0 +1,83 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PLATFORM_BASE_OPTIONAL_H_
+#define PLATFORM_BASE_OPTIONAL_H_
+
+#include <utility>
+
+namespace openscreen {
+
+// Tag type used for NullOptType construction.
+struct NullOptTypeInit {
+  NullOptTypeInit() = default;
+};
+
+struct NullOptType {
+  // Purposefully not default-constructible.
+  explicit constexpr NullOptType(NullOptTypeInit) noexcept {}
+};
+
+constexpr NullOptType kNullOpt{NullOptTypeInit()};
+
+// This class is meant to be a drop-in replacement for std::optional or
+// Abseil optional. It helps avoid use of abseil header files, and also heap
+// allocations and having to write lots of boilerplate by using either raw
+// pointers or unique pointers, which typically require declaring all of the
+// move/copy assignment/constructors as well as a destructor and reasonable
+// defaults.
+template <typename T>
+class Optional {
+ public:
+  constexpr Optional() noexcept {}
+  explicit constexpr Optional(NullOptType) noexcept {}
+  explicit constexpr Optional(T value)
+      : has_value_(true), value_(std::move(value)) {}
+  Optional(const Optional&) = default;
+  Optional(Optional&&) = default;
+  constexpr Optional& operator=(const Optional&) = default;
+  constexpr Optional& operator=(Optional&&) noexcept = default;
+  constexpr Optional& operator=(NullOptType) noexcept {
+    has_value_ = false;
+    return *this;
+  }
+
+  // Assignment operators for value types.
+  Optional& operator=(T&& value) noexcept {
+    has_value_ = true;
+    value_ = std::move(value);
+    return *this;
+  }
+
+  // Dereferencing operators.
+  constexpr const T* operator->() const { return &value_; }
+  constexpr T* operator->() { return &value_; }
+  ~Optional() {}
+
+  // Value checking.
+  // NOTE: we explicitly avoid providing the bool() operator, to improve
+  // readability and decrease likelihood of tricky states, e.g. if the type
+  // in Optional is also implicit bool, you may not get what you expect in
+  // some cases.
+  constexpr bool has_value() const noexcept { return has_value_; }
+
+  // Value getters.
+  constexpr T& value() { return value_; }
+  constexpr const T& value() const { return value_; }
+
+ private:
+  bool has_value_ = false;
+
+  // Note: we don't want to call the default constructor for T when the
+  // optional class is empty (it may not exist or may be non-trivial).
+  // We use a union here to avoid default construction.
+  union {
+    NullOptType empty_;
+    T value_;
+  };
+};
+
+}  // namespace openscreen
+
+#endif  // PLATFORM_BASE_OPTIONAL_H_
