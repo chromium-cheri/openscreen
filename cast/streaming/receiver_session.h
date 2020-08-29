93210e42a0e30e10a2f7306989fe2e88616414bf
Jordan Bayles <jophba@chromium.org>
2020-08-28 18:04:51 -0700

    Implement sender session class
    This patch adds a new SenderSession class along with unit testing for
    this class.
    
    Bug: b/162542753
    Change-Id: Ia91df728d947e1fa20ed0b360f595f75d41bf11c

--- cast/streaming/receiver_session.h
+++ cast/streaming/receiver_session.h
@@ -13,23 +13,20 @@
 // TODO(jophba): remove public abseil dependencies. Will require modifying
 // either Optional or ConfiguredReceivers, as the compiler currently has an
 // error.
-#include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "cast/streaming/answer_messages.h"
 #include "cast/streaming/message_port.h"
 #include "cast/streaming/offer_messages.h"
 #include "cast/streaming/receiver_packet_router.h"
+#include "cast/streaming/session_base.h"
 #include "cast/streaming/session_config.h"
 #include "util/json/json_serialization.h"
 
 namespace openscreen {
 namespace cast {
 
-class CastSocket;
 class Environment;
 class Receiver;
-class VirtualConnectionRouter;
-struct VirtualConnection;
 
 class ReceiverSession final : public MessagePort::Client {
  public:
@@ -131,13 +128,14 @@ class ReceiverSession final : public MessagePort::Client {
   ~ReceiverSession();
 
   // MessagePort::Client overrides
-  void OnMessage(absl::string_view sender_id,
-                 absl::string_view message_namespace,
-                 absl::string_view message) override;
+  void OnMessage(const std::string& sender_id,
+                 const std::string& message_namespace,
+                 const std::string& message) override;
   void OnError(Error error) override;
 
  private:
-  struct Message {
+  // TODO(jophba): refactor into message channel client?
+  struct JsonMessage {
     const std::string sender_id = {};
     const std::string message_namespace = {};
     const int sequence_number = 0;
@@ -145,7 +143,7 @@ class ReceiverSession final : public MessagePort::Client {
   };
 
   // Specific message type handler methods.
-  void OnOffer(Message* message);
+  void OnOffer(JsonMessage* message);
 
   // Used by SpawnReceivers to generate a receiver for a specific stream.
   std::pair<SessionConfig, std::unique_ptr<Receiver>> ConstructReceiver(
@@ -157,12 +155,12 @@ class ReceiverSession final : public MessagePort::Client {
                                      const VideoStream* video);
 
   // Callers of this method should ensure at least one stream is non-null.
-  Answer ConstructAnswer(Message* message,
+  Answer ConstructAnswer(JsonMessage* message,
                          const AudioStream* audio,
                          const VideoStream* video);
 
   // Sends a message over the message port.
-  void SendMessage(Message* message);
+  void SendMessage(JsonMessage* message);
 
   // Handles resetting receivers and notifying the client.
   void ResetReceivers(Client::ReceiversDestroyingReason reason);
