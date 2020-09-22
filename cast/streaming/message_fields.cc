f73d872cdf8d96090a07d246adb4119618387b1c
Jordan Bayles <jophba@chromium.org>
2020-09-22 13:28:30 -0700

    Cleanup Receiver, ReceiverSession APIs
    This patch does some cleanup to the Receiver and ReceiverSession APIs,
    including:
    
    1. Adding SessionConfig as a property on Receiver.
    2. Change ConfiguredReceivers to behave more like ConfiguredSenders
    3. Removes abseil dependencies
    4. Cleans up AudioCodec, VideoCodec usages
    5. Replaces AudioStream, VideoStream usage with capture options, and
    renames capture options to capture configs.
    
    Bug: b/168651087, b/166640702
    Change-Id: I4cd53a0681fd9756cd0a7556732b2991a0decb63

---
+++ cast/streaming/message_fields.cc
@@ -0,0 +1,74 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cast/streaming/message_fields.h"
+
+#include <array>
+#include <utility>
+
+#include "absl/strings/string_view.h"
+#include "util/osp_logging.h"
+
+namespace openscreen {
+namespace cast {
+namespace {
+
+constexpr std::array<std::pair<absl::string_view, AudioCodec>, 2>
+    kAudioCodecNames{{{"aac", AudioCodec::kAac}, {"opus", AudioCodec::kOpus}}};
+
+constexpr std::array<std::pair<absl::string_view, VideoCodec>, 4>
+    kVideoCodecNames{{{"h264", VideoCodec::kH264},
+                      {"vp8", VideoCodec::kVp8},
+                      {"hevc", VideoCodec::kHevc},
+                      {"vp9", VideoCodec::kVp9}}};
+
+void NotReachedCodec() {
+  OSP_NOTREACHED() << "Codec not accounted for in name array.";
+}
+
+template <typename T, size_t size>
+std::string GetCodecName(
+    const std::array<std::pair<absl::string_view, T>, size>& codecs,
+    T codec) {
+  for (auto pair : codecs) {
+    if (pair.second == codec) {
+      return std::string(pair.first);
+    }
+  }
+  NotReachedCodec();
+  return {};
+}
+
+template <typename T, size_t size>
+T GetCodec(const std::array<std::pair<absl::string_view, T>, size>& codecs,
+           absl::string_view name) {
+  for (auto pair : codecs) {
+    if (pair.first == name) {
+      return pair.second;
+    }
+  }
+  NotReachedCodec();
+  return {};
+}
+
+}  // namespace
+
+std::string CodecToString(AudioCodec codec) {
+  return GetCodecName(kAudioCodecNames, codec);
+}
+
+AudioCodec StringToAudioCodec(const std::string& name) {
+  return GetCodec(kAudioCodecNames, name);
+}
+
+std::string CodecToString(VideoCodec codec) {
+  return GetCodecName(kVideoCodecNames, codec);
+}
+
+VideoCodec StringToVideoCodec(const std::string& name) {
+  return GetCodec(kVideoCodecNames, name);
+}
+
+}  // namespace cast
+}  // namespace openscreen
