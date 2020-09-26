84f1a990f13fc0e787a5d81e5bf82da3dd700e52
Jordan Bayles <jophba@chromium.org>
2020-09-26 12:50:01 -0700

    Add more trace logging to standalone_ impls
    This patch adds additional trace logging to the standalone receiver and
    sender applications, in order to allow more diagnostics and performance
    analysis.
    
    Change-Id: I504f391f1a46b6bf0ac44a74bce244d8d36433aa

--- cast/standalone_receiver/main.cc
+++ cast/standalone_receiver/main.cc
@@ -53,6 +53,7 @@ struct DiscoveryState {
 ErrorOr<std::unique_ptr<DiscoveryState>> StartDiscovery(
     TaskRunner* task_runner,
     const InterfaceInfo& interface) {
+  TRACE_DEFAULT_SCOPED(TraceCategory::kStandaloneReceiver);
   discovery::Config config;
 
   discovery::Config::NetworkInfo::AddressFamilies supported_address_families =
@@ -99,6 +100,7 @@ ErrorOr<std::unique_ptr<DiscoveryState>> StartDiscovery(
 std::unique_ptr<CastAgent> StartCastAgent(TaskRunnerImpl* task_runner,
                                           const InterfaceInfo& interface,
                                           GeneratedCredentials* creds) {
+  TRACE_DEFAULT_SCOPED(TraceCategory::kStandaloneReceiver);
   auto agent = std::make_unique<CastAgent>(
       task_runner, interface, creds->provider.get(), creds->tls_credentials);
   const auto error = agent->Start();
