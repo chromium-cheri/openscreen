a13ba8b97a83eb865d768171ab3875263bd2488a
Jordan Bayles <jophba@chromium.org>
2020-11-09 12:50:44 -0800

    Add JSON schema validation (using valijson)
    This patch adds a dependency on the valijson library, and new JSON
    validation methods in util that use it.
    
    Change-Id: I3607a95dcefb3efff103cc3b5c7c14e8ee2b9739

--- util/json/json_serialization.h
+++ util/json/json_serialization.h
@@ -9,11 +9,9 @@
 
 #include "absl/strings/string_view.h"
 #include "json/value.h"
+#include "platform/base/error.h"
 
 namespace openscreen {
-template <typename T>
-class ErrorOr;
-
 namespace json {
 
 ErrorOr<Json::Value> Parse(absl::string_view value);
---
+++ util/json/json_validation.cc
@@ -0,0 +1,58 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "util/json/json_validation.h"
+
+#include <numeric>
+#include <string>
+
+#include "util/osp_logging.h"
+#include "util/std_util.h"
+#include "util/stringprintf.h"
+#include "valijson/adapters/jsoncpp_adapter.hpp"
+#include "valijson/schema.hpp"
+#include "valijson/schema_parser.hpp"
+#include "valijson/utils/jsoncpp_utils.hpp"
+#include "valijson/validator.hpp"
+
+namespace openscreen {
+namespace json {
+
+namespace {
+
+std::vector<Error> MapErrors(const valijson::ValidationResults& results) {
+  std::vector<Error> errors;
+  for (auto error_it = results.begin(); error_it != results.end(); ++error_it) {
+    const std::string context = Join(error_it->context, ", ");
+    errors.emplace_back(
+        Error{Error::Code::kJsonParseError,
+              StringPrintf("Node: %s, Message: %s", context.c_str(),
+                           error_it->description.c_str())});
+
+    OSP_VLOG << "JsonCpp validation error: "
+             << errors.at(errors.size() - 1).message();
+  }
+  return errors;
+}
+
+}  // anonymous namespace
+
+std::vector<Error> Validate(const Json::Value& document,
+                            const Json::Value& schema_root) {
+  valijson::adapters::JsonCppAdapter adapter(schema_root);
+  valijson::Schema schema;
+  valijson::SchemaParser parser;
+  parser.populateSchema(adapter, schema);
+
+  valijson::Validator validator;
+  valijson::adapters::JsonCppAdapter document_adapter(document);
+  valijson::ValidationResults results;
+  if (validator.validate(schema, document_adapter, &results)) {
+    return {};
+  }
+  return MapErrors(results);
+}
+
+}  // namespace json
+}  // namespace openscreen
