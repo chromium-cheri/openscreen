6c186a4a3a187be36885ffc54f07c9114e0b51cd
Jordan Bayles <jophba@chromium.org>
2020-11-13 11:34:25 -0800

    Port Chrome's enum table to Open Screen
    This patch ports EnumTable from Chrome's components to Open Screen.
    
    Bug: 111
    Change-Id: I92dd781823da71ddfe88902e23da7b53aff2237a

---
+++ util/enum_table.cc
@@ -0,0 +1,43 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "util/enum_table.h"
+
+#include <cstdlib>
+
+namespace openscreen {
+
+#ifdef ARCH_CPU_64_BITS
+// This assertion is pretty paranoid.  It will probably only ever be triggered
+// if someone who doesn't understand how EnumTable works tries to add extra
+// members to GenericEnumTableEntry.
+static_assert(sizeof(GenericEnumTableEntry) == 16,
+              "Instances of GenericEnumTableEntry are too big.");
+#endif
+
+// static
+const GenericEnumTableEntry* GenericEnumTableEntry::FindByString(
+    const std::vector<GenericEnumTableEntry>& data,
+    absl::string_view str) {
+  for (const auto& datum : data) {
+    if (datum.length == str.length() &&
+        std::memcmp(datum.chars, str.data(), str.length()) == 0) {
+      return &datum;
+    }
+  }
+  return nullptr;
+}
+
+// static
+absl::optional<absl::string_view> GenericEnumTableEntry::FindByValue(
+    const std::vector<GenericEnumTableEntry>& data,
+    int value) {
+  for (const auto& datum : data) {
+    if (datum.value == value && datum.has_str())
+      return datum.str();
+  }
+  return absl::nullopt;
+}
+
+}  // namespace openscreen
