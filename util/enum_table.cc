83af14dda25d16cf93c75337d1dde216e05325d9
Jordan Bayles <jophba@chromium.org>
2020-11-14 00:51:14 -0800

    Simplest possible enum_table port
    This patch is a enum_table port, with the simplest possible changes.
    Switching from base types or using alignas incorrectly breaks
    everything. None of it works.
    
    Change-Id: I8c1fba53f82d5f620c63b0270f34b34531309e9d

---
+++ util/enum_table.cc
@@ -0,0 +1,44 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "util/enum_table.h"
+
+#include <cstdlib>
+
+namespace openscreen {
+
+#ifdef ARCH_CPU_64_BITS
+// This assertion is pretty paranoid.  It will probably only ever be triggered
+// if someone who doesn't understand how EnumTable works tries to add extra
+// members to GenericEnumTableEntry.
+static_assert(sizeof(GenericEnumTableEntry) == 16,
+              "Instances of GenericEnumTableEntry are too big.");
+#endif
+
+// static
+const GenericEnumTableEntry* GenericEnumTableEntry::FindByString(
+    const GenericEnumTableEntry data[],
+    std::size_t size,
+    absl::string_view str) {
+  for (std::size_t i = 0; i < size; i++) {
+    if (data[i].length == str.length() &&
+        std::memcmp(data[i].chars, str.data(), str.length()) == 0)
+      return &data[i];
+  }
+  return nullptr;
+}
+
+// static
+absl::optional<absl::string_view> GenericEnumTableEntry::FindByValue(
+    const GenericEnumTableEntry data[],
+    std::size_t size,
+    int value) {
+  for (std::size_t i = 0; i < size; i++) {
+    if (data[i].value == value && data[i].has_str())
+      return data[i].str();
+  }
+  return absl::nullopt;
+}
+
+}  // namespace openscreen
