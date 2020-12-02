16cb5ca380316f2200eeb7b3e205d271ae46512d
Jordan Bayles <jophba@chromium.org>
2020-12-02 07:11:03 -0800

    Add JSON schema validation (using valijson)
    This patch adds a dependency on the valijson library, and new JSON
    validation methods in util that use it.
    
    Change-Id: I3607a95dcefb3efff103cc3b5c7c14e8ee2b9739

--- util/std_util.h
+++ util/std_util.h
@@ -12,6 +12,7 @@
 #include <vector>
 
 #include "absl/algorithm/container.h"
+#include "util/stringprintf.h"
 
 namespace openscreen {
 
@@ -30,6 +31,23 @@ constexpr size_t countof(T (&array)[N]) {
 template <typename CharT, typename Traits, typename Allocator>
 CharT* data(std::basic_string<CharT, Traits, Allocator>& str) {
   return std::addressof(str[0]);
+}
+
+inline std::string Join(const std::vector<std::string>& strings,
+                        const char* delimiter) {
+  size_t size_to_reserve = 0;
+  for (const auto& piece : strings) {
+    size_to_reserve += piece.length();
+  }
+  std::string out;
+  out.reserve(size_to_reserve);
+  auto it = strings.begin();
+  out += *it;
+  for (++it; it != strings.end(); ++it) {
+    out += delimiter + *it;
+  }
+
+  return out;
 }
 
 template <typename Key, typename Value>
