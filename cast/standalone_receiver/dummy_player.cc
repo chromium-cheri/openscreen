TinkerBoard-Android/rockchip-android-external-openscreen

commit c9201dd611b0aa8d66fe7ef2749c7ce0c157f0a1

Move std::chrono using statements to util/
This patch moves openscreen::<time type> using statements to be part of
a new util/ header, chrono_helpers.h. Some helpful convenience
functions are provided for doing conversions, and a new conversion
method, to_duration, is added to the Clock implementation.

Change-Id: I5a153c4da5266bceea97de0cad00a71a739f71ca
Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/2222684
Reviewed-by: mark a. foltz <mfoltz@chromium.org>
Reviewed-by: Ryan Keane <rwkeane@google.com>
Commit-Queue: Jordan Bayles <jophba@chromium.org>

cast/standalone_receiver/dummy_player.cc
@@ -8,6 +8,7 @@
 
 #include "absl/types/span.h"
 #include "cast/streaming/encoded_frame.h"
+#include "util/chrono_helpers.h"
 #include "util/osp_logging.h"
 
 namespace openscreen {
@@ -31,7 +32,7 @@ void DummyPlayer::OnFramesReady(int buffer_size) {
   // Convert the RTP timestamp to a human-readable timestamp (in µs) and log
   // some short information about the frame.
   const auto media_timestamp =
-      frame.rtp_timestamp.ToTimeSinceOrigin<microseconds>(
+      frame.rtp_timestamp.ToTimeSinceOrigin<std::chrono::microseconds>(
           receiver_->rtp_timebase());
   OSP_LOG_INFO << "[SSRC " << receiver_->ssrc() << "] "
                << (frame.dependency == EncodedFrame::KEY_FRAME ? "KEY " : "")

cast/standalone_receiver/sdl_player_base.cc
@@ -6,11 +6,13 @@
 
 #include <chrono>
 #include <sstream>
+#include <utility>
 
 #include "absl/types/span.h"
 #include "cast/standalone_receiver/avcodec_glue.h"
 #include "cast/streaming/encoded_frame.h"
 #include "util/big_endian.h"
+#include "util/chrono_helpers.h"
 #include "util/osp_logging.h"
 #include "util/trace_logging.h"
 
@@ -71,8 +73,7 @@ Clock::time_point SDLPlayerBase::ResyncAndDeterminePresentationTime(
           .ToDuration<Clock::duration>(receiver_->rtp_timebase());
   Clock::time_point presentation_time =
       last_sync_reference_time_ + media_time_since_last_sync;
-  const auto drift =
-      duration_cast<milliseconds>(frame.reference_time - presentation_time);
+  const auto drift = to_microseconds(frame.reference_time - presentation_time);
   if (drift > kMaxPlayoutDrift || drift < -kMaxPlayoutDrift) {
     // Only log if not the very first frame.
     OSP_LOG_IF(INFO, frame.frame_id != FrameId::first())

cast/standalone_sender/main.cc
@@ -4,7 +4,7 @@
 
 #include <getopt.h>
 
-#include <chrono>  // NOLINT
+#include <chrono>
 #include <cinttypes>
 #include <csignal>
 #include <cstdio>
@@ -26,6 +26,7 @@
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
 #include "util/alarm.h"
+#include "util/chrono_helpers.h"
 
 #if defined(CAST_STANDALONE_SENDER_HAVE_EXTERNAL_LIBS)
 #include "cast/standalone_sender/simulated_capturer.h"
@@ -243,9 +244,8 @@ class LoopingFileSender final : public SimulatedAudioCapturer::Client,
 
   void UpdateStatusOnConsole() {
     const Clock::duration elapsed = latest_frame_time_ - capture_start_time_;
-    const auto seconds_part = duration_cast<seconds>(elapsed);
-    const auto millis_part =
-        duration_cast<milliseconds>(elapsed - seconds_part);
+    const auto seconds_part = to_seconds(elapsed);
+    const auto millis_part = to_microseconds(elapsed - seconds_part);
     // The control codes here attempt to erase the current line the cursor is
     // on, and then print out the updated status text. If the terminal does not
     // support simple ANSI escape codes, the following will still work, but
@@ -440,7 +440,7 @@ int StandaloneSenderMain(int argc, char* argv[]) {
 #if defined(CAST_STANDALONE_SENDER_HAVE_EXTERNAL_LIBS)
 
   auto* const task_runner = new TaskRunnerImpl(&Clock::now);
-  PlatformClientPosix::Create(milliseconds{50}, milliseconds{50},
+  PlatformClientPosix::Create(Clock::duration{50}, Clock::duration{50},
                               std::unique_ptr<TaskRunnerImpl>(task_runner));
 
   {

cast/standalone_sender/streaming_opus_encoder.cc
@@ -7,7 +7,7 @@
 #include <opus/opus.h>
 
 #include <algorithm>
-#include <chrono>  // NOLINT
+#include <chrono>
 
 namespace openscreen {
 namespace cast {
@@ -34,7 +34,7 @@ StreamingOpusEncoder::StreamingOpusEncoder(int num_channels,
       sender_(sender),
       samples_per_cast_frame_(sample_rate() / cast_frames_per_second),
       approximate_cast_frame_duration_(
-          duration_cast<Clock::duration>(seconds(1)) / cast_frames_per_second),
+          Clock::to_duration(std::chrono::seconds(1)) / cast_frames_per_second),
       encoder_storage_(new uint8_t[opus_encoder_get_size(num_channels_)]),
       input_(new float[num_channels_ * samples_per_cast_frame_]),
       output_(new uint8_t[kOpusMaxPayloadSize]) {

cast/streaming/sender_packet_router.cc
@@ -9,6 +9,7 @@
 
 #include "cast/streaming/constants.h"
 #include "cast/streaming/packet_util.h"
+#include "util/chrono_helpers.h"
 #include "util/osp_logging.h"
 #include "util/saturate_cast.h"
 #include "util/stringprintf.h"
@@ -223,8 +224,7 @@ int SenderPacketRouter::SendJustTheRtpPackets(Clock::time_point send_time,
 
 namespace {
 constexpr int kBitsPerByte = 8;
-constexpr auto kOneSecondInMilliseconds =
-    duration_cast<milliseconds>(seconds(1));
+constexpr auto kOneSecondInMilliseconds = to_microseconds(seconds(1));
 }  // namespace
 
 // static

cast/streaming/ntp_time_unittest.cc
@@ -4,7 +4,10 @@
 
 #include "cast/streaming/ntp_time.h"
 
+#include <chrono>
+
 #include "gtest/gtest.h"
+#include "util/chrono_helpers.h"
 
 namespace openscreen {
 namespace cast {
@@ -18,8 +21,7 @@ TEST(NtpTimestampTest, SplitsIntoParts) {
   // 1 Jan 1900 plus 10 ms.
   timestamp = UINT64_C(0x00000000028f5c29);
   EXPECT_EQ(NtpSeconds::zero(), NtpSecondsPart(timestamp));
-  EXPECT_EQ(milliseconds(10),
-            duration_cast<milliseconds>(NtpFractionPart(timestamp)));
+  EXPECT_EQ(milliseconds(10), to_microseconds(NtpFractionPart(timestamp)));
 
   // 1 Jan 1970 minus 2^-32 seconds.
   timestamp = UINT64_C(0x83aa7e80ffffffff);
@@ -29,8 +31,7 @@ TEST(NtpTimestampTest, SplitsIntoParts) {
   // 2019-03-23 17:25:50.500.
   timestamp = UINT64_C(0xe0414d0e80000000);
   EXPECT_EQ(NtpSeconds(INT64_C(3762375950)), NtpSecondsPart(timestamp));
-  EXPECT_EQ(milliseconds(500),
-            duration_cast<milliseconds>(NtpFractionPart(timestamp)));
+  EXPECT_EQ(milliseconds(500), to_microseconds(NtpFractionPart(timestamp)));
 }
 
 TEST(NtpTimestampTest, AssemblesFromParts) {
@@ -39,13 +40,14 @@ TEST(NtpTimestampTest, AssemblesFromParts) {
       AssembleNtpTimestamp(NtpSeconds::zero(), NtpFraction::zero());
   EXPECT_EQ(UINT64_C(0x0000000000000000), timestamp);
 
-  // 1 Jan 1900 plus 10 ms. Note that the duration_cast<NtpFraction>(10ms)
-  // truncates rather than rounds the 10ms value, so the resulting timestamp is
-  // one fractional tick less than the one found in the SplitsIntoParts test.
-  // The ~0.4 nanosecond error in the conversion is totally insignificant to a
-  // live system.
+  // 1 Jan 1900 plus 10 ms. Note that the
+  // std::chrono::duration_cast<NtpFraction>(10ms) truncates rather than rounds
+  // the 10ms value, so the resulting timestamp is one fractional tick less than
+  // the one found in the SplitsIntoParts test. The ~0.4 nanosecond error in the
+  // conversion is totally insignificant to a live system.
   timestamp = AssembleNtpTimestamp(
-      NtpSeconds::zero(), duration_cast<NtpFraction>(milliseconds(10)));
+      NtpSeconds::zero(),
+      std::chrono::duration_cast<NtpFraction>(milliseconds(10)));
   EXPECT_EQ(UINT64_C(0x00000000028f5c28), timestamp);
 
   // 1 Jan 1970 minus 2^-32 seconds.
@@ -54,9 +56,9 @@ TEST(NtpTimestampTest, AssemblesFromParts) {
   EXPECT_EQ(UINT64_C(0x83aa7e7fffffffff), timestamp);
 
   // 2019-03-23 17:25:50.500.
-  timestamp =
-      AssembleNtpTimestamp(NtpSeconds(INT64_C(3762375950)),
-                           duration_cast<NtpFraction>(milliseconds(500)));
+  timestamp = AssembleNtpTimestamp(
+      NtpSeconds(INT64_C(3762375950)),
+      std::chrono::duration_cast<NtpFraction>(milliseconds(500)));
   EXPECT_EQ(UINT64_C(0xe0414d0e80000000), timestamp);
 }
 

platform/test/trace_logging_helpers.h
@@ -7,8 +7,11 @@
 #ifndef PLATFORM_TEST_TRACE_LOGGING_HELPERS_H_
 #define PLATFORM_TEST_TRACE_LOGGING_HELPERS_H_
 
+#include <chrono>
+
 #include "gmock/gmock.h"
 #include "platform/base/trace_logging_activation.h"
+#include "util/chrono_helpers.h"
 #include "util/osp_logging.h"
 
 namespace openscreen {
@@ -60,8 +63,7 @@ void ValidateTraceTimestampDiff(const char* name,
                                 Clock::time_point end_time,
                                 TraceIdHierarchy ids,
                                 Error error) {
-  const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
-      end_time - start_time);
+  const auto elapsed = to_microseconds(end_time - start_time);
   ASSERT_GE(static_cast<uint64_t>(elapsed.count()), milliseconds);
 }
 

cast/standalone_receiver/main.cc
@@ -5,7 +5,7 @@
 #include <getopt.h>
 
 #include <array>
-#include <chrono>  // NOLINT
+#include <chrono>
 #include <iostream>
 
 #include "absl/strings/str_cat.h"
@@ -26,6 +26,7 @@
 #include "platform/impl/platform_client_posix.h"
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
+#include "util/chrono_helpers.h"
 #include "util/stringprintf.h"
 #include "util/trace_logging.h"
 
@@ -178,7 +179,7 @@ int RunStandaloneReceiver(int argc, char* argv[]) {
                          : openscreen::LogLevel::kInfo);
 
   auto* const task_runner = new TaskRunnerImpl(&Clock::now);
-  PlatformClientPosix::Create(milliseconds{50}, milliseconds{50},
+  PlatformClientPosix::Create(milliseconds(50), milliseconds(50),
                               std::unique_ptr<TaskRunnerImpl>(task_runner));
 
   auto discovery_state = StartDiscovery(task_runner, interface_info);

chromium-cheri/openscreen

commit 69d49d3a5b370dc9d8c4a45027f81baf7f733b41

Fix some places that became microseconds but are supposed to be millis.
The refactoring in commit c9201dd611b0aa8d66fe7ef2749c7ce0c157f0a1
accidentally replaced some time conversions/definitions that were
milliseconds with microseconds.

Change-Id: Ie7f329060b77064ce41acf72696a99c4f88b2104

cast/standalone_receiver/dummy_player.cc
@@ -32,7 +32,7 @@ void DummyPlayer::OnFramesReady(int buffer_size) {
   // Convert the RTP timestamp to a human-readable timestamp (in µs) and log
   // some short information about the frame.
   const auto media_timestamp =
-      frame.rtp_timestamp.ToTimeSinceOrigin<std::chrono::microseconds>(
+      frame.rtp_timestamp.ToTimeSinceOrigin<microseconds>(
           receiver_->rtp_timebase());
   OSP_LOG_INFO << "[SSRC " << receiver_->ssrc() << "] "
                << (frame.dependency == EncodedFrame::KEY_FRAME ? "KEY " : "")

cast/standalone_receiver/sdl_player_base.cc
@@ -74,7 +74,7 @@ Clock::time_point SDLPlayerBase::ResyncAndDeterminePresentationTime(
           .ToDuration<Clock::duration>(receiver_->rtp_timebase());
   Clock::time_point presentation_time =
       last_sync_reference_time_ + media_time_since_last_sync;
-  const auto drift = to_microseconds(frame.reference_time - presentation_time);
+  const auto drift = to_milliseconds(frame.reference_time - presentation_time);
   if (drift > kMaxPlayoutDrift || drift < -kMaxPlayoutDrift) {
     // Only log if not the very first frame.
     OSP_LOG_IF(INFO, frame.frame_id != FrameId::first())

cast/standalone_sender/looping_file_sender.cc
@@ -132,7 +132,7 @@ void LoopingFileSender::OnVideoFrame(const AVFrame& av_frame,
 void LoopingFileSender::UpdateStatusOnConsole() {
   const Clock::duration elapsed = latest_frame_time_ - capture_start_time_;
   const auto seconds_part = to_seconds(elapsed);
-  const auto millis_part = to_microseconds(elapsed - seconds_part);
+  const auto millis_part = to_milliseconds(elapsed - seconds_part);
   // The control codes here attempt to erase the current line the cursor is
   // on, and then print out the updated status text. If the terminal does not
   // support simple ANSI escape codes, the following will still work, but

cast/standalone_sender/main.cc
@@ -26,6 +26,7 @@
 #include "platform/impl/platform_client_posix.h"
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
+#include "util/chrono_helpers.h"
 #include "util/stringprintf.h"
 
 namespace openscreen {
@@ -173,7 +174,7 @@ int StandaloneSenderMain(int argc, char* argv[]) {
 #endif
 
   auto* const task_runner = new TaskRunnerImpl(&Clock::now);
-  PlatformClientPosix::Create(Clock::duration{50}, Clock::duration{50},
+  PlatformClientPosix::Create(milliseconds(50), milliseconds(50),
                               std::unique_ptr<TaskRunnerImpl>(task_runner));
 
   IPEndpoint remote_endpoint = ParseAsEndpoint(iface_or_endpoint);

cast/standalone_sender/streaming_opus_encoder.cc
@@ -35,8 +35,8 @@ StreamingOpusEncoder::StreamingOpusEncoder(int num_channels,
     : num_channels_(num_channels),
       sender_(sender),
       samples_per_cast_frame_(sample_rate() / cast_frames_per_second),
-      approximate_cast_frame_duration_(
-          Clock::to_duration(std::chrono::seconds(1)) / cast_frames_per_second),
+      approximate_cast_frame_duration_(Clock::to_duration(seconds(1)) /
+                                       cast_frames_per_second),
       encoder_storage_(new uint8_t[opus_encoder_get_size(num_channels_)]),
       input_(new float[num_channels_ * samples_per_cast_frame_]),
       output_(new uint8_t[kOpusMaxPayloadSize]) {

cast/streaming/sender_packet_router.cc
@@ -224,7 +224,7 @@ int SenderPacketRouter::SendJustTheRtpPackets(Clock::time_point send_time,
 
 namespace {
 constexpr int kBitsPerByte = 8;
-constexpr auto kOneSecondInMilliseconds = to_microseconds(seconds(1));
+constexpr auto kOneSecondInMilliseconds = to_milliseconds(seconds(1));
 }  // namespace
 
 // static

cast/streaming/ntp_time_unittest.cc
@@ -21,7 +21,7 @@ TEST(NtpTimestampTest, SplitsIntoParts) {
   // 1 Jan 1900 plus 10 ms.
   timestamp = UINT64_C(0x00000000028f5c29);
   EXPECT_EQ(NtpSeconds::zero(), NtpSecondsPart(timestamp));
-  EXPECT_EQ(milliseconds(10), to_microseconds(NtpFractionPart(timestamp)));
+  EXPECT_EQ(milliseconds(10), to_milliseconds(NtpFractionPart(timestamp)));
 
   // 1 Jan 1970 minus 2^-32 seconds.
   timestamp = UINT64_C(0x83aa7e80ffffffff);
@@ -31,7 +31,7 @@ TEST(NtpTimestampTest, SplitsIntoParts) {
   // 2019-03-23 17:25:50.500.
   timestamp = UINT64_C(0xe0414d0e80000000);
   EXPECT_EQ(NtpSeconds(INT64_C(3762375950)), NtpSecondsPart(timestamp));
-  EXPECT_EQ(milliseconds(500), to_microseconds(NtpFractionPart(timestamp)));
+  EXPECT_EQ(milliseconds(500), to_milliseconds(NtpFractionPart(timestamp)));
 }
 
 TEST(NtpTimestampTest, AssemblesFromParts) {
@@ -40,11 +40,11 @@ TEST(NtpTimestampTest, AssemblesFromParts) {
       AssembleNtpTimestamp(NtpSeconds::zero(), NtpFraction::zero());
   EXPECT_EQ(UINT64_C(0x0000000000000000), timestamp);
 
-  // 1 Jan 1900 plus 10 ms. Note that the
-  // std::chrono::duration_cast<NtpFraction>(10ms) truncates rather than rounds
-  // the 10ms value, so the resulting timestamp is one fractional tick less than
-  // the one found in the SplitsIntoParts test. The ~0.4 nanosecond error in the
-  // conversion is totally insignificant to a live system.
+  // 1 Jan 1900 plus 10 ms. Note that the duration_cast<NtpFraction>(10ms)
+  // truncates rather than rounds the 10ms value, so the resulting timestamp is
+  // one fractional tick less than the one found in the SplitsIntoParts test.
+  // The ~0.4 nanosecond error in the conversion is totally insignificant to a
+  // live system.
   timestamp = AssembleNtpTimestamp(
       NtpSeconds::zero(),
       std::chrono::duration_cast<NtpFraction>(milliseconds(10)));
@@ -70,7 +70,7 @@ TEST(NtpTimeConverterTest, ConvertsToNtpTimeAndBack) {
   // our core assumptions (or the design) about the time math are wrong and
   // should be looked into!
   const Clock::time_point steady_clock_start = Clock::now();
-  const std::chrono::seconds wall_clock_start = GetWallTimeSinceUnixEpoch();
+  const seconds wall_clock_start = GetWallTimeSinceUnixEpoch();
   SCOPED_TRACE(::testing::Message()
                << "steady_clock_start.time_since_epoch().count() is "
                << steady_clock_start.time_since_epoch().count()

platform/test/trace_logging_helpers.h
@@ -63,7 +63,7 @@ void ValidateTraceTimestampDiff(const char* name,
                                 Clock::time_point end_time,
                                 TraceIdHierarchy ids,
                                 Error error) {
-  const auto elapsed = to_microseconds(end_time - start_time);
+  const auto elapsed = to_milliseconds(end_time - start_time);
   ASSERT_GE(static_cast<uint64_t>(elapsed.count()), milliseconds);
 }
 