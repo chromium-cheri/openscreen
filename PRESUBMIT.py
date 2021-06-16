00faea821050311a4947048592c493d7a9041071
Jordan Bayles <jophba@chromium.org>
2021-06-16 14:18:26 -0700

    Fix GN check issues and add to PRESUBMIT
    This patch fixes various issues found through `gn check` throughout the
    repository, and adds gn check as a PRESUBMIT step.
    
    Bug: b/155434332
    Change-Id: I2dffa42f5dfd291a7c59653eb7ca16ec680bfddb

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -5,9 +5,18 @@
 import os
 import re
 import sys
+import subprocess
+import shutil
+import tempfile
 
 _REPO_PATH = os.path.dirname(os.path.realpath('__file__'))
 _IMPORT_SUBFOLDERS = ['tools', os.path.join('buildtools', 'checkdeps')]
+
+# GN_ERROR_RE matches the summary of an error output by `gn check`.
+# Matches "ERROR" and following lines until it sees an empty line or a line
+# containing just underscores.
+GN_ERROR_RE = re.compile(
+    r'ERROR at ([^:]+:\d+:\d+): ([^\n]*)([\s\S]+?)((?=\n_+\n)|(?=$))')
 
 # git-cl upload is not compatible with __init__.py based subfolder imports, so
 # we extend the system path instead.
@@ -41,6 +50,34 @@ _EXCLUDED_PATHS = (
   r'.+\.patch$',
 )
 
+
+def _IsRealDepotTools(path):
+  return os.path.isfile(os.path.join(path, 'gclient.py'))
+
+
+def _GetDepotToolsPath():
+  """Search for depot_tools and add it to sys.path."""
+  # First look if depot_tools is already in PYTHONPATH.
+  for i in sys.path:
+    if i.rstrip(os.sep).endswith('depot_tools') and _IsRealDepotTools(i):
+      return i
+  # Then look if depot_tools is in PATH, common case.
+  for i in os.environ['PATH'].split(os.pathsep):
+    if _IsRealDepotTools(i):
+      return i
+  # Rare case, it's not even in PATH, look upward up to root.
+  root_dir = os.path.dirname(os.path.abspath(__file__))
+  previous_dir = os.path.abspath(__file__)
+  while root_dir and root_dir != previous_dir:
+    i = os.path.join(root_dir, 'depot_tools')
+    if _IsRealDepotTools(i):
+      return i
+    previous_dir = root_dir
+    root_dir = os.path.dirname(root_dir)
+  print >> sys.stderr, 'Failed to find depot_tools'
+  return None
+
+DEPOT_TOOLS_PATH = _GetDepotToolsPath()
 
 def _CheckLicenses(input_api, output_api):
     """Checks third party licenses and returns a list of violations."""
@@ -127,7 +164,7 @@ def _CheckNoexceptOnMove(filename, clean_lines, linenum, error):
 # - We disable whitespace/braces because of various false positives.
 # - There are some false positives with 'explicit' checks, but it's useful
 #   enough to keep.
-# - We add a custom check for 'noexcept' usage.
+# - We have additional custom checks for 'noexcept' and DCHECK usage.
 def _CheckChangeLintsClean(input_api, output_api):
     """Checks that all '.cc' and '.h' files pass cpplint.py."""
     cpplint = input_api.cpplint
@@ -152,6 +189,37 @@ def _CheckChangeLintsClean(input_api, output_api):
         return [res_type('Changelist failed cpplint.py check.')]
 
     return []
+
+
+def _RunGnCommand(args, root_dir=None):
+  """Runs `gn` with provided args and return error if any."""
+  try:
+    command = [
+      sys.executable,
+      os.path.join(DEPOT_TOOLS_PATH, 'gn.py')
+    ] + args
+    subprocess.check_output(command, cwd=root_dir)
+  except subprocess.CalledProcessError as err:
+    return err.output.decode('utf-8')
+  return None
+
+def _GnCheck(output_api, root_dir=None):
+  """Runs `gn gen --check` with default args to detect mismatches between
+  #includes and dependencies in the BUILD.gn files, as well as general build
+  errors.
+
+  Returns a list of error summary strings.
+  """
+  out_dir = tempfile.mkdtemp('gn')
+  try:
+    output = _RunGnCommand(['gen', '--check', out_dir], root_dir)
+  finally:
+    shutil.rmtree(out_dir, ignore_errors=True)
+
+  if not output:
+      return []
+  return [output_api.PresubmitError(e[1], [e[0]], e[2])
+            for e in GN_ERROR_RE.findall(output)]
 
 
 def _CommonChecks(input_api, output_api):
@@ -204,6 +272,9 @@ def _CommonChecks(input_api, output_api):
     licenses.PRUNE_PATHS.add(os.path.join('buildtools', 'third_party'));
     results.extend(_CheckLicenses(input_api, output_api))
 
+    # Run `gn check` on code change.
+    results.extend(_GnCheck(output_api))
+
     return results
 
 
@@ -211,8 +282,10 @@ def CheckChangeOnUpload(input_api, output_api):
     input_api.DEFAULT_FILES_TO_SKIP = _EXCLUDED_PATHS
     # We always run the OnCommit checks, as well as some additional checks.
     results = CheckChangeOnCommit(input_api, output_api)
-    results.extend(
-        input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api))
+
+    # TODO(crbug.com/1220846): Open Screen needs a `main` config_set.
+    #results.extend(
+    #    input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api))
     return results
 
 
