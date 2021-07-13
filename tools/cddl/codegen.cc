c36780b61a8de225043b673a0072df53913e87f8
mark a. foltz <mfoltz@chromium.org>
2021-07-13 10:08:07 -0700

    [Open Screen] Fix lint errors in tools/cddl.
    Fixes lint errors in tools/cddl.
    
    Change-Id: Ib309e1d1ab99b097399cdb9a9c01bc1931a44573

--- tools/cddl/codegen.cc
+++ tools/cddl/codegen.cc
@@ -7,6 +7,7 @@
 #include <cinttypes>
 #include <iostream>
 #include <limits>
+#include <memory>
 #include <set>
 #include <sstream>
 #include <string>
@@ -1589,12 +1590,11 @@ namespace {
 #define CBOR_RETURN_WHAT_ON_ERROR(stmt, what)                           \
   {                                                                     \
     CborError error = stmt;                                             \
-    /* Encoder-specific errors, so it's fine to check these even in the \
-     * parser.                                                          \
-     */                                                                 \
-    OSP_DCHECK_NE(error, CborErrorTooFewItems);                             \
-    OSP_DCHECK_NE(error, CborErrorTooManyItems);                            \
-    OSP_DCHECK_NE(error, CborErrorDataTooLarge);                            \
+    // Encoder-specific errors, so it's fine to check these even in the \
+    // parser.                                                          \
+    OSP_DCHECK_NE(error, CborErrorTooFewItems);                         \
+    OSP_DCHECK_NE(error, CborErrorTooManyItems);                        \
+    OSP_DCHECK_NE(error, CborErrorDataTooLarge);                        \
     if (error != CborNoError && error != CborErrorOutOfMemory)          \
       return what;                                                      \
   }
