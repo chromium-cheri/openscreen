e7f1020ab276d362a983671f5232f771b4060d88
Jordan Bayles <jophba@chromium.org>
2021-07-16 11:33:03 -0700

    [Cast Standalone] Implement Play/Pause
    This patch adds play/pause support to the standalone receiver and sender
    classes while remoting.
    
    The implementation uses SDL keyboard event bindings to pick up on spacebar
    presses when the SDL player window (e.g. the standalone receiver video)
    is in focus. This event binding results in a SetPlaybackRate message being
    sent over the RPCMessenger, and then the standalone sender class uses the
    message's double value to toggle playback on the simulated capturer
    implementations.
    
    While falling far short of a full media player implementation, this patch
    may help embedders see the basics of how RPC messenging and keeping the
    sender and receiver media streams in sync can behave.
    
    Change-Id: I4d2e1ed7f6d69f8a24385c9139320492c2101600

--- cast/standalone_sender/looping_file_cast_agent.cc
+++ cast/standalone_sender/looping_file_cast_agent.cc
@@ -272,7 +272,7 @@ void LoopingFileCastAgent::CreateAndStartSession() {
   if (connection_settings_->use_remoting) {
     remoting_sender_ = std::make_unique<RemotingSender>(
         current_session_->rpc_messenger(), AudioCodec::kOpus, VideoCodec::kVp8,
-        [this]() { OnRemotingReceiverReady(); });
+        this);
 
     negotiation_error =
         current_session_->NegotiateRemoting(audio_config, video_config);
@@ -320,11 +320,15 @@ void LoopingFileCastAgent::OnError(const SenderSession* session, Error error) {
   Shutdown();
 }
 
-void LoopingFileCastAgent::OnRemotingReceiverReady() {
+void LoopingFileCastAgent::OnReady() {
   is_ready_for_remoting_ = true;
   if (current_negotiation_) {
     StartRemotingSenders();
   }
+}
+
+void LoopingFileCastAgent::OnPlaybackRateChange(double rate) {
+  file_sender_->SetPlaybackRate(rate);
 }
 
 void LoopingFileCastAgent::StartRemotingSenders() {
