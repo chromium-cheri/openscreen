e7576e869a749d735d4af66e90cbe797cbb606f6
mark a. foltz <mfoltz@chromium.org>
2021-09-13 12:34:51 -0700

    [Open Screen] Add API for use of Cast Streaming App IDs.
    This API hides the specific application ID constants for Cast Streaming.
    It can be used in Chromium or other embedders whenever they need to know
    the specific App IDs for streaming.
    
    Bug: b/199543768
    Change-Id: I43a87d14385e33c4aed60feef07a8c2a4beb2032

---
+++ cast/common/public/cast_streaming_app_ids.h
@@ -0,0 +1,32 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CAST_COMMON_PUBLIC_CAST_STREAMING_APP_IDS_H_
+#define CAST_COMMON_PUBLIC_CAST_STREAMING_APP_IDS_H_
+
+#include <string>
+#include <vector>
+
+namespace openscreen {
+namespace cast {
+
+// Returns true only if |app_id| matches the Cast application ID for the
+// corresponding Cast Streaming receiver application.
+bool IsCastStreamingAppId(const std::string& app_id);
+bool IsCastStreamingAudioVideoAppId(const std::string& app_id);
+bool IsCastStreamingAudioOnlyAppId(const std::string& app_id);
+
+// Returns all app IDs for Cast Streaming receivers.
+std::vector<std::string> GetCastStreamingAppIds();
+
+// Returns the app ID for the audio and video streaming receiver.
+const char* GetCastStreamingAudioVideoAppId();
+
+// Returns the app ID for the audio-only streaming receiver.
+const char* GetCastStreamingAudioOnlyAppId();
+
+}  // namespace cast
+}  // namespace openscreen
+
+#endif  // CAST_COMMON_PUBLIC_CAST_STREAMING_APP_IDS_H_
