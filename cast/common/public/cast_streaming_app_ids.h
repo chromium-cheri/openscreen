5d0a15e92c9c3573c4dcdc437f3b69740aa95da4
Ryan Keane <rwkeane@google.com>
2021-10-29 13:00:55 -0700

    Add new app ids for mirroring
    Different AppIds from the currently accounted for chromium app ids are
    used for other mirroring sources - currently limited to those for
    android and for a single iOS and Android app. This CL adds them as
    constants
    
    Bug: b/204372019, b/203474951
    Change-Id: I13ec482b74e0f0d1931dbabb522f33e8b91c40f8

--- cast/common/public/cast_streaming_app_ids.h
+++ cast/common/public/cast_streaming_app_ids.h
@@ -12,10 +12,16 @@ namespace openscreen {
 namespace cast {
 
 // Returns true only if |app_id| matches the Cast application ID for the
-// corresponding Cast Streaming receiver application.
+// corresponding Chromium Cast Streaming receiver application.
+//
+// TODO(b/204583004): Use absl::string_view instead of std::string so that these
+// comparisons can be made constexpr.
 bool IsCastStreamingAppId(const std::string& app_id);
 bool IsCastStreamingAudioVideoAppId(const std::string& app_id);
 bool IsCastStreamingAudioOnlyAppId(const std::string& app_id);
+
+// Returns true only if |app_id| matches any Cast application ID.
+bool IsCastStreamingReceiverAppId(const std::string& app_id);
 
 // Returns all app IDs for Cast Streaming receivers.
 std::vector<std::string> GetCastStreamingAppIds();
@@ -28,6 +34,26 @@ constexpr const char* GetCastStreamingAudioVideoAppId() {
 // Returns the app ID for the audio-only streaming receiver.
 constexpr const char* GetCastStreamingAudioOnlyAppId() {
   return "85CDB22F";
+}
+
+// Returns the app ID for the audio and video streaming receiver.
+constexpr const char* GetAndroidMirroringAudioVideoAppId() {
+  return "674A0243";
+}
+
+// Returns the app ID for the audio-only streaming receiver.
+constexpr const char* GetAndroidMirroringAudioOnlyAppId() {
+  return "8E6C866D";
+}
+
+// Returns the app ID for the audio and video streaming receiver.
+constexpr const char* GetAndroidAppStreamingAudioVideoAppId() {
+  return "96084372";
+}
+
+// Returns the app ID for the audio-only streaming receiver.
+constexpr const char* GetIosAppStreamingAudioVideoAppId() {
+  return "BFD92C23";
 }
 
 }  // namespace cast
