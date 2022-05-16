8211c53f49966a95273d82045e7f8737e4d0c19f
Jordan Bayles <jophba@chromium.org>
2022-05-16 12:58:19 -0700

    [Libcast-In-Chrome #1.1] Refactors to support API
    Patch:
    This is a refactor patch to allow Chrome to handle conversions to
    openscreen types, such as creating encoded frames.
    
    Overall feature:
    Libcast-In-Chrome is a project with the goal of having libcast be the
    backing implementation of cast streaming in Chrome.
    
    Bug: 1212803
    Change-Id: If538cef9187b02ca74c61d82a4910a0c50086ba6

--- cast/streaming/frame_id.h
+++ cast/streaming/frame_id.h
@@ -41,6 +41,8 @@ class FrameId : public ExpandedValueBase<int64_t, FrameId> {
   // been set and/or a "not applicable" indicator.
   constexpr FrameId() : FrameId(std::numeric_limits<int64_t>::min()) {}
 
+  constexpr explicit FrameId(int64_t value) : ExpandedValueBase(value) {}
+
   // Allow copy construction and assignment.
   constexpr FrameId(const FrameId&) = default;
   constexpr FrameId& operator=(const FrameId&) = default;
@@ -104,14 +106,11 @@ class FrameId : public ExpandedValueBase<int64_t, FrameId> {
   // begins.
   static constexpr FrameId leader() { return FrameId(-1); }
 
+  constexpr int64_t value() const { return value_; }
+
  private:
   friend class ExpandedValueBase<int64_t, FrameId>;
   friend std::ostream& operator<<(std::ostream& out, const FrameId rhs);
-
-  constexpr explicit FrameId(int64_t value) : ExpandedValueBase(value) {}
-
-  // Accessor used by ostream output function.
-  constexpr int64_t value() const { return value_; }
 };
 
 }  // namespace cast
