5e8c19c051c594c41c2ca4416280a6d47216291f
mark a. foltz <mfoltz@chromium.org>
2023-11-06 21:47:19 -0800

    [Open Screen] Add string utilities.
    These utilities replace the use of ascii.h and match.h from Abseil.
    
    Change-Id: I599752ea05d82446144c55ff6c1bd37f87685888

---
+++ util/string_util_unittest.cc
@@ -0,0 +1,70 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "util/string_util.h"
+
+#include "gtest/gtest.h"
+
+namespace openscreen::string_util {
+
+// Reference: https://ascii-code.com
+TEST(StringUtilTest, AsciiTest) {
+  constexpr char kAlpha[] = "aAzZ";
+  constexpr char kDigits[] = "09";
+  constexpr char kPrintable[] = "*&$^ ";
+  constexpr char kNonPrintable[] = "\000\010\015\177\202";
+
+  for (size_t i = 0; i < sizeof(kAlpha) - 1; i++) {
+    EXPECT_TRUE(ascii_isalpha(kAlpha[i])) << i;
+    EXPECT_FALSE(ascii_isdigit(kAlpha[i])) << i;
+    EXPECT_TRUE(ascii_isprint(kAlpha[i])) << i;
+  }
+
+  for (size_t i = 0; i < sizeof(kDigits) - 1; i++) {
+    EXPECT_FALSE(ascii_isalpha(kDigits[i])) << i;
+    EXPECT_TRUE(ascii_isdigit(kDigits[i])) << i;
+    EXPECT_TRUE(ascii_isprint(kDigits[i])) << i;
+  }
+
+  for (size_t i = 0; i < sizeof(kPrintable) - 1; i++) {
+    EXPECT_FALSE(ascii_isalpha(kPrintable[i])) << i;
+    EXPECT_FALSE(ascii_isdigit(kPrintable[i])) << i;
+    EXPECT_TRUE(ascii_isprint(kPrintable[i])) << i;
+  }
+
+  for (size_t i = 0; i < sizeof(kNonPrintable) - 1; i++) {
+    EXPECT_FALSE(ascii_isalpha(kNonPrintable[i])) << i;
+    EXPECT_FALSE(ascii_isdigit(kNonPrintable[i])) << i;
+    EXPECT_FALSE(ascii_isprint(kNonPrintable[i])) << i;
+  }
+
+  EXPECT_EQ(ascii_tolower('A'), 'a');
+  EXPECT_EQ(ascii_tolower('a'), 'a');
+  EXPECT_EQ(ascii_tolower('0'), '0');
+}
+
+TEST(StringUtilTest, StartsAndEndsWith) {
+  constexpr char kString[] = "United Federation of Planets";
+  EXPECT_TRUE(starts_with("", ""));
+  EXPECT_TRUE(starts_with(kString, ""));
+  EXPECT_TRUE(starts_with(kString, "United"));
+  EXPECT_FALSE(starts_with(kString, "Klingons"));
+
+  EXPECT_TRUE(ends_with("", ""));
+  EXPECT_TRUE(ends_with(kString, ""));
+  EXPECT_TRUE(ends_with(kString, "Planets"));
+  EXPECT_FALSE(ends_with(kString, "Borg"));
+}
+
+TEST(StringUtilTest, EqualsIgnoreCase) {
+  constexpr char kString[] = "Vulcans!";
+  EXPECT_TRUE(EqualsIgnoreCase("", ""));
+  EXPECT_FALSE(EqualsIgnoreCase("", kString));
+  EXPECT_FALSE(EqualsIgnoreCase("planet vulcan", kString));
+  EXPECT_TRUE(EqualsIgnoreCase("Vulcans!", kString));
+  EXPECT_TRUE(EqualsIgnoreCase("vUlCaNs!", kString));
+  EXPECT_FALSE(EqualsIgnoreCase("vUlKaNs!", kString));
+}
+
+}  // namespace openscreen::string_util
