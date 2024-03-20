4d9909052256c5c30f94cdfa5b9784315bed8de9
Wei4 Wang <wei4.wang@intel.com>
2024-03-20 16:53:43 +0800

    [OpenScreen] Remove unnecessary explicitly define constructor for structs
    This CL removes unnecessary explicitly define constructor for structs,
    so that they can be aggregate initialized.
    
    Bug: none
    Change-Id: I4786ad53e6eb686196ce6fa7849d71f7d936e58b

--- osp/impl/quic/quic_service_common.h
+++ osp/impl/quic/quic_service_common.h
@@ -55,12 +55,6 @@ class QuicProtocolConnection final : public ProtocolConnection {
 };
 
 struct ServiceStreamPair {
-  ServiceStreamPair(QuicStream* stream,
-                    QuicProtocolConnection* protocol_connection);
-  ~ServiceStreamPair();
-  ServiceStreamPair(ServiceStreamPair&&) noexcept;
-  ServiceStreamPair& operator=(ServiceStreamPair&&) noexcept;
-
   QuicStream* stream;
   uint64_t protocol_connection_id;
   QuicProtocolConnection* protocol_connection;
@@ -89,7 +83,7 @@ class ServiceConnectionDelegate final : public QuicConnection::Delegate,
                             const IPEndpoint& endpoint);
   ~ServiceConnectionDelegate() override;
 
-  void AddStreamPair(ServiceStreamPair&& stream_pair);
+  void AddStreamPair(const ServiceStreamPair& stream_pair);
   void DropProtocolConnection(QuicProtocolConnection* connection);
 
   // This should be called at the end of each event loop that effects this
@@ -123,9 +117,8 @@ class ServiceConnectionDelegate final : public QuicConnection::Delegate,
 };
 
 struct ServiceConnectionData {
-  explicit ServiceConnectionData(
-      std::unique_ptr<QuicConnection> connection,
-      std::unique_ptr<ServiceConnectionDelegate> delegate);
+  ServiceConnectionData(std::unique_ptr<QuicConnection> connection,
+                        std::unique_ptr<ServiceConnectionDelegate> delegate);
   ServiceConnectionData(ServiceConnectionData&&) noexcept;
   ~ServiceConnectionData();
   ServiceConnectionData& operator=(ServiceConnectionData&&) noexcept;
