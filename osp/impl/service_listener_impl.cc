067fe91ab90e4755556eb940a5f9fadcf58cd782
Wei4 Wang <wei4.wang@intel.com>
2024-04-09 14:48:05 +0800

    [OpenScreen] Pass Error instances by const reference whenever possible
    Class Error has a std::string member, so it is better to pass Error
    instances by const reference whenever possible to avoid copying
    std::string even through the std::string may be short.
    
    Bug: none
    Change-Id: I1a409eddfd4b4fc6126366be8699f8cbee824b79

--- osp/impl/service_listener_impl.cc
+++ osp/impl/service_listener_impl.cc
@@ -133,7 +133,7 @@ void ServiceListenerImpl::OnAllReceiversRemoved() {
   }
 }
 
-void ServiceListenerImpl::OnError(Error error) {
+void ServiceListenerImpl::OnError(const Error& error) {
   last_error_ = error;
   for (auto* observer : observers_) {
     observer->OnError(error);
@@ -206,11 +206,11 @@ const std::vector<ServiceInfo>& ServiceListenerImpl::GetReceivers() const {
   return receiver_list_.receivers();
 }
 
-void ServiceListenerImpl::OnFatalError(Error error) {
+void ServiceListenerImpl::OnFatalError(const Error& error) {
   OnError(error);
 }
 
-void ServiceListenerImpl::OnRecoverableError(Error error) {
+void ServiceListenerImpl::OnRecoverableError(const Error& error) {
   OnError(error);
 }
 
