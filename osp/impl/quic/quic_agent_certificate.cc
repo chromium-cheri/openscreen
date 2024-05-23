7dad7a4eb97f6cf6c2a3c1de9ebdc25d830146e9
Wei4 Wang <wei4.wang@intel.com>
2024-05-23 13:20:04 +0800

    [OpenScreen] Add AgentCertificate class
    This CL adds AgentCertificate class to cetrally deal with certificate
    related work. Then the fingerprint can be obtained by calling
    QuicServer::GetAgentCertificate().GetFingerprint().
    
    Bug: 300236996
    Change-Id: I60faf042d57d00c5f8c233a17f50b949c7d7e010

---
+++ osp/impl/quic/quic_agent_certificate.cc
@@ -0,0 +1,83 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "osp/impl/quic/quic_agent_certificate.h"
+
+#include <utility>
+
+#include "util/crypto/pem_helpers.h"
+#include "util/osp_logging.h"
+#include "util/read_file.h"
+
+namespace openscreen::osp {
+
+namespace {
+
+// TODO(issuetracker.google.com/300236996): Replace with OSP certificate
+// generation. Fixed agent certificate is used currently.
+constexpr char kCertificatesPath[] =
+    "osp/impl/quic/certificates/openscreen.pem";
+constexpr char kPrivateKeyPath[] = "osp/impl/quic/certificates/openscreen.key";
+constexpr char kFingerPrint[] =
+    "50:87:8D:CA:1B:9B:67:76:CB:87:88:1C:43:20:82:7A:91:F5:9B:74:4D:85:95:D0:"
+    "76:E6:0B:50:7F:D3:29:D9";
+
+}  // namespace
+
+QuicAgentCertificate::QuicAgentCertificate() {
+  bool result = LoadAgentCertificate(kCertificatesPath);
+  OSP_CHECK(result);
+  result = LoadPrivateKey(kPrivateKeyPath);
+  OSP_CHECK(result);
+  agent_fingerprint_ = kFingerPrint;
+}
+
+QuicAgentCertificate::~QuicAgentCertificate() = default;
+
+std::unique_ptr<quic::ProofSource> QuicAgentCertificate::CreateProofSource() {
+  if (certificates_.empty() || key_raw_.empty()) {
+    return nullptr;
+  }
+
+  auto chain = quiche::QuicheReferenceCountedPointer<quic::ProofSource::Chain>(
+      new quic::ProofSource::Chain(certificates_));
+  OSP_CHECK(chain) << "Failed to create the quic::ProofSource::Chain.";
+
+  std::unique_ptr<quic::CertificatePrivateKey> key =
+      quic::CertificatePrivateKey::LoadFromDer(key_raw_);
+  OSP_CHECK(key) << "Failed to parse the key file.";
+
+  return quic::ProofSourceX509::Create(std::move(chain), std::move(*key));
+}
+
+bool QuicAgentCertificate::LoadAgentCertificate(std::string_view filename) {
+  if (certificates_.empty()) {
+    // NOTE: There are currently some spec issues about certificates that are
+    // still under discussion. Add validations to check if this is a valid OSP
+    // agent certificate one all the issues are closed.
+    certificates_ = ReadCertificatesFromPemFile(filename);
+  }
+
+  return certificates_.empty() ? false : true;
+}
+
+bool QuicAgentCertificate::LoadPrivateKey(std::string_view filename) {
+  if (key_raw_.empty()) {
+    key_raw_ = ReadEntireFileToString(filename);
+  }
+
+  return key_raw_.empty() ? false : true;
+}
+
+// NOTE: OSP certificate generation is not implemented yet and fixed certificate
+// is used currently. So rotate agent certificate is not supported now.
+bool QuicAgentCertificate::RotateAgentCertificate() {
+  return false;
+}
+
+AgentFingerprint QuicAgentCertificate::GetAgentFingerprint() {
+  return agent_fingerprint_;
+}
+
+}  // namespace openscreen::osp
