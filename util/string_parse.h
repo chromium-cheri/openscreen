d4de536785664f84c06877118bb3a3d8f7a32819
mark a. foltz <mfoltz@chromium.org>
2024-07-30 08:48:32 -0700

    [Open Screen] Implement string_parse::ParseAsciiNumber
    This utility function replaces the usage of absl::SimpleAtoi.
    
    Bug: 158433927
    Change-Id: I8bfe880e8fd8723ef41afe7db4b1fc606496fc38

---
+++ util/string_parse.h
@@ -0,0 +1,40 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UTIL_STRING_PARSE_H_
+#define UTIL_STRING_PARSE_H_
+
+#include <charconv>
+#include <optional>
+#include <string_view>
+#include <system_error>
+#include <type_traits>
+
+namespace openscreen {
+
+namespace internal {
+
+template <typename T>
+using EnableIfArithmetic =
+    std::enable_if_t<std::is_arithmetic<T>::value>;  // NOLINT
+
+}  // namespace internal
+
+namespace string_parse {
+
+// Parses `number` into the numeric type `result` and returns true if
+// successful.  `number` must be an ASCII representation of an integer or
+// floating point value, and `result` must be compatible with the resulting
+// value.  If `number` cannot be parsed, then returns false.
+template <typename T, typename = internal::EnableIfArithmetic<T>>
+bool ParseAsciiNumber(std::string_view number, T& result) {
+  auto [ptr, error_code] =
+      std::from_chars(number.begin(), number.end(), result);
+  return ptr == number.end() && error_code == std::errc();
+}
+
+}  // namespace string_parse
+}  // namespace openscreen
+
+#endif  // UTIL_STRING_PARSE_H_
